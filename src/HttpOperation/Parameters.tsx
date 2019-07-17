5a5c32ba89f2df51519d85e8d379f50d0965cab7
Jakub Jankowski <jakubj@stoplight.io>
2019-07-17 18:02:04 +0200

    fix: overlapping params by breaking words [SD-658] (#5)
    * fix: fix overlapping params by breaking words
    
    * chore: use table for parameters
    
    * chore: adding missing lodash dependency

--- src/HttpOperation/Parameters.tsx
+++ src/HttpOperation/Parameters.tsx
@@ -1,6 +1,5 @@
 import { MarkdownViewer } from '@stoplight/markdown-viewer';
 import { IHttpParam } from '@stoplight/types';
-import cn from 'classnames';
 import * as React from 'react';
 
 export interface IParameterProps {
@@ -12,19 +11,23 @@ export const Parameter: React.FunctionComponent<IParameterProps> = ({ parameter,
   if (!parameter || !parameter.schema) return null;
 
   return (
-    <div className={cn('flex', className)}>
-      <div className="w-40">
+    <tr>
+      <td className={className} style={{ minWidth: '10rem' }}>
         <div>{parameter.name}</div>
 
         <div className={`font-semibold text-${parameter.required ? 'red' : 'gray'}-6 text-xs uppercase `}>
           {parameter.required ? 'Required' : 'Optional'}
         </div>
-      </div>
+      </td>
 
-      <div className="w-24">{parameter.schema && parameter.schema.type}</div>
+      <td className="w-24 align-top">{parameter.schema && parameter.schema.type}</td>
 
-      {parameter.description && <MarkdownViewer className="ml-12 flex-1" markdown={parameter.description} />}
-    </div>
+      {parameter.description && (
+        <td className="align-top">
+          <MarkdownViewer className="ml-12 flex-1" markdown={parameter.description} />
+        </td>
+      )}
+    </tr>
   );
 };
 Parameter.displayName = 'HttpOperation.Parameter';
@@ -42,11 +45,13 @@ export const Parameters: React.FunctionComponent<IParametersProps> = ({ paramete
     <div className={className}>
       {title && <div className="text-lg font-semibold">{title}</div>}
 
-      <div className="mt-5">
-        {parameters.map((parameter, index) => (
-          <Parameter key={index} className="mt-3" parameter={parameter} />
-        ))}
-      </div>
+      <table className="mt-5">
+        <tbody>
+          {parameters.map((parameter, index) => (
+            <Parameter key={index} className="pb-3" parameter={parameter} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
