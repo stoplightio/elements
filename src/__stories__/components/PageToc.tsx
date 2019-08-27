99abcdb0938b9d684f692e04a643b2e3de9a9faa
Karol Maciaszek <karol@maciaszek.pl>
2019-08-27 16:11:07 +0200

    feat: added PageToc to storybook

---
+++ src/__stories__/components/PageToc.tsx
@@ -0,0 +1,27 @@
+import { IHttpOperation, NodeType } from '@stoplight/types';
+import { object, withKnobs } from '@storybook/addon-knobs';
+import { boolean, text } from '@storybook/addon-knobs/react';
+import { storiesOf } from '@storybook/react';
+import cn from 'classnames';
+import * as React from 'react';
+import { IPageTocItem, PageToc } from '../../components/PageToc';
+
+const itemsFixture: IPageTocItem[] = require('../../__fixtures__/page-toc.json');
+
+export const darkMode = () => boolean('dark mode', false);
+
+export const knobs = () => ({
+  items: object('items', itemsFixture, 'PageToc'),
+});
+
+storiesOf('components/PageToc', module)
+  .addDecorator(withKnobs)
+  .add('default', () => {
+    const { items } = knobs();
+
+    return (
+      <div className={cn('absolute top-0 bottom-0 right-0 left-0', { 'bp3-dark bg-gray-8': darkMode() })}>
+        <PageToc className="py-12" items={items} />
+      </div>
+    );
+  });
--- src/__stories__/index.ts
+++ src/__stories__/index.ts
@@ -1,6 +1,7 @@
 import './components/HttpOperation';
 import './components/HttpService';
 import './components/Page';
+import './components/PageToc';
 import './components/TableOfContents';
 import './components/TryIt';
 
