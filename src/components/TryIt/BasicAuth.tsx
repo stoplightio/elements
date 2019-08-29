86897220697bd3c8eb2cb11b4c490d53cd78b325
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-08-29 15:42:46 -0500

    fix: flat security definitions

--- src/components/TryIt/BasicAuth.tsx
+++ src/components/TryIt/BasicAuth.tsx
@@ -1,6 +1,7 @@
 import { InputGroup } from '@blueprintjs/core';
 import { IHttpOperation } from '@stoplight/types';
 import cn from 'classnames';
+import flatten from 'lodash/flatten';
 import { observer } from 'mobx-react-lite';
 import * as React from 'react';
 import { useStore } from './context';
@@ -11,7 +12,7 @@ export const BasicAuth = observer<{ security: IHttpOperation['security']; classN
 
     if (!security || !security.length) return null;
 
-    const basicAuth = security.find(s => s.type === 'http' && s.scheme === 'basic');
+    const basicAuth = flatten(security).find(s => s.type === 'http' && s.scheme === 'basic');
     if (!basicAuth) return null;
 
     return (
