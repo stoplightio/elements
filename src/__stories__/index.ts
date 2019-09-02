6f76d844c98a0088247da45470b1e5c72df7cf46
Karol Maciaszek <karol@maciaszek.pl>
2019-09-02 16:03:01 +0200

    fix: linting issues

--- src/__stories__/index.ts
+++ src/__stories__/index.ts
@@ -1,9 +1,9 @@
 import './components/Article';
 import './components/HttpOperation';
 import './components/HttpService';
+import './components/PageToc';
 import './components/TableOfContents';
 import './components/TryIt';
-import './components/PageToc';
 
 import './containers/Hub';
 import './containers/TableOfContents';
