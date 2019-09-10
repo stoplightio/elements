4d156cffae2fe81250e26054f24baba2f7f04bff
Jakub Jankowski <jakubj@stoplight.io>
2019-09-10 18:11:26 +0200

    chore: storybook and styling update

--- src/components/HttpRequest.tsx
+++ src/components/HttpRequest.tsx
@@ -19,16 +19,17 @@ export interface IHttpRequestProps {
 
 export const HttpRequest: React.FunctionComponent<IHttpRequestProps> = ({ request, operation, className }) => {
   const store = new RequestMaker({ request, operation });
-  console.log(document.getElementsByClassName('bp3-dark'));
   return (
     <RequestMakerProvider value={store}>
       <div className={className}>
         <ControlGroup className="mb-3">
+          <SendButton className="HttpRequest__SendButton w-40" intent="primary" icon="play" />
           <MethodSelector className="HttpRequest__MethodSelector" />
           <ActionBar className="HttpRequest__ActionBar flex-auto" />
-          <SendButton className="HttpRequest__SendButton w-24" intent="primary" />
         </ControlGroup>
+
         <RequestEditor className="HttpRequest__RequestEditor mb-3" />
+
         <ResponseViewer className="HttpRequest__ResponseViewer" />
       </div>
     </RequestMakerProvider>
