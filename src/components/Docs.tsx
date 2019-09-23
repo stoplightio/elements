eebef6e7095d53de6524e2cefe4436065d9943dc
Thomas Pytleski <pytlesk4@gmail.com>
2019-09-23 14:55:46 -0500

    chore: pull out useComponents into a hook

--- src/components/Docs.tsx
+++ src/components/Docs.tsx
@@ -1,27 +1,15 @@
 import 'resize-observer-polyfill';
 
-import { IconName } from '@blueprintjs/core';
 import useComponentSize from '@rehooks/component-size';
 import { safeStringify } from '@stoplight/json';
-import { JsonSchemaViewer } from '@stoplight/json-schema-viewer';
-import {
-  BlockHeader,
-  CLASSNAMES,
-  ICodeAnnotations,
-  IComponentMappingProps,
-  MarkdownViewer,
-  processMarkdownTree,
-} from '@stoplight/markdown-viewer';
+import { MarkdownViewer, processMarkdownTree } from '@stoplight/markdown-viewer';
 import { Builder } from '@stoplight/markdown/builder';
 import { NodeType } from '@stoplight/types';
-import cn from 'classnames';
 import { JSONSchema4 } from 'json-schema';
 import * as React from 'react';
-import { ComponentsContext } from '../containers/Provider';
+
+import { useComponents } from '../hooks/useComponents';
 import { useComputePageToc } from '../hooks/useComputePageToc';
-import { useResolver } from '../hooks/useResolver';
-import { HttpOperation } from './HttpOperation';
-import { HttpService } from './HttpService';
 import { PageToc } from './PageToc';
 
 export interface IDocs {
@@ -84,55 +72,3 @@ export const Docs: React.FunctionComponent<IDocs> = ({ type, data, padding }) =>
     </div>
   );
 };
-
-const JSV_MAX_ROWS = 50;
-const MarkdownViewerCode: React.FunctionComponent<{
-  type: NodeType | 'json_schema';
-  value: any;
-  annotations: ICodeAnnotations;
-}> = ({ type, value, annotations }) => {
-  const resolved = useResolver(type, value);
-
-  if (type === NodeType.Model || type === 'json_schema') {
-    const title = annotations && annotations.title;
-    const icon: IconName = 'cube';
-    const color = '#ef932b';
-
-    return (
-      <div>
-        {title && <BlockHeader icon={icon} iconColor={color} title={title} />}
-
-        <div className={cn(CLASSNAMES.block, CLASSNAMES.bordered, 'dark:border-darken')}>
-          <JsonSchemaViewer schema={resolved} maxRows={JSV_MAX_ROWS} />
-        </div>
-      </div>
-    );
-  } else if (type === NodeType.HttpOperation) {
-    return <HttpOperation value={resolved} />;
-  } else if (type === NodeType.HttpService) {
-    return <HttpService value={resolved} />;
-  }
-
-  return null;
-};
-
-function useComponents() {
-  const Components = React.useContext(ComponentsContext);
-
-  return React.useMemo(() => {
-    return {
-      ...Components,
-
-      code: (props: IComponentMappingProps<any>, key: React.Key) => {
-        const { node, defaultComponents } = props;
-
-        const nodeType = node.annotations && node.annotations.type ? node.annotations.type : node.meta;
-        if (['json_schema', NodeType.Model, NodeType.HttpOperation, NodeType.HttpService].includes(nodeType)) {
-          return <MarkdownViewerCode key={key} type={nodeType} value={node.value} annotations={node.annotations} />;
-        }
-
-        return Components && Components.code ? Components.code(props, key) : defaultComponents.code(props, key);
-      },
-    };
-  }, [Components]);
-}
---
+++ src/hooks/useComponents.tsx
@@ -0,0 +1,63 @@
+import { IconName } from '@blueprintjs/core';
+import { JsonSchemaViewer } from '@stoplight/json-schema-viewer';
+import { BlockHeader, CLASSNAMES, ICodeAnnotations, IComponentMappingProps } from '@stoplight/markdown-viewer';
+import { NodeType } from '@stoplight/types';
+import cn from 'classnames';
+import React from 'react';
+
+import { HttpOperation } from '../components/HttpOperation';
+import { HttpService } from '../components/HttpService';
+import { ComponentsContext } from '../containers/Provider';
+import { useResolver } from './useResolver';
+
+const JSV_MAX_ROWS = 50;
+const MarkdownViewerCode: React.FunctionComponent<{
+  type: NodeType | 'json_schema';
+  value: any;
+  annotations: ICodeAnnotations;
+}> = ({ type, value, annotations }) => {
+  const resolved = useResolver(type, value);
+
+  if (type === NodeType.Model || type === 'json_schema') {
+    const title = annotations && annotations.title;
+    const icon: IconName = 'cube';
+    const color = '#ef932b';
+
+    return (
+      <div>
+        {title && <BlockHeader icon={icon} iconColor={color} title={title} />}
+
+        <div className={cn(CLASSNAMES.block, CLASSNAMES.bordered, 'dark:border-darken')}>
+          <JsonSchemaViewer schema={resolved} maxRows={JSV_MAX_ROWS} />
+        </div>
+      </div>
+    );
+  } else if (type === NodeType.HttpOperation) {
+    return <HttpOperation value={resolved} />;
+  } else if (type === NodeType.HttpService) {
+    return <HttpService value={resolved} />;
+  }
+
+  return null;
+};
+
+export function useComponents() {
+  const Components = React.useContext(ComponentsContext);
+
+  return React.useMemo(() => {
+    return {
+      ...Components,
+
+      code: (props: IComponentMappingProps<any>, key: React.Key) => {
+        const { node, defaultComponents } = props;
+
+        const nodeType = node.annotations && node.annotations.type ? node.annotations.type : node.meta;
+        if (['json_schema', NodeType.Model, NodeType.HttpOperation, NodeType.HttpService].includes(nodeType)) {
+          return <MarkdownViewerCode key={key} type={nodeType} value={node.value} annotations={node.annotations} />;
+        }
+
+        return Components && Components.code ? Components.code(props, key) : defaultComponents.code(props, key);
+      },
+    };
+  }, [Components]);
+}
