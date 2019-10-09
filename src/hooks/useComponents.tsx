e54f45f4349b02fde22c9d63c32ef3e610587b1c
Chris Lott <chrisnlott@gmail.com>
2019-10-09 13:58:53 -0500

    feat: resolve relative file $refs (#74)
    * feat: resolve relative file $refs
    
    * chore: move resolver hook usage lower in tree
    
    * chore: srn doesnt need to be required
    
    * test: find by Model component
    
    * test: http_operation and http_service
    
    * feat: allow passing a custom resolver

---
+++ src/hooks/useComponents.tsx
@@ -0,0 +1,45 @@
+import { CLASSNAMES, IComponentMappingProps } from '@stoplight/markdown-viewer';
+import { NodeType } from '@stoplight/types';
+import cn from 'classnames';
+import * as React from 'react';
+import { HttpOperation } from '../components/HttpOperation';
+import { HttpService } from '../components/HttpService';
+import { Model } from '../components/Model';
+import { ComponentsContext } from '../containers/Provider';
+
+export function useComponents() {
+  const Components = React.useContext(ComponentsContext);
+
+  return React.useMemo(() => {
+    return {
+      ...Components,
+
+      code: (props: IComponentMappingProps<any>, key: React.Key) => {
+        const { node, defaultComponents, parent } = props;
+        const { annotations, value } = node;
+
+        const nodeType = annotations && annotations.type ? annotations.type : node.meta;
+
+        if (nodeType === NodeType.Model || nodeType === 'json_schema') {
+          return (
+            <Model
+              key={key}
+              className={cn('dark:border-darken', {
+                [CLASSNAMES.bordered]: !parent || parent.type !== 'tab',
+                [CLASSNAMES.block]: !parent || parent.type !== 'tab',
+              })}
+              title={annotations && annotations.title}
+              schema={value}
+            />
+          );
+        } else if (nodeType === NodeType.HttpOperation) {
+          return <HttpOperation key={key} value={value} />;
+        } else if (nodeType === NodeType.HttpService) {
+          return <HttpService key={key} value={value} />;
+        }
+
+        return Components && Components.code ? Components.code(props, key) : defaultComponents.code(props, key);
+      },
+    };
+  }, [Components]);
+}
