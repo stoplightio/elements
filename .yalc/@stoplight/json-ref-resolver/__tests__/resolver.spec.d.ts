6ac7e83bee683fd63d159a8067366f0f5693f7bb
Chris <chrisnlott@gmail.com>
2019-10-12 13:30:28 -0500

    chore: remove json-ref-resolver yalc

--- .yalc/@stoplight/json-ref-resolver/__tests__/resolver.spec.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="urijs" />
-import * as Types from '../types';
-export declare class FileReader implements Types.IResolver {
-    resolve(uri: uri.URI): Promise<unknown>;
-}
-export declare class HttpReader implements Types.IResolver {
-    resolve(uri: uri.URI): Promise<any>;
-}
