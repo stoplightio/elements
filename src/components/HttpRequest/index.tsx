c2c6e95b71b03dd9b57695b6e4eb5b079fb6bf78
Chris <chrisnlott@gmail.com>
2019-10-24 02:14:37 -0500

    feat: embedded request-maker

--- src/components/HttpRequest/index.tsx
+++ src/components/HttpRequest/index.tsx
@@ -1,17 +1,6 @@
-import {
-  ActionBar,
-  MethodSelector,
-  RequestEditor,
-  RequestMaker,
-  RequestMakerProvider,
-  ResponseStatus,
-  ResponseViewer,
-  SendButton,
-} from '@stoplight/request-maker';
-import { ControlGroup } from '@stoplight/ui-kit';
-import cn from 'classnames';
-import { get } from 'lodash';
+import { RequestMaker } from '@stoplight/request-maker';
 import * as React from 'react';
+import { useRequestMaker } from '../../hooks/useRequestMaker';
 import { useResolver } from '../../hooks/useResolver';
 
 export interface IHttpRequestProps {
@@ -21,34 +10,7 @@ export interface IHttpRequestProps {
 
 export const HttpRequest = React.memo<IHttpRequestProps>(({ value, className }) => {
   const { result } = useResolver('http', value);
+  const store = useRequestMaker(result);
 
-  let request;
-  let operation;
-
-  // TODO (CL): Need a better way to handle this
-  if (get(result, 'id') === '?http-operation-id?') {
-    operation = result;
-  } else {
-    request = result;
-  }
-
-  const store = new RequestMaker({ request, operation });
-
-  return (
-    <RequestMakerProvider value={store}>
-      <div className={cn('HttpRequest', className)}>
-        <ControlGroup>
-          <SendButton className="HttpRequest__SendButton w-40" intent="primary" icon="play" />
-          <MethodSelector className="HttpRequest__MethodSelector" />
-          <ActionBar className="HttpRequest__ActionBar flex-auto" />
-        </ControlGroup>
-
-        <RequestEditor className="HttpRequest__RequestEditor mt-6" />
-
-        <ResponseStatus className="HttpRequest__ResponseStatus mt-6" />
-
-        <ResponseViewer className="HttpRequest__ResponseViewer mt-6" />
-      </div>
-    </RequestMakerProvider>
-  );
+  return <RequestMaker className={className} store={store} />;
 });
--- src/hooks/useRequestMaker.ts
+++ src/hooks/useRequestMaker.ts
@@ -1,16 +1,27 @@
-import { RequestMaker } from '@stoplight/request-maker';
-import { IHttpOperation } from '@stoplight/types';
+import { RequestMakerStore } from '@stoplight/request-maker';
+import { IHttpOperation, IHttpRequest } from '@stoplight/types';
+import { get } from 'lodash';
 import { MD5 } from 'object-hash';
 
 // Maps a hash of the operation to the request maker object
-const RequestMakers = new Map<string, RequestMaker>();
+const RequestMakers = new Map<string, RequestMakerStore>();
 
-export function useRequestMaker(operation: IHttpOperation, validate: boolean = false): RequestMaker {
-  const id = MD5(operation);
+export function useRequestMaker(value: IHttpOperation | IHttpRequest, validate: boolean = false): RequestMakerStore {
+  const id = MD5(value);
+
+  let operation: IHttpOperation | undefined;
+  let request: IHttpRequest | undefined;
+
+  // TODO (CL): Need a better way to handle this
+  if (get(value, 'id') === '?http-operation-id?') {
+    operation = value as IHttpOperation;
+  } else {
+    request = value as IHttpRequest;
+  }
 
   let requestMaker = RequestMakers.get(id);
   if (!requestMaker) {
-    requestMaker = new RequestMaker({ operation, validate });
+    requestMaker = new RequestMakerStore({ operation, request, validate });
     RequestMakers.set(id, requestMaker);
   }
 
