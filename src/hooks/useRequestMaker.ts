bd863f1bdaae1b694379016f51c7b41216c56857
Chris Lott <chrisnlott@gmail.com>
2019-10-29 19:50:16 -0500

    feat: embedded request maker (#91)
    - feat: adds support for embedding http requests in markdown using @stoplight/request-maker
    
    ![image](https://user-images.githubusercontent.com/7423098/67819236-4f9ea400-fa82-11e9-94bc-c5d99ea0f33a.png)

--- src/hooks/useRequestMaker.ts
+++ src/hooks/useRequestMaker.ts
@@ -1,16 +1,27 @@
-import { RequestMaker } from '@stoplight/request-maker';
-import { IHttpOperation } from '@stoplight/types';
+import { RequestMakerStore } from '@stoplight/request-maker';
+import { IHttpOperation, IHttpRequest } from '@stoplight/types';
+import { has } from 'lodash';
 import { MD5 } from 'object-hash';
 
 // Maps a hash of the operation to the request maker object
-const RequestMakers = new Map<string, RequestMaker>();
+const RequestMakers = new Map<string, RequestMakerStore>();
 
-export function useRequestMaker(operation: IHttpOperation, validate: boolean = false): RequestMaker {
-  const id = MD5(operation);
+export function useRequestMaker(value: IHttpOperation | IHttpRequest, validate: boolean = false): RequestMakerStore {
+  const id = MD5(value);
+
+  let operation: IHttpOperation | undefined;
+  let request: IHttpRequest | undefined;
+
+  // TODO (CL): Need a better way to determine if value is an http operation or http request
+  if (has(value, 'id') || has(value, 'iid') || has(value, 'request') || has(value, 'servers')) {
+    operation = value as IHttpOperation;
+  } else {
+    request = value as IHttpRequest;
+  }
 
   let requestMaker = RequestMakers.get(id);
   if (!requestMaker) {
-    requestMaker = new RequestMaker({ operation, validate });
+    requestMaker = new RequestMakerStore({ operation, request, validate });
     RequestMakers.set(id, requestMaker);
   }
 
