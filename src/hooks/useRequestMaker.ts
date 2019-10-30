c454065ed61735ad2cf23d43439b56a32884c3ee
Chris <chrisnlott@gmail.com>
2019-10-29 19:44:47 -0500

    chore: determine http operation

--- src/hooks/useRequestMaker.ts
+++ src/hooks/useRequestMaker.ts
@@ -1,6 +1,6 @@
 import { RequestMakerStore } from '@stoplight/request-maker';
 import { IHttpOperation, IHttpRequest } from '@stoplight/types';
-import { get } from 'lodash';
+import { has } from 'lodash';
 import { MD5 } from 'object-hash';
 
 // Maps a hash of the operation to the request maker object
@@ -12,8 +12,8 @@ export function useRequestMaker(value: IHttpOperation | IHttpRequest, validate: 
   let operation: IHttpOperation | undefined;
   let request: IHttpRequest | undefined;
 
-  // TODO (CL): Need a better way to handle this
-  if (get(value, 'id') === '?http-operation-id?') {
+  // TODO (CL): Need a better way to determine if value is an http operation or http request
+  if (has(value, 'id') || has(value, 'iid') || has(value, 'request') || has(value, 'servers')) {
     operation = value as IHttpOperation;
   } else {
     request = value as IHttpRequest;
