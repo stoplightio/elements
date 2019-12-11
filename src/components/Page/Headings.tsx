63092b1d97511c59dd64a2dc3524d70a4f3f48b7
Sarah Spear <sarahrva@gmail.com>
2019-12-11 14:58:19 -0600

    fix: enable scrolling for long heading lists

--- src/components/Page/Headings.tsx
+++ src/components/Page/Headings.tsx
@@ -64,7 +64,7 @@ export const PageHeadings: React.FC<IPageHeadings> = ({
   }
 
   return (
-    <div className={cn(`sticky top-0 pt-${padding} h-full px-4 overflow-auto`, className)}>
+    <div className={cn(`sticky top-0 pt-${padding} h-screen px-4 overflow-auto`, className)}>
       <div className="border-l border-gray-2 dark:border-lighten-4">{component}</div>
     </div>
   );
