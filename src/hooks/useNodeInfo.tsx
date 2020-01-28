034c94703b3ad09c38cd9779c276ee34fee23e45
Chris <chrisnlott@gmail.com>
2020-01-27 23:02:40 -0600

    fix: add deref bundle to nodes info request

--- src/hooks/useNodeInfo.tsx
+++ src/hooks/useNodeInfo.tsx
@@ -2,5 +2,8 @@ import { INodeInfo } from '../types';
 import { useRequest } from './useRequest';
 
 export function useNodeInfo(srn: string, opts: { group?: string; version?: string } = {}) {
-  return useRequest<INodeInfo>({ url: '/nodes.info', params: { srn, version: opts.version, group: opts.group } });
+  return useRequest<INodeInfo>({
+    url: '/nodes.info',
+    params: { srn, version: opts.version, group: opts.group, deref: 'bundle' },
+  });
 }
