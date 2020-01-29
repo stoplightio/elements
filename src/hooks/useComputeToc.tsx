99ae14c7c49f965dbddb69208055d82856ca3a36
Jakub Jankowski <jakubj@stoplight.io>
2020-01-29 22:43:16 +0100

    feat: remove docs from uri

--- src/hooks/useComputeToc.tsx
+++ src/hooks/useComputeToc.tsx
@@ -56,7 +56,9 @@ export function computeToc(_nodes: IProjectNode[], icons: NodeIconMapping): ICon
     if (!docsNodes[nodeIndex]) continue;
     const node = docsNodes[nodeIndex];
 
-    const parts = node.uri.split('/');
+    // Strip off the /docs since we ignore that folder
+    const uri = node.uri.replace(/^\/docs\//, '');
+    const parts = uri.split('/');
 
     // Handle adding the parent folders if we haven't already added them
     if (parts.length > 1) {
