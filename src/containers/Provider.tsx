a735632172f6f523803b04c877b941b3f3d929d0
Chris <chrisnlott@gmail.com>
2020-02-04 23:29:54 -0600

    fix: lint error

--- src/containers/Provider.tsx
+++ src/containers/Provider.tsx
@@ -41,8 +41,8 @@ export const Provider: React.FunctionComponent<IProvider> = ({
       host: host || defaultHost,
       headers: token
         ? {
-          Authorization: `Bearer ${token}`,
-        }
+            Authorization: `Bearer ${token}`,
+          }
         : null,
     }),
     [host, token],
