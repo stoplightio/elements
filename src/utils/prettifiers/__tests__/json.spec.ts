00090dbf0a384c5c5fba249d72d8f4c7f26d1b51
Chris Lott <chrisnlott@gmail.com>
2020-02-20 15:45:14 -0600

    feat: fix build for hooks, remove prettier
    BREAKING CHANGE
    
    * fix: hook imports
    
    * chore: fix prettier warning
    
    * chore: yarn.lock
    
    * chore: remove webpack
    
    * chore: reorganise scripts
    
    * chore: fix typescript presence
    
    * Revert "fix: hook imports"
    
    This reverts commit b8f016e754f06bfdf9e14ac64cd4b7c6458eab86.
    
    * chore: remove old widgets
    
    * refactor: remove prettifier stuff
    
    * refactor: prettifier remove
    
    Co-authored-by: Vincenzo Chianese <vincenz.chianese@icloud.com>

--- src/utils/prettifiers/__tests__/json.spec.ts
+++
@@ -1,7 +0,0 @@
-import { prettifyJSON } from '../json';
-
-describe('JSON Prettifier', () => {
-  test('should prettify content', () => {
-    expect(prettifyJSON(`{"foo":true,"bar":false}`)).toMatchSnapshot();
-  });
-});
