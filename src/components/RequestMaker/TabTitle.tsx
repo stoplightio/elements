c6bb5ed2ad03ecf56791ae26ed10d0540b2152e4
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-02-24 12:36:42 -0600

    refactor: if it can only be a number, let it be a number

--- src/components/RequestMaker/TabTitle.tsx
+++ src/components/RequestMaker/TabTitle.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export function TabTitle({ title, count, color = 'blue' }: { title: string; count?: number | string; color?: string }) {
+export function TabTitle({ title, count, color = 'blue' }: { title: string; count?: number; color?: string }) {
   return (
     <div className="flex items-center p-1">
       <div className="flex-1">{title}</div>
