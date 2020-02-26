aa2166c0bdae3002bb28d99ad96d9e392f0409d3
Marcell Toth <marcell@stoplight.io>
2020-02-26 13:35:01 +0100

    chore: remove local @types folder and install from DefinitelyTyped

--- @types/parse-prefer-header/index.d.ts
+++
@@ -1,6 +0,0 @@
-// TODO: remove and install @types/parse-prefer-header when available
-
-declare module 'parse-prefer-header' {
-  function parsePreferHeader(preferHeader: string | string[]): { [key: string]: string | boolean };
-  export = parsePreferHeader;
-}
