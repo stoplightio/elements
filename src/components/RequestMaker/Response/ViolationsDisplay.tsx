34ce961a693e49ddf4ca10b524ef5085c0410390
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-02-26 10:48:29 -0600

    refactor: violations assumes the array is not empty

--- src/components/RequestMaker/Response/ViolationsDisplay.tsx
+++ src/components/RequestMaker/Response/ViolationsDisplay.tsx
@@ -1,25 +1,21 @@
 import { IPrismDiagnostic } from '@stoplight/prism-core';
 import * as React from 'react';
 
-export const ViolationsDisplay = ({ violations }: { violations: IPrismDiagnostic[] }) => {
+export const ViolationsDisplay = ({ violations }: { violations: IPrismDiagnostic[] & { 0: IPrismDiagnostic } }) => {
   return (
     <section className="RequestMaker__SuggestionBar p-2 pl-4 pr-4">
-      {violations.length ? (
-        <div>
-          <strong>
-            The returned response has some violations with the JSON Schema associated with the current operation
-          </strong>
-          <ul>
-            {violations.map(v => (
-              <li key={v.message}>
-                {v.path} {v.message}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        undefined
-      )}
+      <div>
+        <strong>
+          The returned response has some violations with the JSON Schema associated with the current operation
+        </strong>
+        <ul>
+          {violations.map(v => (
+            <li key={v.message}>
+              {v.path} {v.message}
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
