66686edf1c4c59a602b914b9fd110ccde1c65446
Marcell Toth <marcell@stoplight.io>
2020-02-28 17:53:36 +0100

    chore: implement UI tweaks to ViolationsDisplay

--- src/components/RequestMaker/Response/ViolationsDisplay.tsx
+++ src/components/RequestMaker/Response/ViolationsDisplay.tsx
@@ -1,9 +1,9 @@
 import { ITreeNode, Tree } from '@blueprintjs/core';
 import { IPrismDiagnostic } from '@stoplight/prism-core';
-import { Button, Code, Collapse } from '@stoplight/ui-kit';
+import { Button, Code, Collapse, Icon } from '@stoplight/ui-kit';
 import { NonEmptyArray } from 'fp-ts/lib/NonEmptyArray';
 import { isEqual, uniq } from 'lodash';
-import { useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as React from 'react';
 
 type ViolationsDisplayProps = {
@@ -11,19 +11,33 @@ type ViolationsDisplayProps = {
   defaultOpen?: boolean;
 };
 
-export const ViolationsDisplay: React.FC<ViolationsDisplayProps> = ({ violations, defaultOpen = true }) => {
+export const ViolationsDisplay: React.FC<ViolationsDisplayProps> = ({ violations, defaultOpen = false }) => {
   const [isOpen, setOpen] = useState(defaultOpen);
 
-  const treeData = useMemo(() => buildViolationsTree(violations), [violations]);
+  const errorCount = violations.filter(v => v.severity === 0).length;
+  const warningCount = violations.length - errorCount;
 
   return (
     <section className="RequestMaker__ViolationsDisplay p-3 px-4">
-      <strong className="mb-1">
-        The returned response does not match the JSON Schema associated with the current operation.
-        <Button icon={isOpen ? 'caret-up' : 'caret-down'} onClick={() => setOpen(!isOpen)} minimal />
-      </strong>
+      <Button rightIcon={isOpen ? 'caret-up' : 'caret-down'} onClick={() => setOpen(!isOpen)} minimal>
+        <strong className="mb-1">
+          {errorCount > 0 && (
+            <span className="pr-3">
+              <Icon icon="error" className="mr-1" />
+              <span>{errorCount}</span>
+            </span>
+          )}
+          {warningCount > 0 && (
+            <span className="pr-3">
+              <Icon icon="warning-sign" className="mr-1" />
+              <span>{warningCount}</span>
+            </span>
+          )}
+          The returned response does not match the JSON Schema associated with the current operation.
+        </strong>
+      </Button>
       <Collapse isOpen={isOpen} transitionDuration={0}>
-        <Tree contents={treeData} />
+        <ViolationsTree violations={violations} />
       </Collapse>
     </section>
   );
@@ -31,7 +45,47 @@ export const ViolationsDisplay: React.FC<ViolationsDisplayProps> = ({ violations
 
 ViolationsDisplay.displayName = 'RequestMaker.ViolationsDisplay';
 
-const buildViolationsTree = (
+type ViolationsTreeProps = {
+  violations: readonly IPrismDiagnostic[];
+} & Omit<React.ComponentPropsWithoutRef<typeof Tree>, 'contents'>;
+
+const ViolationsTree: React.FC<ViolationsTreeProps> = ({ violations, ...rest }) => {
+  const [tree, setTree] = useState<ITreeNode[]>([]);
+  useEffect(() => {
+    setTree(buildTreeStructure(violations));
+  }, [violations]);
+
+  const refresh = () => {
+    setTree([...tree]);
+  };
+
+  const handleNodeCollapse = (nodeData: ITreeNode) => {
+    nodeData.isExpanded = false;
+    refresh();
+  };
+
+  const handleNodeExpand = (nodeData: ITreeNode) => {
+    nodeData.isExpanded = true;
+    refresh();
+  };
+
+  const handleNodeClick = (nodeData: ITreeNode) => {
+    nodeData.isExpanded = !nodeData.isExpanded;
+    refresh();
+  };
+
+  return (
+    <Tree
+      contents={tree}
+      {...rest}
+      onNodeCollapse={handleNodeCollapse}
+      onNodeExpand={handleNodeExpand}
+      onNodeClick={handleNodeClick}
+    />
+  );
+};
+
+const buildTreeStructure = (
   violationsFlat: readonly IPrismDiagnostic[],
   parentPaths: string[] = [],
 ): Array<ITreeNode<IPrismDiagnostic>> => {
@@ -48,11 +102,9 @@ const buildViolationsTree = (
       label: <Code>{path}</Code>,
       childNodes: [
         ...violationsOnPath.map(createViolationTreeNode),
-        ...buildViolationsTree(violationsOnChildren, currentPathArray),
+        ...buildTreeStructure(violationsOnChildren, currentPathArray),
       ],
       isExpanded: true,
-      icon: 'document',
-      hasCaret: false,
     };
   });
 
