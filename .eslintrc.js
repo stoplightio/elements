79647ec39fa7d8f1209781b3b2080878620ab5de
Marcell Toth <marcell@marcelltoth.net>
2020-03-30 23:42:19 +0300

    chore: upgrade to @stoplight/eslint-config 1.0.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,5 @@ module.exports = {
   extends: ['@stoplight'],
   rules: {
     'no-console': ['error', { allow: ['warn', 'error'] }],
-    '@typescript-eslint/no-unused-expressions': 'error',
-    'no-unused-expressions': 'off',
   },
 };
