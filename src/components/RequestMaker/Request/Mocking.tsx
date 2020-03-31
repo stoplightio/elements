f9b19d024d94761690f1c38f8ccb38c49e10ce13
Chris Lott <chrisnlott@gmail.com>
2020-03-31 13:13:45 -0500

    fix: mocking tab ux (#319)
    * fix: mocking tab ux
    
    * fix: run tests
    
    * fix: enable request validation
    
    * fix: update tests
    
    * fix: additional test fixes
    
    * test: add testing library
    
    * fix: update tests to react testing library
    
    * chore: cleanup
    
    * style: fix lint stuff
    
    * test: fix validation
    
    * test: fix example name
    
    Co-authored-by: Sarah Spear <sarahrva@gmail.com>
    Co-authored-by: Marcell Toth <marcell@marcelltoth.net>
    Co-authored-by: Vincenzo Chianese <vincenz.chianese@icloud.com>

--- src/components/RequestMaker/Request/Mocking.tsx
+++ src/components/RequestMaker/Request/Mocking.tsx
@@ -1,4 +1,5 @@
-import { HTMLSelect, Popover, Switch } from '@stoplight/ui-kit';
+import { Button, HTMLSelect, Menu, MenuDivider, MenuItem, Popover, Position, Switch } from '@stoplight/ui-kit';
+import cn from 'classnames';
 import { observer } from 'mobx-react-lite';
 import * as React from 'react';
 
@@ -21,13 +22,6 @@ export const Mocking = observer(() => {
 
   const currentCode = (store.prismConfig.mock && store.prismConfig.mock.code) || '';
 
-  const codeOptions = React.useMemo(() => {
-    if (!operationResponses) {
-      return [notSetOption];
-    }
-    return [notSetOption, ...operationResponses.map(r => ({ value: r.code, label: r.code }))];
-  }, [operationResponses]);
-
   const currentExample = (store.prismConfig.mock && store.prismConfig.mock.exampleKey) || '';
 
   const exampleOptions = React.useMemo(() => {
@@ -48,144 +42,170 @@ export const Mocking = observer(() => {
 
   const currentDynamicSetting = store.prismConfig.mock && store.prismConfig.mock.dynamic ? 'dynamic' : 'static';
 
+  let responseText = 'Not Set';
+  if (currentCode) {
+    responseText = currentCode;
+
+    if (currentExample) {
+      responseText = `${currentCode} - ${currentExample}`;
+    }
+  }
+
   return (
-    <div>
-      <Popover
-        interactionKind={'hover'}
-        position={'bottom-right'}
-        boundary={'window'}
-        disabled={store.isMatchingOperation}
-        content={
-          <div className="p-3 w-80 text-center">
-            <div className="text-red uppercase text-sm">Unable to mock this request</div>
-            <div className="pt-1">
-              Request method "{store.request.method}" does not match operation method "{store.operation.method}"
+    <div className="px-8 py-6">
+      <ConfigurationRow
+        title="Mocking"
+        description={
+          <div>
+            <div>
+              Enable mocking to send requests to a simulated API. You can choose to receive a specific response code and
+              example or have one dynamically generated for you based on this operation's response schema.
+            </div>
+
+            <div className="mt-2">
+              For more information on mocking,{' '}
+              <a href="https://stoplight.io/p/docs/gh/stoplightio/prism/docs/guides/01-mocking.md">
+                check out the docs
+              </a>
             </div>
           </div>
         }
       >
-        <Switch
-          className="mx-8 mt-5"
-          label="Enable Mocking"
-          checked={store.isMockEnabled}
-          onChange={() => {
-            requestStore.shouldMock = !requestStore.shouldMock;
-          }}
-          disabled={!store.isMatchingOperation}
-        />
-      </Popover>
-
-      <ConfigurationRow
-        title="Response Code"
-        description={
-          <>
-            You can request a specific HTTP response by specifying the status code in the <code>__code</code> in the
-            query string parameter.
-          </>
-        }
-      >
-        <HTMLSelect
-          className="code-selector"
-          disabled={!store.isMockEnabled}
-          options={codeOptions}
-          onChange={event => store.setPrismMockingOption('code', event.currentTarget.value || undefined)}
-          value={currentCode}
-        />
+        <Popover
+          interactionKind={'hover'}
+          position={'bottom-right'}
+          boundary={'window'}
+          disabled={store.isMatchingOperation}
+          content={
+            <div className="p-3 w-80 text-center">
+              <div className="text-red uppercase text-sm">Unable to mock this request</div>
+              <div className="pt-1">
+                Request method "{store.request.method}" does not match operation method "{store.operation.method}"
+              </div>
+            </div>
+          }
+        >
+          <Switch
+            className="mx-8 mt-5"
+            label="Enable Mocking"
+            checked={store.isMockEnabled}
+            onChange={() => {
+              requestStore.shouldMock = !requestStore.shouldMock;
+            }}
+            disabled={!store.isMatchingOperation}
+          />
+        </Popover>
       </ConfigurationRow>
 
-      <ConfigurationRow
-        title="Response Example"
-        description={
-          <>
-            You can request a specific example from your document by using the <code>__example</code> query string
-            parameter.
-          </>
-        }
-      >
-        <HTMLSelect
-          className="example-selector"
-          disabled={!store.isMockEnabled}
-          options={exampleOptions}
-          onChange={event => store.setPrismMockingOption('exampleKey', event.currentTarget.value || undefined)}
-          value={currentExample}
-        />
-      </ConfigurationRow>
+      <div className="relative pt-6">
+        {!store.isMockEnabled && <div className="absolute bg-lighten-9 dark:bg-darken-9 inset-0 z-10" />}
 
-      <ConfigurationRow
-        title="Dynamic mocking"
-        description={
-          <>
-            You can request a dynamically generated response example by using the <code>__dynamic</code> query string
-            parameter. For more information on static vs dynamic generation,{' '}
-            <a
-              href="https://stoplight.io/p/docs/gh/stoplightio/prism/docs/guides/01-mocking.md#static-or-dynamic-generation"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              check out the docs
-            </a>
-            .
-          </>
-        }
-      >
-        <HTMLSelect
-          className="dynamic-mode-selector"
-          disabled={!store.isMockEnabled}
-          options={dynamicOptions}
-          onChange={event => {
-            store.setPrismMockingOption('dynamic', event.currentTarget.value === 'dynamic');
-          }}
-          value={currentDynamicSetting}
-        />
-      </ConfigurationRow>
+        <ConfigurationRow
+          title="Response Code"
+          description={
+            <div>
+              <div>
+                By default, an appropriate response will be returned based on the request input. If you would like to
+                receive a specific response, you can choose one of the defined codes and examples using the dropdown to
+                the right and a <code>Prefer</code> header will be configured for you.
+              </div>
+            </div>
+          }
+        >
+          <Popover
+            autoFocus={false}
+            content={
+              <Menu>
+                <MenuItem
+                  active={!currentCode}
+                  text="Not Set"
+                  onClick={() => {
+                    store.setPrismMockingOption('code', undefined);
+                    store.setPrismMockingOption('exampleKey', undefined);
+                  }}
+                />
 
-      <ConfigurationRow
-        title="Validate request"
-        description="You can validate your request. If you turn off request validation Prism will return a response even if the input does not match the specification."
-      >
-        <Switch
-          className="validate-request-switch mx-8 mt-6"
-          large
-          innerLabel="Off"
-          innerLabelChecked="On"
-          checked={store.prismConfig.validateRequest}
-          onChange={() => {
-            store.setPrismConfigurationOption('validateRequest', !store.prismConfig.validateRequest);
-          }}
-          disabled={!store.isMockEnabled}
-        />
-      </ConfigurationRow>
+                {operationResponses?.map(operationResponse => {
+                  const isActive = operationResponse.code === currentCode;
+                  const exampleKeys = operationResponse.contents
+                    ?.flatMap(c => c.examples || [])
+                    .map(example => example.key);
 
-      <ConfigurationRow
-        title="Validate response"
-        description="You can validate the response. This checks if the selected example matches the specification."
-      >
-        <Switch
-          className="validate-response-switch mx-8 mt-6"
-          large
-          innerLabel="Off"
-          innerLabelChecked="On"
-          checked={store.prismConfig.validateResponse}
-          onChange={() => {
-            store.setPrismConfigurationOption('validateResponse', !store.prismConfig.validateResponse);
-          }}
-          disabled={!store.isMockEnabled}
-        />
-      </ConfigurationRow>
+                  const exampleChildren = exampleKeys?.map(exampleKey => (
+                    <MenuItem
+                      active={isActive && exampleKey === currentExample}
+                      text={exampleKey}
+                      key={exampleKey}
+                      onClick={() => {
+                        store.setPrismMockingOption('code', operationResponse.code);
+                        store.setPrismMockingOption('exampleKey', exampleKey);
+                      }}
+                    />
+                  ));
 
-      <ConfigurationRow title="Check security" description="You can check the security of your request.">
-        <Switch
-          className="check-security-switch mx-8 mt-6"
-          large
-          innerLabel="Off"
-          innerLabelChecked="On"
-          checked={store.prismConfig.checkSecurity}
-          onChange={() => {
-            store.setPrismConfigurationOption('checkSecurity', !store.prismConfig.checkSecurity);
-          }}
-          disabled={!store.isMockEnabled}
-        />
-      </ConfigurationRow>
+                  if (exampleKeys?.length) {
+                    exampleChildren?.unshift(
+                      <MenuItem
+                        key="no-example"
+                        active={isActive && !currentExample}
+                        text="No Example"
+                        onClick={() => {
+                          store.setPrismMockingOption('code', operationResponse.code);
+                          store.setPrismMockingOption('exampleKey', undefined);
+                        }}
+                      />,
+                      <MenuDivider key="divider" />,
+                    );
+                  }
+
+                  return (
+                    <MenuItem
+                      active={isActive}
+                      text={operationResponse.code}
+                      key={operationResponse.code}
+                      onClick={() => {
+                        store.setPrismMockingOption('code', operationResponse.code);
+                        store.setPrismMockingOption('exampleKey', undefined);
+                      }}
+                    >
+                      {exampleChildren}
+                    </MenuItem>
+                  );
+                })}
+              </Menu>
+            }
+            position={Position.BOTTOM}
+            boundary="window"
+            minimal
+          >
+            <Button rightIcon="double-caret-vertical" disabled={!store.isMockEnabled} text={responseText} />
+          </Popover>
+        </ConfigurationRow>
+
+        <ConfigurationRow
+          className="pt-6"
+          title="Response Generation"
+          description={
+            <div>
+              <div>
+                By default, mocked responses are statically generated. If you would like to receive a dynamically
+                generated response, you can choose the <code>dynamic</code> option using the dropdown to the right and a{' '}
+                <code>Prefer</code> headed will be configured for you.
+              </div>
+            </div>
+          }
+        >
+          <HTMLSelect
+            className="dynamic-mode-selector"
+            disabled={!store.isMockEnabled}
+            options={dynamicOptions}
+            onChange={event => {
+              store.setPrismMockingOption('dynamic', event.currentTarget.value === 'dynamic');
+            }}
+            value={currentDynamicSetting}
+          />
+        </ConfigurationRow>
+      </div>
     </div>
   );
 });
@@ -193,14 +213,16 @@ export const Mocking = observer(() => {
 type ConfigurationRowProps = {
   title: string;
   description: React.ReactNode;
+  className?: string;
 };
 
-const ConfigurationRow: React.FC<ConfigurationRowProps> = ({ title, description, children }) => (
-  <div className="mx-8 my-6 flex">
+const ConfigurationRow: React.FC<ConfigurationRowProps> = ({ className, title, description, children }) => (
+  <div className={cn('flex', className)}>
     <div className="w-2/3">
       <div className="font-bold">{title}</div>
       <div className="pt-4">{description}</div>
     </div>
-    <div className="flex items-center mx-auto">{children}</div>
+
+    <div className="mx-auto">{children}</div>
   </div>
 );
