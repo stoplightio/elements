1878d492a1144b1e6b24abf0e80d2d2bb501f502
Sarah Spear <sarahrva@gmail.com>
2020-04-22 15:25:09 -0500

    fix: set active node edge in useEffect

--- src/components/Dependencies/Outbound.tsx
+++ src/components/Dependencies/Outbound.tsx
@@ -67,6 +67,7 @@ export const OutboundDependencies = ({ className, node, edges, getNetwork }: IOu
           color: '#66b1e7',
         },
       });
+      setActiveNodeEdge(undefined);
     }
   }, [activeNodeEdge, rootNodeId, edges, node.snapshot.name]);
 
