811973e5eae3ee22035f043dd61082eb11300e0d
Chris <chrisnlott@gmail.com>
2020-04-24 16:56:04 -0500

    refactor: graph to functional component

--- src/components/Dependencies/Graph.tsx
+++ src/components/Dependencies/Graph.tsx
@@ -1,22 +1,16 @@
 import { isEqual } from 'lodash';
 import * as React from 'react';
-import * as vis from 'vis-network/standalone';
-import { DataSetEdges, DataSetNodes, Network, NetworkEvents, Options } from 'vis-network/standalone';
+import { Network, NetworkEvents, Options } from 'vis-network/standalone';
 
 import { IVisGraph } from '../../hooks/useComputeVisGraph';
-
-// @ts-ignore: Need to add typings
-const DataSet = vis.DataSet;
 
 export interface IGraph {
   id: string | number;
   graph: IVisGraph;
-  events?: {
+  events: {
     [key in NetworkEvents]?: (params: any) => void;
   };
-  getNetwork?: (network?: Network) => void;
-  getNodes?: (nodes: DataSetNodes) => void;
-  getEdges?: (edges: DataSetEdges) => void;
+  getNetwork: (network?: Network) => void;
 }
 
 const visOptions: Options = {
@@ -71,85 +65,36 @@ const visOptions: Options = {
   },
 };
 
-// TODO (CL): Convert to function component
-export class Graph extends React.Component<IGraph> {
-  public readonly container = React.createRef<HTMLElement>();
-  public readonly nodes: DataSetNodes = new DataSet();
-  public readonly edges: DataSetEdges = new DataSet();
-  public Network?: Network;
+export const Graph = React.memo<IGraph>(
+  ({ id, graph, events, getNetwork }) => {
+    const container = React.useRef<HTMLDivElement | null>(null);
 
-  constructor(props: IGraph) {
-    super(props);
+    React.useEffect(() => {
+      console.log('Graph useEffect');
+      if (!container.current) return;
 
-    this.updateGraph = this.updateGraph.bind(this);
-  }
+      const visNetwork = new Network(container.current, graph, visOptions);
 
-  public componentDidMount() {
-    this.edges.add(this.props.graph.edges);
-    this.nodes.add(this.props.graph.nodes);
-    this.updateGraph();
-  }
+      visNetwork.once('afterDrawing', () => visNetwork?.setSize('', ''));
 
-  public shouldComponentUpdate(nextProps: IGraph) {
-    const nodesChange = !isEqual(this.props.graph.nodes, nextProps.graph.nodes);
-    const edgesChange = !isEqual(this.props.graph.edges, nextProps.graph.edges);
+      if (getNetwork) {
+        getNetwork(visNetwork);
+      }
 
-    // Should update if nodes, edges, or the ID changes
-    return nodesChange || edgesChange || this.props.id !== nextProps.id;
-  }
-
-  public componentDidUpdate() {
-    this.updateGraph();
-  }
-
-  public updateGraph() {
-    if (this.container.current) {
-      this.Network = new Network(
-        this.container.current,
-        Object.assign({}, this.props.graph, {
-          edges: this.edges,
-          nodes: this.nodes,
-        }),
-        visOptions,
-      );
-
-      this.Network.once('afterDrawing', () => this.Network?.setSize('', ''));
-    }
-
-    if (this.props.getNetwork) {
-      this.props.getNetwork(this.Network);
-    }
-
-    if (this.props.getNodes) {
-      this.props.getNodes(this.nodes);
-    }
-
-    if (this.props.getEdges) {
-      this.props.getEdges(this.edges);
-    }
-
-    if (this.Network) {
-      // Add user provied events to network
-      const events = this.props.events || {};
-      for (const eventName of Object.keys(events) as NetworkEvents[]) {
-        const cb = events[eventName];
-        if (cb) {
-          this.Network.on(eventName, cb);
+      if (visNetwork) {
+        // Add user provied events to network
+        for (const eventName of Object.keys(events) as NetworkEvents[]) {
+          const cb = events[eventName];
+          if (cb) {
+            visNetwork.on(eventName, cb);
+          }
         }
       }
-    }
-  }
+    });
 
-  public render() {
-    const { id } = this.props;
-    return React.createElement(
-      'div',
-      {
-        id,
-        ref: this.container,
-        style: { width: '100%', height: '100%' },
-      },
-      id,
-    );
-  }
-}
+    return <div id={String(id)} ref={container} style={{ width: '100%', height: '100%' }} />;
+  },
+  (p, n) => {
+    return p.id === n.id && isEqual(p.graph, n.graph);
+  },
+);
