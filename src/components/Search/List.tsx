09cef65066b8114440a0e1a51fdbd539b0d377bc
Chris <chrisnlott@gmail.com>
2020-05-19 12:10:20 -0500

    fix: tree shake useComponents hook

--- src/components/Search/List.tsx
+++ src/components/Search/List.tsx
@@ -4,7 +4,7 @@ import { ScrollContainer } from '@stoplight/ui-kit/ScrollContainer';
 import cn from 'classnames';
 import * as React from 'react';
 
-import { useComponents } from '../../hooks';
+import { useComponents } from '../../hooks/useComponents';
 import { IProjectNode } from '../../types';
 import { NodeTypeColors, NodeTypeIcons, NodeTypePrettyName } from '../../utils/node';
 
