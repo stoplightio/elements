e0ddaea45d7173a4d614db10c3d3ab611cf1eee7
Jakub Rożek <jakub@stoplight.io>
2020-06-04 17:03:46 +0200

    fix: revert: "fix: resolve http operation refs against the root (#423)"
    This reverts commit 8e6f1749f788188440d8b180d333bb623d9565da.

--- src/components/Docs/HttpOperation/index.tsx
+++ src/components/Docs/HttpOperation/index.tsx
@@ -1,15 +1,11 @@
-import { pointerToPath } from '@stoplight/json';
-import { SchemaTreeRefDereferenceFn } from '@stoplight/json-schema-viewer';
 import { withErrorBoundary } from '@stoplight/react-error-boundary';
 import { IHttpOperation } from '@stoplight/types';
 import { Classes } from '@stoplight/ui-kit';
 import cn from 'classnames';
-import { get } from 'lodash';
 import * as React from 'react';
 
 import { IDocsComponentProps } from '..';
 import { HttpMethodColors } from '../../../constants';
-import { InlineRefResolverContext } from '../../../containers/Provider';
 import { MarkdownViewer } from '../../MarkdownViewer';
 import { Request } from './Request';
 import { Responses } from './Responses';
@@ -19,33 +15,27 @@ export type HttpOperationProps = IDocsComponentProps<Partial<IHttpOperation>>;
 const HttpOperationComponent = React.memo<HttpOperationProps>(({ className, data }) => {
   const color = HttpMethodColors[data.method!] || 'gray';
 
-  const inlineRefResolver: SchemaTreeRefDereferenceFn = ({ pointer }) => {
-    return pointer ? get(data, pointerToPath(pointer)) : null;
-  };
-
   return (
-    <InlineRefResolverContext.Provider value={inlineRefResolver}>
-      <div className={cn('HttpOperation', className)}>
-        <h2 className={cn(Classes.HEADING, 'flex items-center mb-10')}>
-          <div
-            className={cn(`uppercase mr-5 font-semibold border rounded py-1 px-2`, `text-${color}`, `border-${color}`)}
-          >
-            {data.method || 'UNKNOWN'}
-          </div>
+    <div className={cn('HttpOperation', className)}>
+      <h2 className={cn(Classes.HEADING, 'flex items-center mb-10')}>
+        <div
+          className={cn(`uppercase mr-5 font-semibold border rounded py-1 px-2`, `text-${color}`, `border-${color}`)}
+        >
+          {data.method || 'UNKNOWN'}
+        </div>
 
-          {data.path && <div className="flex-1 font-medium text-gray-6 dark:text-gray-3">{data.path}</div>}
-        </h2>
+        {data.path && <div className="flex-1 font-medium text-gray-6 dark:text-gray-3">{data.path}</div>}
+      </h2>
 
-        <MarkdownViewer
-          className="HttpOperation__Description mb-10 ml-1"
-          markdown={data.description || '*No description.*'}
-        />
+      <MarkdownViewer
+        className="HttpOperation__Description mb-10 ml-1"
+        markdown={data.description || '*No description.*'}
+      />
 
-        <Request request={data.request} security={data.security} />
+      <Request request={data.request} security={data.security} />
 
-        {data.responses && <Responses responses={data.responses} />}
-      </div>
-    </InlineRefResolverContext.Provider>
+      {data.responses && <Responses responses={data.responses} />}
+    </div>
   );
 });
 HttpOperationComponent.displayName = 'HttpOperation.Component';
