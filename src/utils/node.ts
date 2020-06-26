3b9ce8533337640624da7da1b25b47f2d5e1f117
Thomas Pytleski <pytlesk4@gmail.com>
2020-06-25 19:50:38 -0500

    Update src/utils/node.ts
    Co-authored-by: Jakub Rożek <jakub@stoplight.io>

--- src/utils/node.ts
+++ src/utils/node.ts
@@ -20,7 +20,7 @@ export function matchesNodeFilter(branchNode: IBranchNode, filter?: INodeFilter)
   return isMatch;
 }
 
-export const IRRELEVANT_NODE_TYPES = Object.freeze([NodeType.Generic, NodeType.Unknown]).map(String);
+export const IRRELEVANT_NODE_TYPES = Object.freeze([NodeType.Generic, NodeType.Unknown].map(String));
 // could be camel case or pascal case, whatever we use in elements
 
 export function isIrrelevantNodeType(type: NodeType | string) {
