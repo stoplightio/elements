0ceead3cac0b6004cc12c9396282665d477b86f9
Thomas Pytleski <pytlesk4@gmail.com>
2020-06-25 23:11:21 -0500

    fix: resolve bundle dependencies (#451)

--- src/containers/Docs.tsx
+++ src/containers/Docs.tsx
@@ -1,15 +1,35 @@
+import { pointerToPath } from '@stoplight/json';
+import { SchemaTreeRefDereferenceFn } from '@stoplight/json-schema-viewer';
+import { NodeType } from '@stoplight/types';
+import { get, isObject } from 'lodash';
 import * as React from 'react';
 import { useQuery } from 'urql';
 
-import { Docs as DocsComponent } from '../components/Docs';
+import { ParsedDocs, useParsedData } from '../components/Docs';
 import { DocsSkeleton } from '../components/Docs/Skeleton';
 import { bundledBranchNode } from '../graphql/BranchNodeBySlug';
-import { ActiveInfoContext } from './Provider';
+import { ActiveInfoContext, InlineRefResolverContext } from './Provider';
 
 export interface IDocsProps {
   className?: string;
   node?: string;
 }
+
+const DocsPopup = React.memo<{ nodeType: NodeType; nodeData: unknown; className?: string }>(
+  ({ nodeType, nodeData, className }) => {
+    const document = useParsedData(nodeType, nodeData);
+    const inlineRefResolver = React.useCallback<SchemaTreeRefDereferenceFn>(
+      ({ pointer }, _, schema) =>
+        pointer === null ? null : get(isObject(document) ? document : schema, pointerToPath(pointer)),
+      [document],
+    );
+    return (
+      <InlineRefResolverContext.Provider value={inlineRefResolver}>
+        <ParsedDocs className={className} nodeType={nodeType} nodeData={document} />
+      </InlineRefResolverContext.Provider>
+    );
+  },
+);
 
 export const Docs = ({ className, node }: IDocsProps) => {
   const info = React.useContext(ActiveInfoContext);
@@ -26,11 +46,12 @@ export const Docs = ({ className, node }: IDocsProps) => {
   if (fetching || !result) {
     return <DocsSkeleton />;
   }
+
   return (
-    <DocsComponent
-      className={className}
+    <DocsPopup
       nodeType={result.bundledBranchNode.type}
       nodeData={result.bundledBranchNode.data}
+      className={className}
     />
   );
 };
