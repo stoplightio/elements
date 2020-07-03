8df5a8503329399e5bd4abf47e69acfdb4ab9310
Marcell Toth <marcell@stoplight.io>
2020-07-03 14:01:30 +0200

    feat: install lerna & move current elements to under packages

--- packages/elements/.storybook/webpack.config.js
+++ packages/elements/.storybook/webpack.config.js
@@ -1,4 +1,7 @@
 const defaultConfig = require('@stoplight/storybook-config/webpack.config');
+const inliner = require('sass-inline-svg');
+const path = require('path');
+const { last } = require('lodash');
 
 module.exports = (baseConfig, env, config) => {
   config = defaultConfig(baseConfig, env, config);
@@ -10,6 +13,14 @@ module.exports = (baseConfig, env, config) => {
     ...config.node,
     fs: 'empty',
   };
+
+  last(last(config.module.rules).use).options.sassOptions.functions['svg-icon'] = inliner(
+    path.resolve('../', '../', 'node_modules', '@stoplight', 'ui-kit', 'styles', 'icons'),
+    {
+      optimize: true,
+      encodingFormat: 'uri',
+    },
+  );
 
   return config;
 };
