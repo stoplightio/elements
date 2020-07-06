447bf5e6ffa2445e5a35ec297064a61fca5475b5
Marcell Toth <marcell@stoplight.io>
2020-07-06 14:26:58 +0200

    feat: implement attribute handling

--- packages/web-components/src/__stories__/components/SchemaViewer.tsx
+++ packages/web-components/src/__stories__/components/SchemaViewer.tsx
@@ -3,3 +3,14 @@ import '../../index';
 export default { title: 'Schema Viewer' };
 
 export const defaultState = () => '<stoplight-component-schema-viewer/>';
+
+export const withSchemaAsAttribute = () =>
+  `<stoplight-component-schema-viewer 
+    schema='{ 
+      "type": "object", 
+      "properties": { 
+          "firstName": { "type": "string" }, 
+          "lastName": { "type": "string" } 
+      }, 
+      "required": ["firstName"] 
+    }'/>`;
--- packages/web-components/src/components/SchemaViewer.tsx
+++ packages/web-components/src/components/SchemaViewer.tsx
@@ -1,15 +1,52 @@
-import { SchemaViewer } from '@stoplight/elements/dist/components/SchemaViewer';
+import { SchemaViewer, ISchemaViewerProps } from '@stoplight/elements/dist/components/SchemaViewer';
+import { safeStringify, safeParse } from '@stoplight/json';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { JSONSchema4, JSONSchema6, JSONSchema7 } from 'json-schema';
+
+type JSONSchema = JSONSchema4 | JSONSchema6 | JSONSchema7;
+
+const defaultProps: ISchemaViewerProps = {
+  schema: { type: 'any' },
+};
 
 export class SchemaViewerComponentElement extends HTMLElement {
   private _mountPoint: HTMLElement | undefined;
+  private readonly _props: Partial<ISchemaViewerProps> = {};
+
+  static get observedAttributes() {
+    return ['schema'];
+  }
+
+  public get schema(): JSONSchema | undefined {
+    return this._props.schema;
+  }
+
+  public set schema(value: JSONSchema | undefined) {
+    this._props.schema = value;
+    this.renderComponent();
+    if (!value) {
+      this.removeAttribute('schema');
+    }
+    this.setAttribute('schema', safeStringify(value));
+  }
 
   connectedCallback() {
+    const schemaFromAttr = safeParse(this.getAttribute('schema') ?? '');
+    if (schemaFromAttr) {
+      this._props.schema = schemaFromAttr;
+    }
+
     this._mountPoint = document.createElement('div');
     this.appendChild(this._mountPoint);
 
-    ReactDOM.render(<SchemaViewer schema={{ type: 'string' }} />, this._mountPoint);
+    this.renderComponent();
+  }
+
+  private renderComponent() {
+    if (this._mountPoint) {
+      ReactDOM.render(<SchemaViewer {...defaultProps} {...this._props} />, this._mountPoint);
+    }
   }
 
   disconnectedCallback() {
