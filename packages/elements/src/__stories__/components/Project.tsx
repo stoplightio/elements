dd9355d28ab0ce9761e0a0abb03f670dcfe51dd4
Jakub Jankowski <jakubj@stoplight.io>
2020-07-27 15:16:25 +0200

    chore: add storybook

---
+++ packages/elements/src/__stories__/components/Project.tsx
@@ -0,0 +1,23 @@
+import { text, withKnobs } from '@storybook/addon-knobs';
+import { boolean } from '@storybook/addon-knobs/react';
+import { storiesOf } from '@storybook/react';
+import cn from 'classnames';
+import * as React from 'react';
+import StoryRouter from 'storybook-react-router';
+
+import { StoplightProject } from '../../containers/StoplightProject';
+
+const darkMode = () => boolean('dark mode', false);
+const workspace = () => text('workspace', 'http://burrito.localhost:8080');
+const project = () => text('project', 'studio-demo');
+
+storiesOf('components/Project', module)
+  .addDecorator(StoryRouter())
+  .addDecorator(withKnobs())
+  .add('Playground', () => {
+    return (
+      <div className={cn('p-10', { 'bp3-dark bg-gray-8': darkMode() })}>
+        <StoplightProject workspace={workspace()} project={project()} />
+      </div>
+    );
+  });
