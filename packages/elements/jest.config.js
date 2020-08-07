12f2c24f7f20e233d72a1c1a2cdd17222d1b3024
Jakub Rożek <jakub@stoplight.io>
2020-08-07 18:18:42 +0200

    chore: mock

--- packages/elements/jest.config.js
+++ packages/elements/jest.config.js
@@ -4,18 +4,4 @@ module.exports = {
   setupFilesAfterEnv: ['./setupTests.ts'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
   coveragePathIgnorePatterns: ['__tests__', '__fixtures__', '__stories__'],
-  moduleNameMapper: {
-    'use-resize-observer': 'use-resize-observer/dist/bundle.esm.js',
-  },
-  transformIgnorePatterns: ['node_modules/(?!use-resize-observer/)'],
-  globals: {
-    'ts-jest': {
-      tsConfig: {
-        allowJs: true,
-      },
-      diagnostics: {
-        ignoreCodes: [151001],
-      },
-    },
-  },
 };
