d860cbd8b1fb673d1fa076f4da6b1edfaac5ad70
Jakub Rożek <jakub@stoplight.io>
2020-08-07 17:19:18 +0200

    test: do not mock use-resize-observer

--- packages/elements/setupTests.ts
+++ packages/elements/setupTests.ts
@@ -1,6 +1,23 @@
+type WindowWithResizeObserver = Window &
+  typeof globalThis & {
+    ResizeObserver(
+      entries: any[],
+    ): {
+      observe(el: HTMLElement): void;
+      unobserve(el: HTMLElement): void;
+      disconnect(): void;
+    };
+  };
+
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
 
 process.env.TZ = 'UTC';
+
+(window as WindowWithResizeObserver).ResizeObserver = jest.fn(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
