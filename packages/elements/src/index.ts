88d6c53ac1fdaf7baace39b189fba95d5c7a8222
Jakub Jankowski <jakubj@stoplight.io>
2020-08-11 11:11:21 +0200

    fix: exports

--- packages/elements/src/index.ts
+++ packages/elements/src/index.ts
@@ -4,5 +4,5 @@ export * from './containers/Provider';
 export * from './containers/TableOfContents';
 export * from './containers/TryIt';
 export * from './containers/StoplightProject';
-export * from './containers/APIComponent';
+export * from './containers/API';
 export { SchemaViewer, ISchemaViewerProps } from './components/SchemaViewer';
