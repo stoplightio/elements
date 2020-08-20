deb7f8ff7a1966eb06f24a0cc16839f5c33f117d
Jakub Jankowski <jakubj@stoplight.io>
2020-08-20 23:10:48 +0200

    feat: api web component

--- packages/elements-web-components/src/index.ts
+++ packages/elements-web-components/src/index.ts
@@ -1,4 +1,5 @@
-import { SchemaViewerComponentElement, StoplightProjectElement } from './components';
+import { ApiElement, SchemaViewerComponentElement, StoplightProjectElement } from './components';
 
 window.customElements.define('stoplight-component-schema-viewer', SchemaViewerComponentElement);
 window.customElements.define('stoplight-project', StoplightProjectElement);
+window.customElements.define('api-component', ApiElement);
