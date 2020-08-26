0cd187aa4b944d60e4cee6c020c2959a996c9bdd
Marcell Toth <marcell@stoplight.io>
2020-08-26 12:43:35 +0200

    feat: add API example

--- examples/angular/src/app/docs/docs.component.ts
+++ examples/angular/src/app/docs/docs.component.ts
@@ -1,10 +1,9 @@
 import { Component, NgZone } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-docs',
   templateUrl: './docs.component.html',
-  styleUrls: ['../app.component.css'],
 })
 export class DocsComponent {
   constructor(private ngZone: NgZone, private router: Router) {}
