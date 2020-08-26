2acd898c75239b1afee72b525d1029350796c5b3
Marcell Toth <marcell@stoplight.io>
2020-08-26 12:37:32 +0200

    feat: more sane routing configuration using a basePath

--- examples/angular/src/app/app-routing.module.ts
+++ examples/angular/src/app/app-routing.module.ts
@@ -7,8 +7,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'docs', component: DocsComponent },
-  { path: '**', component: DocsComponent },
+  { path: 'workspace-docs', component: DocsComponent },
 ];
 
 @NgModule({
