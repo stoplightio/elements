7089c5c5bb4af2e079a14871eec68b5eba77701c
Marcell Toth <marcell@stoplight.io>
2020-09-02 14:14:37 +0200

    chore: remove extra props from Styled

--- packages/elements/src/styled.tsx
+++ packages/elements/src/styled.tsx
@@ -2,11 +2,7 @@ import * as React from 'react';
 
 import { getDisplayName } from './hoc/utils';
 
-export const Styled: React.FC = ({ children, ...props }) => (
-  <div {...props} className="sl-elements">
-    {children}
-  </div>
-);
+export const Styled: React.FC = ({ children }) => <div className="sl-elements">{children}</div>;
 
 export function withStyles<T>(Component: React.ComponentType<T>): React.ComponentType<T> {
   const Inner: React.FC<T> = props => {
