6cf70bd4f82fbfaf82760e874bc82df0ab9c4f90
Marcell Toth <marcell@stoplight.io>
2020-09-03 17:30:42 +0200

    feat: only export public exports

--- packages/elements/src/index.ts
+++ packages/elements/src/index.ts
@@ -1,27 +1,2 @@
-// Public exports
-
 export { StoplightProject } from './containers/StoplightProject';
 export { API } from './containers/API';
-
-// Ninja requirements
-
-export { Provider, IProvider } from './containers/Provider';
-export { TableOfContents, ITableOfContents } from './containers/TableOfContents';
-export { Changelog, IChangelogProps } from './components/Changelog';
-export { Docs, IDocsProps, IDocsComponentProps, IParsedDocsProps, ParsedDocs, DocsSkeleton } from './components/Docs';
-export { TryIt, ITryItProps } from './components/TryIt';
-export { RequestEditor, RequestEndpoint, RequestMakerProvider, ResponseViewer } from './components/RequestMaker';
-export {
-  InboundDependencies,
-  IInboundDependencies,
-  OutboundDependencies,
-  IOutboundDependencies,
-} from './components/Dependencies';
-
-export { useComponents, defaultComponents } from './hooks/useComponents';
-export { useParsedData } from './hooks/useParsedData';
-export { useParsedValue } from './hooks/useParsedValue';
-export { useRequestMaker } from './hooks/useRequestMaker';
-export { useComputeToc } from './hooks/useComputeToc';
-
-export * from './types';
