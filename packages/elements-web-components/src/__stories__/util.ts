8cb5aa8fe5b4b4050a35f925042bdb3ec370e1d0
Marcell Toth <marcell@stoplight.io>
2020-09-15 12:27:46 +0200

    fix: typings

--- packages/elements-web-components/src/__stories__/util.ts
+++ packages/elements-web-components/src/__stories__/util.ts
@@ -1,6 +1,8 @@
-import { Story } from '@storybook/react';
+import { BaseStory } from '@storybook/addons';
+import { Args as DefaultArgs } from '@storybook/addons/dist/types';
+import { StoryFnHtmlReturnType } from '@storybook/web-components/dist/client/preview/types';
 
-export const createTemplate = (elementName: string): Story => {
+export const createTemplate = (elementName: string): BaseStory<DefaultArgs, StoryFnHtmlReturnType> => {
   return (args: any) => {
     const element = document.createElement(elementName);
     Object.assign(element, args);
