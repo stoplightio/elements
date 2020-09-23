4144e8e032d47a2b20567d6be46e29adbbcc9ec5
mmiask <mmiask92@gmail.com>
2020-09-23 17:17:19 +0200

    fix: addressed review comment

--- packages/elements/src/__stories__/components/RequestMaker.tsx
+++ packages/elements/src/__stories__/components/RequestMaker.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import { operation, request } from '../../__fixtures__/operations/simple';
 import { RequestMaker } from '../../components/RequestMaker';
 
-storiesOf('components/StoplightProject', module)
+storiesOf('components/RequestMaker', module)
   .addDecorator(withKnobs())
   .add('Request Maker', () => {
     return (
