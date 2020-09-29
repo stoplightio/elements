4b2222e6ee98ee0414f8363f2728b2825549bc2c
Marcell Toth <marcell@stoplight.io>
2020-09-29 16:40:40 +0200

    fix: release all

--- release.sh
+++ release.sh
@@ -3,12 +3,12 @@ if yarn lerna changed
 then
   if [[ $CIRCLE_BRANCH == "master" ]]
   then
-    yarn lerna publish --conventional-commits --force-publish=elements,elements-web-components --yes --dist-tag latest
+    yarn lerna publish --conventional-commits --force-publish=@stoplight/elements,@stoplight/elements-web-components --yes --dist-tag latest
   elif [[ $CIRCLE_BRANCH == "beta" ]]
   then
-    yarn lerna publish --conventional-commits --force-publish=elements,elements-web-components --yes --dist-tag beta --preid beta
+    yarn lerna publish --conventional-commits --force-publish=@stoplight/elements,@stoplight/elements-web-components --yes --dist-tag beta --preid beta
   else
-    yarn lerna publish --conventional-commits --force-publish=elements,elements-web-components --yes --dist-tag alpha --preid alpha
+    yarn lerna publish --conventional-commits --force-publish=@stoplight/elements,@stoplight/elements-web-components --yes --dist-tag alpha --preid alpha
   fi
 else
   echo "No changes, skipping publish"
