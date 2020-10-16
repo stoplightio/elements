05fa3628bf966186b9b65c976f9b91080389f64f
Mateusz Podlasin <mateusz@stoplight.io>
2020-10-16 16:12:36 +0200

    feat: allow to provide api document directly in API web component

--- packages/elements-web-components/src/__stories__/Api.ts
+++ packages/elements-web-components/src/__stories__/Api.ts
@@ -1,5 +1,8 @@
 import '../index';
 
+import { zoomApiYaml } from '@stoplight/elements/src/__fixtures__/api-descriptions/zoomApiYaml';
+import { parse } from '@stoplight/yaml';
+
 import { createTemplate } from './util';
 
 const Template = createTemplate('elements-api');
@@ -16,3 +19,15 @@ ZoomApi.args = {
   apiDescriptionUrl: 'https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/zoom.yaml',
 };
 ZoomApi.storyName = 'Complex API with inline `$ref`s';
+
+export const APIWithYamlProvidedDirectly = Template.bind({});
+APIWithYamlProvidedDirectly.args = {
+  apiDescriptionDocument: zoomApiYaml,
+};
+APIWithYamlProvidedDirectly.storyName = 'API With Yaml Provided Directly';
+
+export const APIWithJSONProvidedDirectly = Template.bind({});
+APIWithJSONProvidedDirectly.args = {
+  apiDescriptionDocument: JSON.stringify(parse(zoomApiYaml), null, '  '),
+};
+APIWithJSONProvidedDirectly.storyName = 'API With JSON Provided Directly';
--- packages/elements-web-components/src/components.ts
+++ packages/elements-web-components/src/components.ts
@@ -14,6 +14,7 @@ export const StoplightProjectElement = createElementClass(StoplightProject, {
 
 export const ApiElement = createElementClass(API, {
   apiDescriptionUrl: { type: 'string', defaultValue: '' },
+  apiDescriptionDocument: { type: 'string', defaultValue: '' },
   basePath: { type: 'string' },
   router: { type: 'string' },
   layout: { type: 'string' },
