9be9cbcc04159d16265a982c2bc46c61baa6b022
Mateusz Podlasin <mateusz@stoplight.io>
2020-10-30 13:48:00 +0100

    feat: change typings in badge component

--- packages/elements/src/components/Docs/HttpOperation/Badges.tsx
+++ packages/elements/src/components/Docs/HttpOperation/Badges.tsx
@@ -4,22 +4,17 @@ import { Tag } from '@stoplight/ui-kit';
 import cs from 'classnames';
 import React from 'react';
 
-export const Badge = ({
-  icon,
-  children,
-  className,
-}: {
+export const Badge: React.FC<{
   icon?: IconDefinition;
   className?: string;
-  children: React.ReactNode;
-}) => (
+}> = ({ icon, className, children }) => (
   <Tag className={cs('text-md p-1 font-semibold mt-1', className)} round>
     {icon && <FontAwesomeIcon className="mr-2" icon={icon} />}
     <span>{children}</span>
   </Tag>
 );
 
-export const DeprecatedBadge = () => (
+export const DeprecatedBadge: React.FC = () => (
   <Badge icon={faExclamationCircle} className="bg-orange-6">
     Deprecated
   </Badge>
