f8e5daa25d58c1ba28f9edb819bcdd6a9bd1c4d2
Mateusz Podlasin <mateusz@stoplight.io>
2020-11-02 13:45:20 +0100

    feat: add tooltip to deprecated badge

--- packages/elements/src/components/Docs/HttpOperation/Badges.tsx
+++ packages/elements/src/components/Docs/HttpOperation/Badges.tsx
@@ -1,21 +1,28 @@
 import { faExclamationCircle, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Tag } from '@stoplight/ui-kit';
+import { Position, Tag, Tooltip } from '@stoplight/ui-kit';
 import cs from 'classnames';
 import React from 'react';
 
 export const Badge: React.FC<{
   icon?: IconDefinition;
   className?: string;
-}> = ({ icon, className, children }) => (
-  <Tag className={cs('text-md p-1 font-semibold mt-1', className)} round>
-    {icon && <FontAwesomeIcon className="mr-2" icon={icon} />}
-    <span>{children}</span>
-  </Tag>
+  tooltip?: string;
+}> = ({ icon, className, children, tooltip }) => (
+  <Tooltip position={Position.BOTTOM_RIGHT} content={tooltip}>
+    <Tag className={cs('text-md p-1 font-semibold mt-1', className)} round>
+      {icon && <FontAwesomeIcon className="mr-2" icon={icon} />}
+      <span>{children}</span>
+    </Tag>
+  </Tooltip>
 );
 
 export const DeprecatedBadge: React.FC = () => (
-  <Badge icon={faExclamationCircle} className="bg-orange-6">
+  <Badge
+    icon={faExclamationCircle}
+    className="bg-orange-6"
+    tooltip="This operation has been marked as deprecated, which means it could be removed at some point in the future."
+  >
     Deprecated
   </Badge>
 );
