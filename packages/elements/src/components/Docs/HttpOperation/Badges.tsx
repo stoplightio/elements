8f9256480cfdf931a15ee87fb3844c521ec3a40a
Mateusz Podlasin <mateusz@stoplight.io>
2020-11-02 14:35:07 +0100

    feat: remove tooltip from badge abstraction

--- packages/elements/src/components/Docs/HttpOperation/Badges.tsx
+++ packages/elements/src/components/Docs/HttpOperation/Badges.tsx
@@ -7,22 +7,20 @@ import React from 'react';
 export const Badge: React.FC<{
   icon?: IconDefinition;
   className?: string;
-  tooltip?: string;
-}> = ({ icon, className, children, tooltip }) => (
-  <Tooltip position={Position.BOTTOM_RIGHT} content={tooltip}>
-    <Tag className={cs('text-md p-1 font-semibold mt-1', className)} round>
-      {icon && <FontAwesomeIcon className="mr-2" icon={icon} />}
-      <span>{children}</span>
-    </Tag>
-  </Tooltip>
+}> = ({ icon, className, children }) => (
+  <Tag className={cs('text-md p-1 font-semibold mt-1', className)} round>
+    {icon && <FontAwesomeIcon className="mr-2" icon={icon} />}
+    <span>{children}</span>
+  </Tag>
 );
 
 export const DeprecatedBadge: React.FC = () => (
-  <Badge
-    icon={faExclamationCircle}
-    className="bg-orange-6"
-    tooltip="This operation has been marked as deprecated, which means it could be removed at some point in the future."
+  <Tooltip
+    position={Position.BOTTOM_RIGHT}
+    content="This operation has been marked as deprecated, which means it could be removed at some point in the future."
   >
-    Deprecated
-  </Badge>
+    <Badge icon={faExclamationCircle} className="bg-orange-6">
+      Deprecated
+    </Badge>
+  </Tooltip>
 );
