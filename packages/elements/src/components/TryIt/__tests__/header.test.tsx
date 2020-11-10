b2dc2a62d58047d511e8799001b22dca2fedf94a
mmiask <mmiask92@gmail.com>
2020-11-10 15:19:30 +0100

    feat: finished unit tests for Try It Out header
    added @testing-library/dom
    added @testing-library/user-event
    added a test case

--- packages/elements/src/components/TryIt/__tests__/header.test.tsx
+++ packages/elements/src/components/TryIt/__tests__/header.test.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 
 import { TryItHeader } from '../header';
@@ -9,4 +10,25 @@ test('Try It header renders correctly', () => {
   render(<TryItHeader />);
   const elem = screen.getByTestId('try-it-header');
   expect(elem.textContent).toBe(' Try It Out');
+  expect(elem.innerHTML).toContain('data-icon="magic"');
+  expect(elem.innerHTML).toContain('data-icon="question-circle"');
+});
+
+test('When hovered over question circle icon, Try It Out details are displayed', async () => {
+  render(<TryItHeader />);
+  const elem = screen.getByTestId('try-it-header');
+  const icon = elem.querySelector('[data-icon="question-circle"]') as Element;
+
+  userEvent.hover(icon);
+
+  expect(
+    await screen.findByText(
+      "Send HTTP requests to your API, or one of our mock servers, to see how it's going to respond. View the docs",
+    ),
+  ).toBeInTheDocument();
+
+  expect(await (await screen.findByText('here.')).closest('a')).toHaveAttribute(
+    'href',
+    'https://meta.stoplight.io/docs/studio/docs/Design-and-Modeling/05-request-maker.md',
+  );
 });
--- packages/elements/src/components/TryIt/header.tsx
+++ packages/elements/src/components/TryIt/header.tsx
@@ -4,8 +4,8 @@ import { Popover, Position } from '@stoplight/ui-kit';
 import * as React from 'react';
 
 export const TryItHeader: React.FC = () => (
-  <h2 className="opacity-75 ml-1 mt-10 mb-8 font-medium flex items-center">
-    <div data-testid="try-it-header" className="flex-1">
+  <h2 data-testid="try-it-header" className="opacity-75 ml-1 mt-10 mb-8 font-medium flex items-center">
+    <div className="flex-1">
       <FontAwesomeIcon icon={faMagic} className="mr-4" /> Try It Out
     </div>
     <Popover
