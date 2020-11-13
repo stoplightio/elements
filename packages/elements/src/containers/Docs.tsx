abff5a212df4f53649a346e567e1755ec13e51d0
Marcell Toth <marcell@stoplight.io>
2020-11-13 16:21:42 +0100

    chore: eliminate TryItProvider and DocsProvider

--- packages/elements/src/containers/Docs.tsx
+++ packages/elements/src/containers/Docs.tsx
@@ -9,15 +9,11 @@ import { InlineRefResolverProvider } from '../context/InlineRefResolver';
 import { useParsedData } from '../hooks/useParsedData';
 import { usePlatformApi } from '../hooks/usePlatformApi';
 import { BundledBranchNode } from '../types';
-import { ActiveInfoContext, IProvider, Provider } from './Provider';
+import { ActiveInfoContext } from './Provider';
 
 export interface IDocsProps {
   className?: string;
   node?: string;
-}
-
-interface IDocsProvider extends IProvider {
-  className?: string;
 }
 
 const DocsPopup = React.memo<{ nodeType: NodeType; nodeData: unknown; className?: string }>(
@@ -62,29 +58,4 @@ export const Docs = ({ className, node }: IDocsProps) => {
   }
 
   return <DocsPopup nodeType={result.type} nodeData={result.data} className={className} />;
-};
-
-export const DocsProvider: React.FC<IDocsProvider> = ({
-  host,
-  workspace,
-  project,
-  branch,
-  node,
-  components,
-  className,
-  authToken,
-}) => {
-  return (
-    <Provider
-      host={host}
-      workspace={workspace}
-      project={project}
-      branch={branch}
-      components={components}
-      node={node}
-      authToken={authToken}
-    >
-      <Docs className={className} node={node} />
-    </Provider>
-  );
 };
--- packages/elements/src/containers/TryIt.tsx
+++ packages/elements/src/containers/TryIt.tsx
@@ -1,21 +1,17 @@
 import { NodeType } from '@stoplight/types';
 import * as React from 'react';
 
-import { DocsSkeleton } from '../components/Docs/Skeleton';
+import { DocsSkeleton } from '../components/Docs';
 import { TryIt as TryItComponent } from '../components/TryIt';
 import { TryItHeader } from '../components/TryIt/header';
 import { useDereferencedData } from '../hooks/useDereferencedData';
 import { useActionsApi, usePlatformApi } from '../hooks/usePlatformApi';
 import { BundledBranchNode } from '../types';
-import { ActiveInfoContext, IProvider, Provider } from './Provider';
+import { ActiveInfoContext } from './Provider';
 
 export interface ITryItProps {
   className?: string;
   node?: string;
-}
-
-interface ITryItProvider extends IProvider {
-  className?: string;
 }
 
 type MockUrlResult = { servicePath: string; operationPath?: string; id: number };
@@ -78,30 +74,5 @@ export const TryIt = ({ className, node }: ITryItProps) => {
         mockUrl={mockUrlResult?.servicePath}
       />
     </>
-  );
-};
-
-export const TryItProvider: React.FC<ITryItProvider> = ({
-  host,
-  workspace,
-  project,
-  branch,
-  node,
-  components,
-  className,
-  authToken,
-}) => {
-  return (
-    <Provider
-      host={host}
-      workspace={workspace}
-      project={project}
-      branch={branch}
-      components={components}
-      node={node}
-      authToken={authToken}
-    >
-      <TryIt className={className} node={node} />
-    </Provider>
   );
 };
