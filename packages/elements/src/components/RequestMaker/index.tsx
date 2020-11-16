5a7b519d634994c1d3af5609b977345639f286ad
Marcell Toth <marcell@stoplight.io>
2020-11-16 10:23:23 +0100

    chore: remove root RequestMaker component
    It was unused both in elements and in platform-internal

--- packages/elements/src/components/RequestMaker/index.tsx
+++ packages/elements/src/components/RequestMaker/index.tsx
@@ -1,36 +1,4 @@
-import { IHttpOperation, IHttpRequest } from '@stoplight/types';
-import * as React from 'react';
-
-import { RequestMakerProvider } from '../../hooks/useRequestMakerStore';
-import { createRequestMakerStore } from '../../utils/createRequestMakerStore';
-import { RequestEditor, RequestEndpoint } from './Request';
-import { ResponseViewer } from './Response';
-
-export * from './Request';
-export * from './Response';
-
-type RequestMakerProps =
-  | {
-      request: Partial<IHttpRequest>;
-      operation?: unknown;
-    }
-  | {
-      operation: Partial<IHttpOperation>;
-      request?: undefined;
-    };
-
-export const RequestMaker: React.FC<RequestMakerProps> = props => {
-  const input = props.request || props.operation;
-  const store = React.useMemo(() => createRequestMakerStore(input), [input]);
-  return (
-    <RequestMakerProvider value={store}>
-      <RequestEndpoint />
-
-      <RequestEditor />
-
-      <ResponseViewer />
-    </RequestMakerProvider>
-  );
-};
-
-export { RequestMakerProvider };
+export { RequestMakerProvider } from '../../hooks/useRequestMakerStore';
+export { createRequestMakerStore } from '../../utils/createRequestMakerStore';
+export { RequestEditor, RequestEndpoint } from './Request';
+export { ResponseViewer } from './Response';
