309a319e4029d47ba3f3d3cf0e9c9bbfa5e05e29
Marcell Toth <marcell@stoplight.io>
2020-11-28 20:39:53 +0100

    chore: prepare release

--- release.sh
+++ release.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 if yarn lerna changed
 then
-  if [[ $CIRCLE_BRANCH == "master" ]]
-  then
-    yarn lerna publish --conventional-commits --force-publish=@stoplight/elements,@stoplight/elements-web-components --yes --dist-tag latest
-  elif [[ $CIRCLE_BRANCH == "beta" ]]
-  then
-    yarn lerna publish --conventional-commits --force-publish=@stoplight/elements,@stoplight/elements-web-components --yes --dist-tag beta --preid beta
-  else
-    yarn lerna publish --conventional-commits --force-publish=@stoplight/elements,@stoplight/elements-web-components --yes --dist-tag alpha --preid alpha
-  fi
+  yarn lerna publish --conventional-commits --force-publish=@stoplight/elements,@stoplight/elements-web-components --yes --dist-tag latest
 else
   echo "No changes, skipping publish"
 fi
