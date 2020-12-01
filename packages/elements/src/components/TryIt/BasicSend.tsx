f63ffe0cfef3c30ae05354732871f3a1730d6a6d
Marcell Toth <marcell@stoplight.io>
2020-12-01 13:29:30 +0100

    feat: use some mosaic pieces (#737)
    * feat: add mosaic
    
    * feat: attempt to use this piece of happiness
    
    * chore: apply PR feedback
    
    * feat: add new CodeViewer
    
    * fix: styling
    
    BREAKING CHANGE: new CSS injection requirements
    
    * fix: disable button when loading

--- packages/elements/src/components/TryIt/BasicSend.tsx
+++ packages/elements/src/components/TryIt/BasicSend.tsx
@@ -1,7 +1,11 @@
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button, Panel, Text } from '@stoplight/mosaic';
+import { CodeViewer } from '@stoplight/mosaic-code-viewer';
 import { IHttpOperation } from '@stoplight/types';
-import { Button, Card, CodeViewer } from '@stoplight/ui-kit';
 import * as React from 'react';
 
+import { HttpCodeDescriptions } from '../../constants';
 import { getHttpCodeColor } from '../../utils/http';
 
 interface BasicSendProps {
@@ -10,7 +14,6 @@ interface BasicSendProps {
 
 interface ResponseState {
   status: number;
-  statusText: string;
   bodyText: string;
 }
 
@@ -23,13 +26,14 @@ export const BasicSend: React.FC<BasicSendProps> = ({ httpOperation }) => {
   const [loading, setLoading] = React.useState<boolean>(false);
   const server = httpOperation.servers?.[0]?.url;
 
+  if (!server) return null;
+
   const sendRequest = async () => {
     try {
       setLoading(true);
       const response = await fetch(server + httpOperation.path, { method: httpOperation.method });
       setResponse({
         status: response.status,
-        statusText: response.statusText,
         bodyText: await response.text(),
       });
     } catch (e) {
@@ -39,44 +43,53 @@ export const BasicSend: React.FC<BasicSendProps> = ({ httpOperation }) => {
     }
   };
 
-  if (!server) return null;
   return (
-    <Card className="p-0">
-      <p className="flex flex-row bg-gray-7 font-mono rounded-t-lg py-2 pl-4">
-        <div className="text-white uppercase pr-2">{httpOperation.method}</div>
-        <div className="text-gray-2">{httpOperation.path}</div>
-      </p>
-      <div className="bg-gray-5 px-4 py-3">
-        <Button className="bp3-small rounded" loading={loading} intent="primary" onClick={sendRequest}>
-          Send
-        </Button>
-      </div>
-      {response && !('error' in response) && (
-        <div className="font-sans font-light">
-          <div className="bg-gray-6 text-gray-2 px-4 py-3">Response</div>
-          <div className="bg-gray-5">
-            <div className={`p-4 text-${getHttpCodeColor(response.status)}`}>
-              {`${response.status} ${response.statusText}`}
-            </div>
-            {response.bodyText && (
-              <CodeViewer
-                showLineNumbers
-                value={response.bodyText || ''}
-                language="html"
-                className="pr-8 pb-4 font-sans whitespace-pre-wrap break-words"
-              ></CodeViewer>
-            )}
+    <div>
+      <Panel isCollapsible={false} className="p-0">
+        <Panel.Titlebar bg="canvas-300">
+          <div role="heading" className="sl-font-bold">
+            <Text color="primary">PUT</Text>
+            <Text ml={2}>/todos/todoId</Text>
           </div>
+        </Panel.Titlebar>
+        <div className="m-4">
+          <Button appearance="primary" loading={loading} disabled={loading} onClick={sendRequest}>
+            Send
+          </Button>
         </div>
-      )}
-      {response && 'error' in response && (
-        <div className="font-sans font-light">
-          <div className="bg-gray-6 text-gray-2 px-4 py-3">Error</div>
-          <div className="bg-gray-5">
-            <div className="p-4">{response.error.message}</div>
-          </div>
-        </div>
-      )}
-    </Card>
+      </Panel>
+      {response && !('error' in response) && <BasicSendResponse response={response} />}
+      {response && 'error' in response && <BasicSendError state={response} />}
+    </div>
   );
 };
+
+const BasicSendResponse: React.FC<{ response: ResponseState }> = ({ response }) => (
+  <Panel defaultIsOpen>
+    <Panel.Titlebar>Response</Panel.Titlebar>
+    <Panel.Content>
+      <div>
+        <div className={`mb-3 text-${getHttpCodeColor(response.status)}`}>
+          {`${response.status} ${HttpCodeDescriptions[response.status] ?? ''}`}
+        </div>
+        {response.bodyText ? (
+          <CodeViewer className="language-html">{response.bodyText || ''}</CodeViewer>
+        ) : (
+          <p>
+            <FontAwesomeIcon icon={faExclamationCircle} className="mr-2" />
+            No response body returned
+          </p>
+        )}
+      </div>
+    </Panel.Content>
+  </Panel>
+);
+
+const BasicSendError: React.FC<{ state: ErrorState }> = ({ state }) => (
+  <Panel defaultIsOpen>
+    <Panel.Titlebar>Error</Panel.Titlebar>
+    <Panel.Content>
+      <p>{state.error.message}</p>
+    </Panel.Content>
+  </Panel>
+);
