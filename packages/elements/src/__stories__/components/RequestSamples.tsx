a3fa9b987c92893272e90c0db3c69de4756bbd79
Mateusz Podlasin <mateusz@stoplight.io>
2020-12-02 14:32:27 +0100

    feat: add knobs to playground example

--- packages/elements/src/__stories__/components/RequestSamples.tsx
+++ packages/elements/src/__stories__/components/RequestSamples.tsx
@@ -1,16 +1,19 @@
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
 import { RequestSamples } from '../../components/RequestSamples/RequestSamples';
+
+const requestType = () => text('requestType', 'cURL');
+const request = () => text('request', 'curl http://www.example.org:1234/');
 
 storiesOf('Internal/RequestSamples', module)
   .addDecorator(withKnobs({ escapeHTML: false }))
   .add('Playground', () => {
     return (
         <div className="m-5">
-            <RequestSamples requestType="cURL">
-                curl http://www.example.org:1234/
+            <RequestSamples requestType={requestType()}>
+                {request()}
             </RequestSamples>
         </div>
     );
