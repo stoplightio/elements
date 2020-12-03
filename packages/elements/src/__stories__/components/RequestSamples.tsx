f5d5837c9da897796b9ec5c1853991aee3feba5e
Mateusz Podlasin <mateusz@stoplight.io>
2020-12-03 13:54:49 +0100

    fix: fix linter errors

--- packages/elements/src/__stories__/components/RequestSamples.tsx
+++ packages/elements/src/__stories__/components/RequestSamples.tsx
@@ -1,13 +1,14 @@
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { text, withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import { Request } from 'har-format';
 import * as React from 'react';
+
 import { SupportedRequestType } from '../../components/RequestSamples/requestConverters';
 import { RequestSamples } from '../../components/RequestSamples/RequestSamples';
 
 const initialRequest = {
   url: 'https://google.com',
-  method: 'post'
+  method: 'post',
 } as Request;
 
 const requestType = () => text('requestType', 'cURL');
@@ -17,11 +18,8 @@ storiesOf('Internal/RequestSamples', module)
   .addDecorator(withKnobs({ escapeHTML: false }))
   .add('Playground', () => {
     return (
-        <div className="m-5">
-            <RequestSamples 
-              requestType={requestType() as SupportedRequestType} 
-              request={JSON.parse(request())} 
-            />
-        </div>
+      <div className="m-5">
+        <RequestSamples requestType={requestType() as SupportedRequestType} request={JSON.parse(request())} />
+      </div>
     );
   });
--- packages/elements/src/components/RequestSamples/RequestSamples.tsx
+++ packages/elements/src/components/RequestSamples/RequestSamples.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
 import { Panel } from '@stoplight/mosaic';
 import { CodeViewer } from '@stoplight/mosaic-code-viewer';
 import { Request } from 'har-format';
+import React from 'react';
+
 import { requestConverters, SupportedRequestType } from './requestConverters';
 
 interface RequestSamplesProps {
-    requestType: SupportedRequestType;
-    request: Request;
+  requestType: SupportedRequestType;
+  request: Request;
 }
 
 export const RequestSamples: React.FC<RequestSamplesProps> = ({ request, requestType }) => (
-    <Panel isCollapsible={false}>
-        <Panel.Titlebar>Request: {requestType}</Panel.Titlebar>
-        <Panel.Content p={0}>
-            <CodeViewer>
-                {requestConverters[requestType](request) || 'Generation failed'}
-            </CodeViewer>
-        </Panel.Content>
-    </Panel>
+  <Panel isCollapsible={false}>
+    <Panel.Titlebar>Request: {requestType}</Panel.Titlebar>
+    <Panel.Content p={0}>
+      <CodeViewer>{requestConverters[requestType](request) || 'Generation failed'}</CodeViewer>
+    </Panel.Content>
+  </Panel>
 );
