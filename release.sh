b5bf0d6e8cea4caaaeae407d7134ec492d34a95c
Marcell Toth <marcell@stoplight.io>
2020-12-07 11:48:41 +0100

    fix: force-publish elements-utils as well

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if yarn lerna changed
 then
-  yarn lerna publish --conventional-commits --force-publish=@stoplight/elements,@stoplight/elements-web-components --yes --dist-tag latest
+  yarn lerna publish --conventional-commits --force-publish --yes --dist-tag latest
 else
   echo "No changes, skipping publish"
 fi
