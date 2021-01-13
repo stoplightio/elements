3d8d518b376caaf3139fd6ecaa859408ddb8af28
mmiask <mmiask92@gmail.com>
2021-01-13 15:23:30 +0100

    chore: remove unused RequestMaker
    removed component files
    removed tests
    removed scss file
    removed stores
    removed hooks
    removed utils

--- packages/elements/src/components/RequestMaker/Request/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from './Body';
-export * from './Editor';
-export * from './Endpoint';
-export * from './Headers';
-export * from './Method';
-export * from './Parameters';
-export * from './Send';
--- packages/elements/src/components/RequestMaker/Response/index.tsx
+++
@@ -1,4 +0,0 @@
-export * from './Body';
-export * from './Headers';
-export * from './Status';
-export * from './Viewer';
