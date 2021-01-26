15b400272723ba9b553c327e1ad9bc81ea1c6afe
mmiask <mmiask92@gmail.com>
2021-01-26 13:39:18 +0100

    feat: add story to Storybook

--- packages/elements/src/__stories__/components/TryIt.tsx
+++ packages/elements/src/__stories__/components/TryIt.tsx
@@ -4,8 +4,10 @@ import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
 import { httpOperation } from '../../__fixtures__/operations/bundled-parameter';
+import examplesRequestBody from '../../__fixtures__/operations/examples-request-body';
 import multipartOperation from '../../__fixtures__/operations/multipart-formdata-post';
 import putOperation from '../../__fixtures__/operations/put-todos';
+import requestBodyOperation from '../../__fixtures__/operations/request-body';
 import { operation } from '../../__fixtures__/operations/simple-get';
 import urlEncodedOperation from '../../__fixtures__/operations/urlencoded-post';
 import { TryIt } from '../../components/TryIt';
@@ -22,4 +24,5 @@ storiesOf('Internal/TryIt', module)
   .add('Basic Send', () => <TryItWithPersistence httpOperation={operation} />)
   .add('Operation Parameters', () => <TryItWithPersistence httpOperation={putOperation} />)
   .add('Form data body - urlencoded', () => <TryItWithPersistence httpOperation={urlEncodedOperation} />)
-  .add('Form data body - multipart', () => <TryItWithPersistence httpOperation={multipartOperation} />);
+  .add('Form data body - multipart', () => <TryItWithPersistence httpOperation={multipartOperation} />)
+  .add('Example Request Body', () => <TryItWithPersistence httpOperation={examplesRequestBody} />);
