c384a2fa95810451dd85268cb90b9815ea6ce843
mmiask <mmiask92@gmail.com>
2021-01-26 13:39:00 +0100

    feat: add example operations

---
+++ packages/elements/src/__fixtures__/operations/examples-request-body.ts
@@ -0,0 +1,36 @@
+import { IHttpOperation } from '@stoplight/types';
+
+import requestBody from './request-body';
+
+const examplelessRequestBody = Object.create(requestBody);
+
+examplelessRequestBody.request.body.contents[0].examples = [
+  {
+    key: 'example-1',
+    value: {
+      name: 'Andrew',
+      age: 19,
+      trial: true,
+    },
+  },
+  {
+    key: 'example-2',
+    value: {
+      name: 'Jane',
+      age: 36,
+      trial: false,
+    },
+  },
+  {
+    key: 'example-3',
+    value: {
+      name: 'Max',
+      age: 23,
+      trial: true,
+    },
+  },
+];
+
+export const examplesRequestBody: IHttpOperation = examplelessRequestBody;
+
+export default examplesRequestBody;
---
+++ packages/elements/src/__fixtures__/operations/request-body.ts
@@ -0,0 +1,44 @@
+import { IHttpOperation } from '@stoplight/types';
+
+export const requestBody: IHttpOperation = {
+  id: '?http-operation-id?',
+  iid: 'GET_USERS',
+  method: 'get',
+  path: '/users',
+  summary: 'Get Users',
+  responses: [
+    {
+      code: '200',
+    },
+  ],
+  servers: [
+    {
+      url: 'https://todos.stoplight.io',
+    },
+  ],
+  request: {
+    body: {
+      contents: [
+        {
+          mediaType: 'application/json',
+          schema: {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+              },
+              age: {
+                type: 'number',
+              },
+              trial: {
+                type: 'boolean',
+              },
+            },
+          },
+        },
+      ],
+    },
+  },
+};
+
+export default requestBody;
