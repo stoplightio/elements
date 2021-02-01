ea0f50b710d9c2b61d432b8042fc8fa452d5f818
mmiask <mmiask92@gmail.com>
2021-02-01 11:46:37 +0100

    feat: add story to Storybook
    modify fixture

--- packages/elements/src/__fixtures__/operations/examples-request-body.ts
+++ packages/elements/src/__fixtures__/operations/examples-request-body.ts
@@ -1,36 +1,71 @@
 import { IHttpOperation } from '@stoplight/types';
 
-import requestBody from './request-body';
-
-const examplelessRequestBody = Object.create(requestBody);
-
-examplelessRequestBody.request.body.contents[0].examples = [
-  {
-    key: 'example-1',
-    value: {
-      name: 'Andrew',
-      age: 19,
-      trial: true,
+export const examplesRequestBody: IHttpOperation = {
+  id: '?http-operation-id?',
+  iid: 'GET_USERS',
+  method: 'get',
+  path: '/users',
+  summary: 'Get Users',
+  responses: [
+    {
+      code: '200',
+    },
+  ],
+  servers: [
+    {
+      url: 'https://todos.stoplight.io',
+    },
+  ],
+  request: {
+    body: {
+      contents: [
+        {
+          mediaType: 'application/json',
+          schema: {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+              },
+              age: {
+                type: 'number',
+              },
+              trial: {
+                type: 'boolean',
+                readOnly: true,
+              },
+            },
+          },
+          examples: [
+            {
+              key: 'example-1',
+              value: {
+                name: 'Andrew',
+                age: 19,
+                trial: true,
+              },
+            },
+            {
+              key: 'named example',
+              value: {
+                name: 'Jane',
+                age: 36,
+                trial: false,
+              },
+            },
+            {
+              key: 'example-3',
+              value: {
+                name: 'Max',
+                age: 23,
+                trial: true,
+              },
+            },
+          ],
+        },
+      ],
     },
   },
-  {
-    key: 'named example',
-    value: {
-      name: 'Jane',
-      age: 36,
-      trial: false,
-    },
-  },
-  {
-    key: 'example-3',
-    value: {
-      name: 'Max',
-      age: 23,
-      trial: true,
-    },
-  },
-];
-
-export const examplesRequestBody: IHttpOperation = examplelessRequestBody;
+};
 
 export default examplesRequestBody;
--- packages/elements/src/components/TryIt/TryIt.stories.tsx
+++ packages/elements/src/components/TryIt/TryIt.stories.tsx
@@ -1,8 +1,10 @@
 import { Meta, Story } from '@storybook/react';
 import * as React from 'react';
 
+import { examplesRequestBody } from '../../__fixtures__/operations/examples-request-body';
 import { httpOperation as multipartOperation } from '../../__fixtures__/operations/multipart-formdata-post';
 import { httpOperation as putTodosOperation } from '../../__fixtures__/operations/put-todos';
+import { requestBody } from '../../__fixtures__/operations/request-body';
 import { operation as simpleGetOperation } from '../../__fixtures__/operations/simple-get';
 import { httpOperation as urlEncodedOperation } from '../../__fixtures__/operations/urlencoded-post';
 import { TryIt, TryItProps } from './TryIt';
@@ -35,3 +37,15 @@ Multipart.args = {
   httpOperation: multipartOperation,
 };
 Multipart.storyName = 'multipart/form-data (with file upload)';
+
+export const RequestBodySchema = Template.bind({});
+RequestBodySchema.args = {
+  httpOperation: requestBody,
+};
+RequestBodySchema.storyName = 'Request Body From Schema';
+
+export const RequestBodyExamples = Template.bind({});
+RequestBodyExamples.args = {
+  httpOperation: examplesRequestBody,
+};
+RequestBodyExamples.storyName = 'Request Body from Examples';
