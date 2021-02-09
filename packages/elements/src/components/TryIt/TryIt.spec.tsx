d45ce225b81b8042ca82458341c948cb195745e2
Marcell Toth <marcell@stoplight.io>
2021-02-09 08:30:18 +0100

    test: import `jest-dom` globally

--- packages/elements/src/components/TryIt/TryIt.spec.tsx
+++ packages/elements/src/components/TryIt/TryIt.spec.tsx
@@ -1,5 +1,3 @@
-import '@testing-library/jest-dom/extend-expect';
-
 import { HttpParamStyles, IHttpOperation } from '@stoplight/types';
 import { screen, waitFor } from '@testing-library/dom';
 import { render } from '@testing-library/react';
