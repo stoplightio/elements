9f1695c41894db5b021f7df5c5cd772b0fd035b3
Phil Sturgeon <me@philsturgeon.com>
2021-02-10 13:11:41 +0000

    chore: this might actually work

--- .storybook/main.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  stories: [
-    '../packages/elements/src/__stories__/**/*.{js,jsx,ts,tsx}',
-    '../packages/elements-web-components/src/__stories__/**/*.{js,jsx,ts,tsx}'
-  ],
-  addons: ['@storybook/addon-essentials']
-};
