05ce8c661a22c39f153df6eb577cbb536698102f
mmiask <mmiask92@gmail.com>
2021-03-15 12:52:13 +0100

    feat: move activeInfoContext up to HttpService
    add aria-label to server rows
    start working on styling

--- packages/elements/src/components/Docs/HttpService/HttpService.tsx
+++ packages/elements/src/components/Docs/HttpService/HttpService.tsx
@@ -1,9 +1,10 @@
-import { Heading } from '@stoplight/mosaic';
+import { Box, Heading } from '@stoplight/mosaic';
 import { withErrorBoundary } from '@stoplight/react-error-boundary';
 import { IHttpService } from '@stoplight/types';
 import * as React from 'react';
 
 import { IDocsComponentProps } from '..';
+import { ActiveInfoContext } from '../../../containers/Provider';
 import { MarkdownViewer } from '../../MarkdownViewer';
 import { Badge } from '../HttpOperation/Badges';
 import { ServerInfo } from './ServerInfo';
@@ -17,26 +18,28 @@ const enhanceVersionString = (version: string): string => {
 export type HttpServiceProps = IDocsComponentProps<Partial<IHttpService>>;
 
 const HttpServiceComponent = React.memo<HttpServiceProps>(({ className, data, mockUrl }) => {
+  const info = React.useContext(ActiveInfoContext);
+
   return (
-    // <div className={className + ' flex items-start'}>
-    <div className="grid grid-cols-2">
-      <div>
-        {data.name && (
-          <Heading className="mb-5" fontWeight="medium" size={1}>
-            {data.name}
-          </Heading>
-        )}
-        <div className="mb-12">
-          {data.version && <Badge className="bg-gray-6">{enhanceVersionString(data.version)}</Badge>}
+    <Box className={className} bg="transparent">
+      <div className="grid grid-cols-2">
+        <div>
+          {data.name && (
+            <Heading className="mb-5" fontWeight="medium" size={1}>
+              {data.name}
+            </Heading>
+          )}
+          <div className="mb-12">
+            {data.version && <Badge className="bg-gray-6">{enhanceVersionString(data.version)}</Badge>}
+          </div>
+        </div>
+        <div></div>
+        <div>{data.description && <MarkdownViewer className="mb-10" markdown={data.description} />}</div>
+        <div>
+          <ServerInfo serverUrl={data.servers} mockUrl={mockUrl} activeContextInfo={info} />
         </div>
       </div>
-      <div></div>
-      <div>{data.description && <MarkdownViewer className="mb-10" markdown={data.description} />}</div>
-      <div>
-        <ServerInfo serverUrl={data.servers} mockUrl={mockUrl} />
-      </div>
-    </div>
-    // </div>
+    </Box>
   );
 });
 HttpServiceComponent.displayName = 'HttpService.Component';
--- packages/elements/src/components/Docs/HttpService/ServerInfo.tsx
+++ packages/elements/src/components/Docs/HttpService/ServerInfo.tsx
@@ -1,31 +1,54 @@
-import { Panel, Text } from '@stoplight/mosaic';
+import { InvertTheme, Panel, subscribeTheme, Text } from '@stoplight/mosaic';
 import { IServer } from '@stoplight/types';
-import { ActiveInfoContext } from 'elements/src/containers/Provider';
 import * as React from 'react';
+
+import { IActiveInfo } from '../../../containers/Provider';
 
 interface ServerInfoProps {
   serverUrl: IServer[] | undefined;
   mockUrl: string | undefined;
+  activeContextInfo: IActiveInfo;
 }
 
-export const ServerInfo: React.FC<ServerInfoProps> = ({ serverUrl, mockUrl }) => {
-  const info = React.useContext(ActiveInfoContext);
+export const ServerInfo: React.FC<ServerInfoProps> = ({ serverUrl, mockUrl, activeContextInfo }) => {
   const properUrl = new RegExp(
     /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/,
   );
   const productionUrl = serverUrl?.[0].url;
+
+  subscribeTheme({ mode: 'light' });
 
   if (!mockUrl?.match(properUrl) && !productionUrl?.match(properUrl)) {
     return null;
   }
 
   return (
-    <Panel rounded isCollapsible={false}>
-      <Panel.Titlebar>API Base URL</Panel.Titlebar>
-      <Panel.Content className="flex flex-col">
-        {productionUrl?.match(properUrl) && <Text>{`Production: ${productionUrl}`}</Text>}
-        {info.isStoplightProjectComponent && mockUrl?.match(properUrl) && <Text>{`Mock Server: ${mockUrl}`}</Text>}
-      </Panel.Content>
-    </Panel>
+    <InvertTheme>
+      <Panel rounded isCollapsible={false} className="BaseURLContent">
+        <Panel.Titlebar whitespace="nowrap">API Base URL</Panel.Titlebar>
+        <Panel.Content className="flex flex-col">
+          {productionUrl?.match(properUrl) && (
+            <div className="whitespace-nowrap">
+              <Text whitespace="nowrap" fontWeight="bold">
+                Production:
+              </Text>
+              <Text aria-label="production-server" pl={2}>
+                {productionUrl}
+              </Text>
+            </div>
+          )}
+          {activeContextInfo.isStoplightProjectComponent && mockUrl?.match(properUrl) && (
+            <div className="flex flex-row">
+              <Text whitespace="nowrap" fontWeight="bold">
+                Mock Server:
+              </Text>
+              <Text aria-label="mock-server" pl={2}>
+                {mockUrl}
+              </Text>
+            </div>
+          )}
+        </Panel.Content>
+      </Panel>
+    </InvertTheme>
   );
 };
