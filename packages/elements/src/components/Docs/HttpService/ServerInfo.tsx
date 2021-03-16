2f81c9444dc6dc47881a39ca7fe4c88a2506a9dc
mmiask <mmiask92@gmail.com>
2021-03-16 11:17:35 +0100

    refactor: move activeContextInfo to ServerInfo

--- packages/elements/src/components/Docs/HttpService/ServerInfo.tsx
+++ packages/elements/src/components/Docs/HttpService/ServerInfo.tsx
@@ -2,15 +2,16 @@ import { InvertTheme, Panel, subscribeTheme, Text } from '@stoplight/mosaic';
 import { IServer } from '@stoplight/types';
 import * as React from 'react';
 
-import { IActiveInfo } from '../../../containers/Provider';
+import { ActiveInfoContext } from '../../../containers/Provider';
 
 interface ServerInfoProps {
   serverUrl: IServer[] | undefined;
   mockUrl: string | undefined;
-  activeContextInfo: IActiveInfo;
 }
 
-export const ServerInfo: React.FC<ServerInfoProps> = ({ serverUrl, mockUrl, activeContextInfo }) => {
+export const ServerInfo: React.FC<ServerInfoProps> = ({ serverUrl, mockUrl }) => {
+  const info = React.useContext(ActiveInfoContext);
+
   const properUrl = new RegExp(
     /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/,
   );
@@ -24,30 +25,28 @@ export const ServerInfo: React.FC<ServerInfoProps> = ({ serverUrl, mockUrl, acti
 
   return (
     <InvertTheme>
-      <Panel rounded isCollapsible={false} className="BaseURLContent">
+      <Panel rounded isCollapsible={false} className="BaseURLContent" w="full">
         <Panel.Titlebar whitespace="nowrap">API Base URL</Panel.Titlebar>
-        <Panel.Content className="flex flex-col">
-          {productionUrl?.match(properUrl) && (
-            <div className="whitespace-nowrap">
-              <Text whitespace="nowrap" fontWeight="bold">
-                Production:
-              </Text>
-              <Text aria-label="production-server" pl={2}>
-                {productionUrl}
-              </Text>
-            </div>
-          )}
-          {activeContextInfo.isStoplightProjectComponent && mockUrl?.match(properUrl) && (
-            <div className="flex flex-row">
-              <Text whitespace="nowrap" fontWeight="bold">
-                Mock Server:
-              </Text>
-              <Text aria-label="mock-server" pl={2}>
-                {mockUrl}
-              </Text>
-            </div>
-          )}
-        </Panel.Content>
+        <div className="overflow-x-auto">
+          <Panel.Content w="max" className="flex flex-col">
+            {productionUrl?.match(properUrl) && (
+              <div className="whitespace-nowrap">
+                <Text fontWeight="bold">Production:</Text>
+                <Text aria-label="production-server" pl={2}>
+                  {productionUrl}
+                </Text>
+              </div>
+            )}
+            {info.isStoplightProjectComponent && mockUrl?.match(properUrl) && (
+              <div className="flex flex-row">
+                <Text fontWeight="bold">Mock Server:</Text>
+                <Text aria-label="mock-server" pl={2}>
+                  {mockUrl}
+                </Text>
+              </div>
+            )}
+          </Panel.Content>
+        </div>
       </Panel>
     </InvertTheme>
   );
