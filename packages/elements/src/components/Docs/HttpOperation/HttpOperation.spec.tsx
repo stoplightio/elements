2478ad8e1b037ae7d34e0f89f6096e22c64dffb1
mmiask <mmiask92@gmail.com>
2021-03-24 18:06:36 +0100

    test: check context switching

--- packages/elements/src/components/Docs/HttpOperation/HttpOperation.spec.tsx
+++ packages/elements/src/components/Docs/HttpOperation/HttpOperation.spec.tsx
@@ -6,6 +6,7 @@ import * as React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import httpOperation from '../../../__fixtures__/operations/put-todos';
+import requestBody from '../../../__fixtures__/operations/request-body';
 import { withPersistenceBoundary } from '../../../context/Persistence';
 import { HttpOperation as HttpOperationWithoutPersistence } from './index';
 
@@ -342,6 +343,19 @@ describe('HttpOperation', () => {
       userEvent.click(body);
 
       expect(await screen.findByText('This is JsonSchemaViewer')).toBeInTheDocument();
+    });
+
+    it('request body selection in Docs should update TryIt', () => {
+      render(<HttpOperation data={requestBody} />);
+
+      const body = screen.getByRole('textbox');
+      expect(body).toHaveTextContent('{ "name": "string", "age": 0 }');
+
+      const select = screen.getByLabelText('Choose Request Body Content Type');
+
+      userEvent.selectOptions(select, 'application/x-www-form-urlencoded');
+
+      expect(screen.getByLabelText('someEnum')).toBeInTheDocument();
     });
   });
 
