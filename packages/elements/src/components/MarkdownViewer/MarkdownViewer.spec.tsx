8558b4417be8902fc5f71e7ae009964d90901f91
Mateusz Podlasin <mateusz@stoplight.io>
2021-03-26 13:49:17 +0100

    chore: more test cases

--- packages/elements/src/components/MarkdownViewer/MarkdownViewer.spec.tsx
+++ packages/elements/src/components/MarkdownViewer/MarkdownViewer.spec.tsx
@@ -4,14 +4,14 @@ import { NodeType } from '@stoplight/types';
 import { render, screen } from '@testing-library/react';
 import * as React from 'react';
 
-import { BundledBranchNode, IntegrationKind } from '../../types';
+import { IntegrationKind } from '../../types';
 import { MarkdownViewer } from '.';
 import { MarkdownComponentsProvider } from './CustomComponents/Provider';
 import { createResolvedImageComponent } from './CustomComponents/ResolvedImage';
 
-describe('RequestSend', () => {
-  it('works', () => {
-    const branchNode: BundledBranchNode = {
+describe('MarkdownViewer', () => {
+  describe('with ResolvedImage', () => {
+    const common = {
       id: 54234624,
       data: 'some data',
       type: NodeType.Article,
@@ -28,14 +28,81 @@ describe('RequestSend', () => {
       externalSlug: 'project',
     };
 
-    render(
-      <MarkdownComponentsProvider value={{ image: createResolvedImageComponent(branchNode) }}>
-        <MarkdownViewer markdown={`![alt text](../../common/images/icon48.png "Logo Title Text 1")`} />
-      </MarkdownComponentsProvider>,
-    );
+    it.each([
+      [
+        IntegrationKind.Github,
+        {
+          ...common,
+          workspaceIntegration: {
+            kind: IntegrationKind.Github,
+            apiUrl: 'https://api.github.com',
+            hostUrl: 'https://github.com',
+          },
+        },
+        'https://raw.github.com/organisation/project/master/common/images/icon48.png',
+      ],
 
-    const image = screen.getByTitle('Logo Title Text 1');
+      [
+        IntegrationKind.Gitlab,
+        {
+          ...common,
+          workspaceIntegration: {
+            kind: IntegrationKind.Gitlab,
+            apiUrl: 'https://api.gitlab.com',
+            hostUrl: 'https://gitlab.com',
+          },
+        },
+        'https://gitlab.com/organisation/project/raw/master/common/images/icon48.png',
+      ],
 
-    expect(image).toHaveAttribute('src', 'https://raw.github.com/organisation/project/master/common/images/icon48.png');
+      [
+        IntegrationKind.Gitea,
+        {
+          ...common,
+          workspaceIntegration: {
+            kind: IntegrationKind.Gitea,
+            apiUrl: 'https://api.gitea.com',
+            hostUrl: 'https://gitea.com',
+          },
+        },
+        'https://gitea.com/organisation/project/raw/branch/master/common/images/icon48.png',
+      ],
+
+      [
+        IntegrationKind.BitbucketServer,
+        {
+          ...common,
+          workspaceIntegration: {
+            kind: IntegrationKind.BitbucketServer,
+            apiUrl: 'https://api.bitbucket.com',
+            hostUrl: 'https://bitbucket.com',
+          },
+        },
+        'https://api.bitbucket.com/projects/organisation/repos/project/browse/common/images/icon48.png?raw=',
+      ],
+
+      [
+        IntegrationKind.BitbucketCloud,
+        {
+          ...common,
+          workspaceIntegration: {
+            kind: IntegrationKind.BitbucketCloud,
+            apiUrl: 'https://api.bitbucket.com',
+            hostUrl: 'https://bitbucket.com',
+          },
+        },
+        'https://bitbucket.com/organisation/project/raw/master/common/images/icon48.png',
+      ],
+    ])('resolves relative image url for integration with %s', (_, branchNode, expectedUrl) => {
+      render(
+        <MarkdownComponentsProvider value={{ image: createResolvedImageComponent(branchNode) }}>
+          <MarkdownViewer markdown={`![alt text](../../common/images/icon48.png "Logo Title Text 1")`} />
+        </MarkdownComponentsProvider>,
+      );
+
+      const image = screen.getByTitle('Logo Title Text 1');
+
+      expect(image).toHaveAttribute('src', expectedUrl);
+    });
   });
 });
