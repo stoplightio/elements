6c1c0e052b92f890c990a2a9e29b78d3878b2187
Mateusz Podlasin <mateusz@stoplight.io>
2021-03-30 11:07:02 +0200

    fix: resolve relative image urls in markdown (#967)
    * fix: resolve relative image urls in markdown
    
    * chore: move custom markdown components to separate folder
    
    * chore: more test cases
    
    * chore: memoize resolved image component generation
    
    * fix: linter errors
    
    * fix: remove components file and fix linter errors (again)

---
+++ packages/elements/src/components/MarkdownViewer/CustomComponents/CodeWithSchemaViewer.tsx
@@ -0,0 +1,31 @@
+import { ICode } from '@stoplight/markdown/ast-types/smdast';
+import { defaultComponentMapping, ICodeAnnotations, IComponentMappingProps } from '@stoplight/markdown-viewer';
+import { get } from 'lodash';
+import React from 'react';
+
+import { useParsedValue } from '../../../hooks/useParsedValue';
+import { isJSONSchema } from '../../../utils/guards';
+import { getExamplesFromSchema } from '../../Docs/HttpOperation/utils';
+import { SchemaViewer } from '../../SchemaViewer';
+
+export const CodeWithSchemaViewer = (props: IComponentMappingProps<ICode<ICodeAnnotations>>) => {
+  const {
+    node: { annotations, value, resolved, meta },
+  } = props;
+  const nodeType = get(annotations, 'type') || meta;
+
+  const parsedValue = useParsedValue(resolved ?? value);
+
+  if (nodeType === 'json_schema') {
+    if (!isJSONSchema(parsedValue)) {
+      return null;
+    }
+
+    return (
+      <SchemaViewer title={annotations?.title} schema={parsedValue} examples={getExamplesFromSchema(parsedValue)} />
+    );
+  }
+
+  const DefaultCode = defaultComponentMapping.code!;
+  return <DefaultCode {...props} />;
+};
