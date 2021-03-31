38cf0518883da24282bd05411ffacdf36a116623
Jakub Jankowski <jakubj@stoplight.io>
2021-03-31 19:06:39 +0200

    feat(try-it): basic auth (#995)
    * feat(try-it): basic auth
    
    * chore: do not overuse react hooks
    
    Co-authored-by: Phil Sturgeon <phil@stoplight.io>

---
+++ packages/elements/src/components/TryIt/BasicAuth.tsx
@@ -0,0 +1,62 @@
+import { Flex, Input, Panel, Text } from '@stoplight/mosaic';
+import * as React from 'react';
+
+interface BasicAuthProps {
+  onChange: (value: string) => void;
+  value: string;
+}
+
+export const BasicAuth: React.FC<BasicAuthProps> = ({ onChange, value }) => {
+  const [username = '', password = ''] = decode(value).split(':');
+
+  const onCredentialsChange = (username: string, password: string) => {
+    onChange(encode(`${username}:${password}`));
+  };
+
+  return (
+    <Panel.Content className="sl-overflow-y-auto OperationParametersContent">
+      <Input role="username" appearance="minimal" readOnly value="Username" />
+      <Text mx={3}>:</Text>
+      <Flex flexGrow>
+        <Input
+          style={{ paddingLeft: 15 }}
+          aria-label="Username"
+          appearance="minimal"
+          flexGrow
+          placeholder="username"
+          value={username}
+          type="text"
+          required
+          onChange={e => onCredentialsChange(e.currentTarget.value, password)}
+        />
+      </Flex>
+      <Input role="password" appearance="minimal" readOnly value="Password" />
+      <Text mx={3}>:</Text>
+      <Flex flexGrow>
+        <Input
+          style={{ paddingLeft: 15 }}
+          aria-label="Password"
+          appearance="minimal"
+          flexGrow
+          placeholder="password"
+          value={password}
+          type="password"
+          required
+          onChange={e => onCredentialsChange(username, e.currentTarget.value)}
+        />
+      </Flex>
+    </Panel.Content>
+  );
+};
+
+function encode(value: string) {
+  return btoa(value);
+}
+
+function decode(encoded: string) {
+  try {
+    return atob(encoded);
+  } catch {
+    return '';
+  }
+}
