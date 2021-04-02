d1c904ee5e1d57048009d53b92dc8dbeb5196ea6
Marc MacLeod <marc@stoplight.io>
2021-04-02 14:01:11 -0500

    remove tslib resolution, change demo port to 4025

--- demo/README.md
+++ demo/README.md
@@ -4,4 +4,7 @@
 
 1. Run `yarn` in the root of the repo.
 2. Change into the `demo` directory.
-3. `yarn start` to start the demo. Changes to the demo or other packages in this repo will cause the demo to reload.
+3. `yarn start` to start the demo.
+4. Open `http://localhost:4025` in your browser.
+
+Changes to the demo or other packages in this repo will cause the demo to reload.
--- demo/webpack/dev.js
+++ demo/webpack/dev.js
@@ -6,12 +6,13 @@ module.exports = merge(commonConfig, {
   mode: 'development',
   entry: [
     // 'react-hot-loader/patch', // activate HMR for React
-    'webpack-dev-server/client?http://localhost:8080', // bundle the client for webpack-dev-server and connect to the provided endpoint
+    'webpack-dev-server/client?http://localhost:4025', // bundle the client for webpack-dev-server and connect to the provided endpoint
     'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
     './index.tsx', // the entry point of our app
   ],
   devServer: {
     hot: true, // enable HMR on the server
+    port: 4025,
   },
   devtool: 'cheap-module-source-map',
   plugins: [
