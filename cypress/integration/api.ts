e2480b33ec432869c552c04d54294f1e251840b9
Mateusz Podlasin <mateusz@stoplight.io>
2021-04-07 09:03:02 +0200

    chore: change e2e heading assertion

--- cypress/integration/api.ts
+++ cypress/integration/api.ts
@@ -19,5 +19,5 @@ describe('API component', () => {
 function loadZoomApiPage() {
   cy.visit('/');
   cy.findByRole('link', { name: /Zoom Api/i }).click();
-  cy.findAllByRole('heading', { name: 'Zoom' }).should('have.length', 2);
+  cy.findByRole('heading', { name: 'Zoom', level: 1 }).should('exist');
 }
