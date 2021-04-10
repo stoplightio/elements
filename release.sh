a8ce9bf0e2ff7c92b8bfa7751e06954baeb1ccd8
Phil Sturgeon <me@philsturgeon.com>
2021-04-10 19:42:32 +0100

    chore: release v6 from v6 branch but dont tag latest

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if yarn lerna changed
 then
-  yarn lerna publish --conventional-commits --force-publish --yes --dist-tag latest
+  yarn lerna publish --yes 
 else
   echo "No changes, skipping publish"
 fi
