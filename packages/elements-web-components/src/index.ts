070154f868f3476f99249240fa46a83c21727629
Michael Miaskowski <58433203+mmiask@users.noreply.github.com>
2021-04-12 15:27:15 +0200

    Fix(toc): missing caret icon in web components (#1040)
    * fix: missing ToC icons in web-components
    
    * fix: add FontAwesome packages as devDependencies

--- packages/elements-web-components/src/index.ts
+++ packages/elements-web-components/src/index.ts
@@ -1,4 +1,10 @@
+import { dom, library } from '@fortawesome/fontawesome-svg-core';
+import { faChevronDown, faChevronRight, faCube } from '@fortawesome/free-solid-svg-icons';
+
 import { ApiElement, StoplightProjectElement } from './components';
+
+library.add(faChevronDown, faChevronRight, faCube);
 
 window.customElements.define('elements-stoplight-project', StoplightProjectElement);
 window.customElements.define('elements-api', ApiElement);
+dom.watch();
