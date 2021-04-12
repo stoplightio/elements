f44bcbc119b51060093ec82fecc345478f4397a3
Marcell Toth <marcell@stoplight.io>
2021-04-12 11:17:31 +0200

    docs: add example stories

--- packages/elements/src/containers/API.stories.tsx
+++ packages/elements/src/containers/API.stories.tsx
@@ -26,27 +26,52 @@ export default {
 
 const Template: Story<APIProps> = args => <API {...args} />;
 
-export const TodosAPI = Template.bind({});
-TodosAPI.args = {
-  apiDescriptionUrl: 'https://petstore.swagger.io/v2/swagger.json',
-};
-TodosAPI.storyName = 'Simple API from URL';
-
 export const APIWithYamlProvidedDirectly = Template.bind({});
 APIWithYamlProvidedDirectly.args = {
   apiDescriptionDocument: zoomApiYaml,
 };
-APIWithYamlProvidedDirectly.storyName = 'Direct YAML Input';
+APIWithYamlProvidedDirectly.storyName = 'Direct YAML Input (Zoom)';
 
 export const APIWithJSONProvidedDirectly = Template.bind({});
 APIWithJSONProvidedDirectly.args = {
   apiDescriptionDocument: JSON.stringify(parse(zoomApiYaml), null, '  '),
 };
-APIWithJSONProvidedDirectly.storyName = 'Direct JSON Input';
+APIWithJSONProvidedDirectly.storyName = 'Direct JSON Input (Zoom)';
 
 export const APIWithoutDescription = Template.bind({});
 APIWithoutDescription.args = {
   apiDescriptionDocument: simpleApiWithoutDescription,
 };
-
 APIWithoutDescription.storyName = 'API Without Description';
+
+export const Petstore = Template.bind({});
+Petstore.args = {
+  apiDescriptionUrl: 'https://petstore.swagger.io/v2/swagger.json',
+};
+Petstore.storyName = 'Swagger Petstore v2';
+
+export const Box = Template.bind({});
+Box.args = {
+  apiDescriptionUrl: 'https://raw.githubusercontent.com/box/box-openapi/main/content/openapi.yml',
+};
+Box.storyName = 'Box';
+
+export const DigitalOcean = Template.bind({});
+DigitalOcean.args = {
+  apiDescriptionUrl:
+    'https://raw.githubusercontent.com/digitalocean/openapi/main/specification/DigitalOcean-public.v2.yaml',
+};
+DigitalOcean.storyName = 'DigitalOcean';
+
+export const Github = Template.bind({});
+Github.args = {
+  apiDescriptionUrl:
+    'https://raw.githubusercontent.com/github/rest-api-description/main/descriptions/ghes-3.0/ghes-3.0.json',
+};
+Github.storyName = 'GitHub';
+
+export const Instagram = Template.bind({});
+Instagram.args = {
+  apiDescriptionUrl: 'https://api.apis.guru/v2/specs/instagram.com/1.0.0/swagger.yaml',
+};
+Instagram.storyName = 'Instagram';
