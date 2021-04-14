142373a368d7e8e910a541fb10129bd13aa0e482
Mateusz Podlasin <mateusz@stoplight.io>
2021-04-14 13:10:35 +0200

    chore: copy build configuration from web components to elements

---
+++ packages/elements/src/web-components/__stories__/StoplightProject.stories.ts
@@ -0,0 +1,23 @@
+import '../index';
+
+import { createTemplate } from './util';
+
+const Template = createTemplate('elements-stoplight-project');
+
+export default {
+  title: 'web-components/StoplightProject',
+  argTypes: {
+    workspaceSlug: { control: 'text' },
+    projectSlug: { control: 'text' },
+    platformUrl: { control: 'text' },
+    router: { control: { type: 'inline-radio', options: ['history', 'hash', 'memory'] }, defaultValue: 'history' },
+  },
+};
+
+export const defaultProject = Template.bind({});
+defaultProject.storyName = "Stoplight's Demo workspace";
+defaultProject.args = {
+  workspaceSlug: 'demo',
+  projectSlug: 'public-apis',
+  platformUrl: 'https://stoplight.io',
+};
---
+++ packages/elements/src/web-components/components.ts
@@ -0,0 +1,20 @@
+import { API, StoplightProject } from '../index';
+import { createElementClass } from './createElementClass';
+
+export const StoplightProjectElement = createElementClass(StoplightProject, {
+  workspaceSlug: { type: 'string', defaultValue: '' },
+  projectSlug: { type: 'string', defaultValue: '' },
+  branchSlug: { type: 'string' },
+  authToken: { type: 'string' },
+  basePath: { type: 'string' },
+  router: { type: 'string' },
+  platformUrl: { type: 'string' },
+});
+
+export const ApiElement = createElementClass(API, {
+  apiDescriptionUrl: { type: 'string', defaultValue: '' },
+  apiDescriptionDocument: { type: 'string', defaultValue: '' },
+  basePath: { type: 'string' },
+  router: { type: 'string' },
+  layout: { type: 'string' },
+});
