001f6cc108a65b8e8ab57e9266f7be3687f6e0c6
Marcell Toth <marcell@stoplight.io>
2021-04-16 13:31:30 +0200

    Revert "test: load spec from fixtures"
    This reverts commit 164cc211ba6df714ca7d279d04aae2b915d77503.

--- examples/react-cra/src/components/API.tsx
+++ examples/react-cra/src/components/API.tsx
@@ -4,5 +4,10 @@ import { API } from '@stoplight/elements';
 import React from 'react';
 
 export const StoplightAPI: React.FC = () => {
-  return <API basePath="zoom-api" apiDescriptionUrl="https://fixtures/zoom.yaml" />;
+  return (
+    <API
+      basePath="zoom-api"
+      apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/zoom.yaml"
+    />
+  );
 };
