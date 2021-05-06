9e267b63ef69a16488d5a2c0a62558ec56c102da
Brett Willemsen <bwillemsen@pagerduty.com>
2021-05-06 17:09:28 -0400

    chore: upgrade gatsby example to 3.x

--- examples/react-gatsby/gatsby-config.js
+++ examples/react-gatsby/gatsby-config.js
@@ -27,10 +27,6 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
-    'gatsby-plugin-meta-redirect',
+    }
   ],
 };
