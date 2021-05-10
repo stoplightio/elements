14d739fa9bd2903af3ab7637bc7f9665d553b6f0
lottamus <chrisnlott@gmail.com>
2021-05-09 22:06:00 -0500

    feat: stoplight project routes

--- packages/elements-dev-portal/src/containers/StoplightProject.tsx
+++ packages/elements-dev-portal/src/containers/StoplightProject.tsx
@@ -2,20 +2,22 @@ import { SidebarLayout } from '@stoplight/elements/components/Layout/SidebarLayo
 import { findFirstNode } from '@stoplight/elements/components/MosaicTableOfContents/utils';
 import { withPersistenceBoundary } from '@stoplight/elements/context/Persistence';
 import { withMosaicProvider } from '@stoplight/elements/hoc/withMosaicProvider';
-import { withRouter } from '@stoplight/elements/hoc/withRouter';
+import { useRouter } from '@stoplight/elements/hooks/useRouter';
 import { withStyles } from '@stoplight/elements/styled';
 import { RoutingProps } from '@stoplight/elements/types';
 import { pipe } from 'lodash/fp';
 import * as React from 'react';
-import { Link, useHistory, useLocation } from 'react-router-dom';
+import { Link, Redirect, Route, useHistory, useParams } from 'react-router-dom';
 
 import { Loading } from '../components/Loading';
 import { NodeContent } from '../components/NodeContent';
 import { NotFound } from '../components/NotFound';
 import { Provider } from '../components/Provider';
 import { TableOfContents } from '../components/TableOfContents';
+import { useGetBranches } from '../hooks/useGetBranches';
 import { useGetNodeContent } from '../hooks/useGetNodeContent';
 import { useGetTableOfContents } from '../hooks/useGetTableOfContents';
+import { Branch } from '../interfaces/branch';
 
 export interface StoplightProjectProps extends RoutingProps {
   /**
@@ -24,36 +26,31 @@ export interface StoplightProjectProps extends RoutingProps {
    */
   projectId: string;
   /**
-   * The name of a specific branch of the project to show. If no branch is provided, the default branch will be shown.
-   */
-  branchSlug?: string;
-  /**
    * If your company runs an on-premise deployment of Stoplight,
    * set this prop to point the StoplightProject component at the URL of that instance.
    */
   platformUrl?: string;
 }
 
-const StoplightProjectImpl: React.FC<StoplightProjectProps> = ({ platformUrl, projectId, branchSlug }) => {
+const StoplightProjectImpl: React.FC<StoplightProjectProps> = ({ projectId }) => {
   const history = useHistory();
-  const { pathname } = useLocation();
-  const nodeSlug = pathname === '/' ? '' : pathname;
+  const { branchSlug = '', nodeSlug = '' } = useParams<{ branchSlug?: string; nodeSlug: string }>();
 
-  const { data: tableOfContents } = useGetTableOfContents({ projectId, branchSlug });
+  const { data: tableOfContents, isFetched: isTocFetched } = useGetTableOfContents({ projectId, branchSlug });
+  const { data: branches } = useGetBranches({ projectId });
   const { data: node, isFetched } = useGetNodeContent({ nodeSlug, projectId, branchSlug });
 
-  React.useEffect(() => {
-    // Automatically redirect to the first node in the table of contents
-    if (!nodeSlug && tableOfContents?.items) {
-      const firstNode = findFirstNode(tableOfContents.items);
-      if (firstNode) {
-        history.replace(firstNode.slug);
+  let elem = <Loading />;
+  if (!nodeSlug) {
+    if (isTocFetched) {
+      if (tableOfContents?.items) {
+        const firstNode = findFirstNode(tableOfContents.items);
+        if (firstNode) {
+          return <Redirect to={branchSlug ? `/branches/${branchSlug}/${firstNode.slug}` : `/${firstNode.slug}`} />;
+        }
       }
     }
-  }, [nodeSlug, tableOfContents, history]);
-
-  let elem;
-  if (!isFetched || !nodeSlug) {
+  } else if (!isFetched) {
     elem = <Loading />;
   } else if (!node) {
     elem = <NotFound />;
@@ -63,17 +60,44 @@ const StoplightProjectImpl: React.FC<StoplightProjectProps> = ({ platformUrl, pr
 
   return (
     <SidebarLayout
-      sidebar={tableOfContents ? <TableOfContents activeId="" tableOfContents={tableOfContents} Link={Link} /> : null}
+      sidebar={
+        tableOfContents ? (
+          <TableOfContents
+            activeId={node?.id || ''}
+            tableOfContents={tableOfContents}
+            Link={Link}
+            branchSlug={branchSlug || ''}
+            branches={branches || []}
+            onChange={(branch: Branch) => {
+              history.push(`/branches/${branch.slug}/${nodeSlug}`);
+            }}
+          />
+        ) : null
+      }
     >
       {elem}
     </SidebarLayout>
   );
 };
 
-const WrappedStoplightProject = (props: StoplightProjectProps) => {
+const StoplightProjectRouter = ({ projectId, platformUrl, basePath = '/', router }: StoplightProjectProps) => {
+  const { Router, routerProps } = useRouter(router ?? 'history', basePath);
+
   return (
-    <Provider platformUrl={props.platformUrl}>
-      <StoplightProjectImpl {...props} />
+    <Provider platformUrl={platformUrl}>
+      <Router {...routerProps}>
+        <Route path="/branches/:branchSlug/:nodeSlug" exact>
+          <StoplightProjectImpl projectId={projectId} />
+        </Route>
+
+        <Route path="/:nodeSlug" exact>
+          <StoplightProjectImpl projectId={projectId} />
+        </Route>
+
+        <Route path="/" exact>
+          <StoplightProjectImpl projectId={projectId} />
+        </Route>
+      </Router>
     </Provider>
   );
 };
@@ -81,9 +105,4 @@ const WrappedStoplightProject = (props: StoplightProjectProps) => {
 /**
  * The StoplightProject component displays a traditional documentation UI for an existing Stoplight Project.
  */
-export const StoplightProject = pipe(
-  withRouter,
-  withStyles,
-  withPersistenceBoundary,
-  withMosaicProvider,
-)(WrappedStoplightProject);
+export const StoplightProject = pipe(withStyles, withPersistenceBoundary, withMosaicProvider)(StoplightProjectRouter);
