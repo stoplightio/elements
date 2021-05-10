990a4442d59e39e2caa549a52c8041f5241b177d
lottamus <chrisnlott@gmail.com>
2021-05-09 22:05:43 -0500

    chore: add branch selector to toc

--- packages/elements-dev-portal/src/components/TableOfContents/index.tsx
+++ packages/elements-dev-portal/src/components/TableOfContents/index.tsx
@@ -5,16 +5,28 @@ import { Box, BoxProps, Flex } from '@stoplight/mosaic';
 import * as React from 'react';
 
 import { TableOfContents as TableOfContentsData } from '../../interfaces/tableOfContents';
+import { BranchSelector, BranchSelectorProps } from '../BranchSelector';
 
-export type TableOfContentsProps = BoxProps<'div'> & {
-  activeId: string;
-  tableOfContents: TableOfContentsData;
-  Link: CustomLinkComponent;
-};
+export type TableOfContentsProps = BranchSelectorProps &
+  Omit<BoxProps<'div'>, 'onChange'> & {
+    activeId: string;
+    tableOfContents: TableOfContentsData;
+    Link: CustomLinkComponent;
+  };
 
-export const TableOfContents = ({ tableOfContents, activeId, Link, ...boxProps }: TableOfContentsProps) => {
+export const TableOfContents = ({
+  tableOfContents,
+  activeId,
+  Link,
+  branchSlug,
+  branches,
+  onChange,
+  ...boxProps
+}: TableOfContentsProps) => {
   return (
     <Flex direction="col" bg="canvas-100" h="full" {...boxProps}>
+      <BranchSelector branchSlug={branchSlug} branches={branches} onChange={onChange} />
+
       <Box flex={1}>
         <ElementsTableOfContents tree={tableOfContents.items} activeId={activeId} Link={Link} />
       </Box>
