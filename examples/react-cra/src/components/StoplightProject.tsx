565a0cd2ad1fd00053e7a6f1ec48468a4d51f008
Jakub Jankowski <jakubj@stoplight.io>
2021-05-10 11:54:29 +0200

    fix: e2e

--- examples/react-cra/src/components/StoplightProject.tsx
+++ examples/react-cra/src/components/StoplightProject.tsx
@@ -1,4 +1,4 @@
-import '@stoplight/elements/styles/elements.scss';
+import '@stoplight/elements/styles/elements-scoped.scss';
 
 import { StoplightProject } from '@stoplight/elements-dev-portal';
 import React from 'react';
