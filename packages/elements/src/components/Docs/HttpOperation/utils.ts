95420c7821cc4dd9953b56f9878f51eed45a564d
Jakub Rożek <jakub@stoplight.io>
2021-05-11 11:59:58 +0200

    fix(schema): handle arrayish examples (#1206)

--- packages/elements/src/components/Docs/HttpOperation/utils.ts
+++ packages/elements/src/components/Docs/HttpOperation/utils.ts
@@ -15,11 +15,13 @@ export function getExamplesObject(examples: Array<INodeExample | INodeExternalEx
 export function getExamplesFromSchema(data: unknown) {
   // `examples` are available in JSON Schema v6 and v7. For v4 we can use `x-examples`.
   // `example` is not supported by any version but we can use `x-example` extension.
-  return isObject(data)
-    ? {
-        ...(isObject(data['x-examples']) && { ...data['x-examples'] }),
-        ...(isObject(data['examples']) && { ...data['examples'] }),
-        ...('x-example' in data && { default: data['x-example'] }),
-      }
-    : void 0;
+  if (!isObject(data)) return;
+  if (Array.isArray(data['examples'])) {
+    return data['examples'];
+  }
+
+  return {
+    ...(isObject(data['x-examples']) && { ...data['x-examples'] }),
+    ...('x-example' in data && { default: data['x-example'] }),
+  };
 }
