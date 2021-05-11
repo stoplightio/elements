7dda51632cb561d2364bdb0e3be1e3e1ac77c03b
lottamus <chrisnlott@gmail.com>
2021-05-11 01:50:19 -0500

    chore: remove unused function

--- packages/elements-core/src/utils/isAxiosError.ts
+++
@@ -1,5 +0,0 @@
-import { AxiosError } from 'axios';
-
-export function isAxiosError(e: Error & { isAxiosError?: boolean }): e is AxiosError {
-  return e.isAxiosError !== undefined && e.isAxiosError;
-}
