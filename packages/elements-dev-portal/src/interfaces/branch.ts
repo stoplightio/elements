49a321823a6078fcdbc6315f690a264658dd44c2
lottamus <chrisnlott@gmail.com>
2021-05-13 05:39:06 -0500

    fix: merge conflict

--- packages/elements-dev-portal/src/interfaces/branch.ts
+++
@@ -1,8 +0,0 @@
-export type Branch = {
-  id: number;
-  slug: string;
-  is_default: boolean;
-  is_published: boolean;
-  projectId: number;
-  name?: string;
-};
--- packages/elements-dev-portal/src/interfaces/tableOfContents.ts
+++
@@ -1,6 +0,0 @@
-import { TableOfContentsItem } from '@stoplight/elements-core/components/MosaicTableOfContents/types';
-
-export type TableOfContents = {
-  items: TableOfContentsItem[];
-  hide_powered_by?: boolean;
-};
