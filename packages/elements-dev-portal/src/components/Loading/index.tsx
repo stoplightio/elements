071dd51a17dd9dde349ca5ec36e218a963380f39
Marcell Toth <marcell@stoplight.io>
2021-05-14 13:48:38 +0200

    chore: simplify loading

--- packages/elements-dev-portal/src/components/Loading/index.tsx
+++ packages/elements-dev-portal/src/components/Loading/index.tsx
@@ -1,8 +1,8 @@
-import { Box, Flex, Icon } from '@stoplight/mosaic';
+import { Flex, Icon } from '@stoplight/mosaic';
 import React from 'react';
 
 export const Loading = () => (
   <Flex justify="center" alignItems="center" w="full" minH="screen" color="muted">
-    <Box as={Icon} icon={['fal', 'circle-notch']} size="3x" spin />
+    <Icon icon={['fal', 'circle-notch']} size="3x" spin />
   </Flex>
 );
