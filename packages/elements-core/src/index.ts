a37e819ce1ded17d569a91a216260ba4836e9abb
Marcell Toth <marcell@stoplight.io>
2021-05-19 18:03:54 +0200

    fix: missing exports

--- packages/elements-core/src/index.ts
+++ packages/elements-core/src/index.ts
@@ -1,3 +1,4 @@
+export { TableOfContents as TableOfContentsContainer } from './';
 export { Docs, ParsedDocs } from './components/Docs';
 export { DeprecatedBadge } from './components/Docs/HttpOperation/Badges';
 export { SidebarLayout } from './components/Layout/SidebarLayout';
@@ -5,6 +6,7 @@ export { Logo } from './components/Logo';
 export { MarkdownComponentsProvider } from './components/MarkdownViewer/CustomComponents/Provider';
 export { TableOfContents } from './components/MosaicTableOfContents';
 export { CustomLinkComponent, TableOfContentsItem } from './components/MosaicTableOfContents/types';
+export { findFirstNode } from './components/MosaicTableOfContents/utils';
 export { PoweredByLink } from './components/PoweredByLink';
 export { Row } from './components/TableOfContents/Row';
 export { TryIt, TryItProps, TryItWithRequestSamples, TryItWithRequestSamplesProps } from './components/TryIt';
@@ -15,8 +17,7 @@ export {
   NodeTypeIconDefs,
   NodeTypePrettyName,
 } from './constants';
-export { Provider } from './containers/Provider';
-export { TableOfContents as TableOfContentsContainer } from './containers/TableOfContents';
+export { MockingProvider, Provider } from './containers/Provider';
 export { InlineRefResolverProvider, SchemaTreeRefDereferenceFn } from './context/InlineRefResolver';
 export { PersistenceContextProvider, withPersistenceBoundary } from './context/Persistence';
 export { withMosaicProvider } from './hoc/withMosaicProvider';
@@ -26,6 +27,7 @@ export { useBundleRefsIntoDocument } from './hooks/useBundleRefsIntoDocument';
 export { useDereferencedHttpOperation } from './hooks/useDereferencedHttpOperation';
 export { useParsedData } from './hooks/useParsedData';
 export { useParsedValue } from './hooks/useParsedValue';
+export { useRouter } from './hooks/useRouter';
 export { useTocContents } from './hooks/useTocContents';
 export { withStyles } from './styled';
 export { Divider, Group, ITableOfContentsTree, Item, RoutingProps, TableOfContentItem } from './types';
