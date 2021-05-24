9d7acac5f10b99a1b4b9856224aa53ee0f9097eb
Marcell Toth <marcell@stoplight.io>
2021-05-24 13:27:37 +0200

    fix: title stying

--- packages/elements-core/src/components/Docs/Model/Model.tsx
+++ packages/elements-core/src/components/Docs/Model/Model.tsx
@@ -1,20 +1,30 @@
+import { MarkdownViewer } from '@stoplight/elements-core/components/MarkdownViewer';
+import { useInlineRefResolver } from '@stoplight/elements-core/context/InlineRefResolver';
+import { JsonSchemaViewer } from '@stoplight/json-schema-viewer';
+import { CLASSNAMES } from '@stoplight/markdown-viewer';
 import { Heading } from '@stoplight/mosaic';
 import { withErrorBoundary } from '@stoplight/react-error-boundary';
 import cn from 'classnames';
 import { JSONSchema7 } from 'json-schema';
 import * as React from 'react';
 
-import { SchemaAndDescription } from '../../SchemaAndDescription';
 import { DocsComponentProps } from '..';
 
 export type ModelProps = DocsComponentProps<JSONSchema7>;
 
 const ModelComponent: React.FC<ModelProps> = ({ data, className, headless }) => {
+  const resolveRef = useInlineRefResolver();
   return (
-    <div className={cn('Model MarkdownViewer', className)}>
-      {!headless && data.title !== void 0 && <Heading size={1}>{data.title}</Heading>}
+    <div className={cn('Model', className)}>
+      {!headless && data.title !== void 0 && (
+        <Heading size={1} mb={4} fontWeight="semibold">
+          {data.title}
+        </Heading>
+      )}
 
-      <SchemaAndDescription schema={data} description={data.description} />
+      {data.description && <MarkdownViewer markdown={data.description} />}
+
+      <JsonSchemaViewer resolveRef={resolveRef} className={cn(className, CLASSNAMES.block)} schema={data} />
     </div>
   );
 };
