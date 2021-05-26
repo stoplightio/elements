7ae7f3c4df56bc0900622b611e3c8f89998d33d9
paulatulis <47359669+paulatulis@users.noreply.github.com>
2021-05-26 08:54:31 -0500

    feat: internal badge (#1326)
    * feat: internal badge
    
    * chore: update icon
    
    * chore: add internal badge to model
    
    * chore: address feedback

--- packages/elements-core/src/components/Docs/Model/Model.tsx
+++ packages/elements-core/src/components/Docs/Model/Model.tsx
@@ -1,3 +1,4 @@
+import { HStack } from '@stoplight/mosaic';
 import { withErrorBoundary } from '@stoplight/react-error-boundary';
 import { Classes } from '@stoplight/ui-kit';
 import cn from 'classnames';
@@ -6,14 +7,22 @@ import * as React from 'react';
 
 import { SchemaAndDescription } from '../../SchemaAndDescription';
 import { DocsComponentProps } from '..';
+import { InternalBadge } from '../HttpOperation/Badges';
 
 export type ModelProps = DocsComponentProps<JSONSchema7>;
 
 const ModelComponent: React.FC<ModelProps> = ({ data, className, headless }) => {
+  const isInternal = !!data['x-internal'];
+
   return (
     <div className={cn('Model MarkdownViewer', className)}>
       {!headless && data.title !== void 0 && <h1 className={Classes.HEADING}>{data.title}</h1>}
 
+      {isInternal && (
+        <HStack spacing={2} mt={3} mb={12}>
+          <InternalBadge />
+        </HStack>
+      )}
       <SchemaAndDescription schema={data} description={data.description} />
     </div>
   );
