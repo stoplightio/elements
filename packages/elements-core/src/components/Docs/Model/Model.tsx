ee188b3605f3e48cb39c576eb1b8df720110a814
Marcell Toth <marcell@stoplight.io>
2021-05-27 14:34:57 +0200

    feat(docs): model titles (#1328)
    * feat: use mosaic title
    
    * fix: title stying
    
    * feat(docs): use node title if model doesn't have one
    
    * fix: broken imports
    
    * feat: add feature to API component
    
    * chore: inline SchemaAndDescription
    
    * fix: imports
    
    * fix: cleanup

--- packages/elements-core/src/components/Docs/Model/Model.tsx
+++ packages/elements-core/src/components/Docs/Model/Model.tsx
@@ -1,29 +1,39 @@
-import { HStack } from '@stoplight/mosaic';
+import { JsonSchemaViewer } from '@stoplight/json-schema-viewer';
+import { Heading, HStack } from '@stoplight/mosaic';
 import { withErrorBoundary } from '@stoplight/react-error-boundary';
-import { Classes } from '@stoplight/ui-kit';
 import cn from 'classnames';
 import { JSONSchema7 } from 'json-schema';
 import * as React from 'react';
 
-import { SchemaAndDescription } from '../../SchemaAndDescription';
+import { useInlineRefResolver } from '../../../context/InlineRefResolver';
+import { MarkdownViewer } from '../../MarkdownViewer';
 import { DocsComponentProps } from '..';
 import { InternalBadge } from '../HttpOperation/Badges';
 
 export type ModelProps = DocsComponentProps<JSONSchema7>;
 
-const ModelComponent: React.FC<ModelProps> = ({ data, className, headless }) => {
+const ModelComponent: React.FC<ModelProps> = ({ data, className, headless, nodeTitle }) => {
+  const resolveRef = useInlineRefResolver();
+  const title = data.title ?? nodeTitle;
   const isInternal = !!data['x-internal'];
 
   return (
-    <div className={cn('Model MarkdownViewer', className)}>
-      {!headless && data.title !== void 0 && <h1 className={Classes.HEADING}>{data.title}</h1>}
+    <div className={cn('Model', className)}>
+      {!headless && title !== undefined && (
+        <Heading size={1} mb={4} fontWeight="semibold">
+          {title}
+        </Heading>
+      )}
 
       {isInternal && (
         <HStack spacing={2} mt={3} mb={12}>
           <InternalBadge />
         </HStack>
       )}
-      <SchemaAndDescription schema={data} description={data.description} />
+
+      {data.description && <MarkdownViewer markdown={data.description} />}
+
+      <JsonSchemaViewer resolveRef={resolveRef} className={className} schema={data} />
     </div>
   );
 };
