3aec677bd66733737f8a16c8219a0b31b24bd167
mmiask <mmiask92@gmail.com>
2021-05-28 17:32:45 +0200

    feat: test ci job

--- unlink-examples.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-packages=( elements elements-dev-portal elements-core )
-example=$1
-
-cd examples/$example && yarn unlink "@stoplight/elements" && yarn unlink "@stoplight/elements-dev-portal" && yarn unlink "@stoplight/elements-core" && yarn install --check-files 
-
-cd ../../
---
+++ use-local-elements.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+examplesFolder=$1
+example=$2
+
+cd ./$examplesFolder/$example
+
+npx json -I -f package.json -e "this.resolutions={
+\"@stoplight/elements\": \"file:../../packages/elements/dist\",
+\"@stoplight/elements-dev-portal\": \"file:../../packages/elements-dev-portal/dist\",
+\"@stoplight/elements-core\": \"file:../../packages/elements-core/dist\",
+\"@stoplight/elements-utils\": \"file:../../packages/elements-utils/dist\"
+}"
+
+npx json -I -f package.json -e "this.dependencies[\"@stoplight/elements\"]=\"file:../../packages/elements/dist\""
+npx json -I -f package.json -e "this.dependencies[\"@stoplight/elements-dev-portal\"]=\"file:../../packages/elements-dev-portal/dist\""
+
+npx json -I -f package.json -e "this.scripts.reinstall=\"rm -rf node_modules && yarn install\""
