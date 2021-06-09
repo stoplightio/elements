9306a40811a46ac8b5edae7666e8a824a1f30f3a
Marcell Toth <marcell@stoplight.io>
2021-06-09 13:20:36 +0200

    chore: undo broken CRA example

--- examples/react-cra/src/components/StoplightProject.tsx
+++ examples/react-cra/src/components/StoplightProject.tsx
@@ -1,7 +1,8 @@
 import '@stoplight/elements/styles.min.css';
 
+import { StoplightProject } from '@stoplight/elements-dev-portal';
 import React from 'react';
 
 export const StoplightProjectDocs: React.FC = () => {
-  return null;
+  return <StoplightProject basePath="stoplight-project" platformUrl="https://stoplight.io" projectId="cHJqOjExOTY" />;
 };
