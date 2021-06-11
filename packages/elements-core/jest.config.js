a07c89908c92ffa5fff0982305411cdaa7d60bea
Marcell Toth <marcell@stoplight.io>
2021-06-11 16:38:16 +0200

    test: deduplicate jest configs

--- packages/elements-core/jest.config.js
+++ packages/elements-core/jest.config.js
@@ -1,9 +1,1 @@
-module.exports = {
-  preset: '@stoplight/scripts',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['./setupTests.ts'],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
-  testMatch: ['<rootDir>/src/**/*.(spec|test).(ts|js)?(x)'],
-  coveragePathIgnorePatterns: ['__tests__', '__fixtures__', '__stories__'],
-  testTimeout: 10000,
-};
+module.exports = require('../../jest.config');
