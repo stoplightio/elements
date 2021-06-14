a5962d964eacdb49bfc3ba1a6c705f7a1d24e23c
Marcell Toth <marcell@stoplight.io>
2021-06-14 13:36:24 +0200

    fix: fix v6 release

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if yarn lerna changed
 then
-  yarn lerna publish --yes 
+  yarn lerna publish --yes --registry https://registry.npmjs.org
 else
   echo "No changes, skipping publish"
 fi
