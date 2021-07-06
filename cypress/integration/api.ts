e8d7ba16ac0f63855115bdaf69d91450dd18b3cc
Jakub Jankowski <jakubj@stoplight.io>
2021-07-06 09:36:41 +0200

    test: enhance e2e suite (#1540)
    * test: e2e update
    
    * chore: cleanup
    
    * fix: deprecated api
    
    * chore: cleanup

--- cypress/integration/api.ts
+++ cypress/integration/api.ts
@@ -4,8 +4,73 @@ describe('API component', () => {
     cy.redirectUnpkgToLocalDistFolder('elements');
   });
 
-  it('loads correctly', () => {
-    loadZoomApiPage();
+  describe('Api page', () => {
+    it('loads api page correctly', () => {
+      loadZoomApiPage();
+      cy.findByText('https://api.zoom.us/v2').should('exist');
+      cy.findByRole('heading', { name: /API Key/ }).should('exist');
+      cy.findByText('API Description').should('exist');
+    });
+  });
+
+  describe('Operation page', () => {
+    it('loads operation docs correctly', () => {
+      cy.visit('/zoom-api/operations/groupCreate');
+      cy.findByRole('heading', { name: /Create a group/i }).should('exist');
+      cy.findByRole('heading', { name: /Request/i }).should('exist');
+      cy.findByRole('heading', { name: /post \/groups/i }).should('exist');
+      cy.findByRole('heading', { name: /Response/i }).should('exist');
+    });
+
+    it('does not break on select dropdown', () => {
+      loadZoomApiPage();
+      cy.visit('/zoom-api/operations/groups');
+      cy.findByRole('button', { name: /request sample/i }).click();
+      cy.findByText('Obj-C').should('exist');
+    });
+
+    it('displays request sample correctly', () => {
+      cy.visit('/zoom-api/operations/userCreate');
+      cy.findByLabelText(/curl/i).should('exist');
+      cy.findByLabelText(/--request post/i).should('exist');
+    });
+
+    it('displays example correctly', () => {
+      cy.visit('/zoom-api/operations/users');
+      cy.findByLabelText(/{.+}/).should('exist');
+    });
+
+    it('displays schema correctly', () => {
+      cy.visit('/zoom-api/operations/userCreate');
+      cy.findAllByText('action').should('exist');
+      cy.findAllByText('password').should('exist');
+    });
+
+    it('invokes TryIt request (receives network error)', () => {
+      cy.visit('/zoom-api/operations/userCreate');
+      cy.findByRole('button', { name: /Send Request/i }).click();
+      cy.findByRole('button', { name: 'Error' }).should('exist');
+    });
+
+    it('navigates on auth badge', () => {
+      cy.visit('/zoom-api/operations/userCreate');
+      cy.findByRole('link', { name: 'API Key' }).click();
+      cy.findByRole('heading', { name: 'Zoom API', level: 1 }).should('exist');
+    });
+  });
+
+  describe('Schema page', () => {
+    it('loads schema page correctly', () => {
+      cy.visit('/zoom-api/schemas/Account');
+      cy.findByRole('heading', { name: /Account/i }).should('exist');
+      cy.findAllByText('first_name').should('exist');
+      cy.findAllByText('pay_mode').should('exist');
+    });
+
+    it('displays example correctly', () => {
+      cy.visit('/zoom-api/schemas/Account');
+      cy.findByLabelText(/{.+}/).should('exist');
+    });
   });
 
   it('navigates via table of contents', () => {
@@ -13,18 +78,6 @@ describe('API component', () => {
     cy.findByText('groups').click();
     cy.findByText('Create a group').click();
     cy.findByRole('heading', { name: /Create a group/i }).should('exist');
-  });
-
-  it('opens via url', () => {
-    cy.visit('/zoom-api/operations/groupCreate');
-    cy.findByRole('heading', { name: /Create a group/i }).should('exist');
-  });
-
-  it('does not break on select dropdown', () => {
-    loadZoomApiPage();
-    cy.visit('/zoom-api/operations/groups');
-    cy.findByRole('button', { name: /request sample/i }).click();
-    cy.findByText('Obj-C').should('exist');
   });
 });
 
