d604dbe79b5e9ce916e28a22139b750f07f7bcec
Michael Miaskowski <mmiask92@gmail.com>
2021-08-13 16:33:12 +0200

    fix: address review comments

--- packages/elements-core/src/components/ResponseExamples/ResponseExamples.tsx
+++ packages/elements-core/src/components/ResponseExamples/ResponseExamples.tsx
@@ -1,4 +1,4 @@
-import { Box, Panel, Select, Text } from '@stoplight/mosaic';
+import { Button, Flex, Panel, Select, Text } from '@stoplight/mosaic';
 import { CodeViewer } from '@stoplight/mosaic-code-viewer';
 import { IHttpOperation, IMediaTypeContent } from '@stoplight/types';
 import React from 'react';
@@ -13,6 +13,8 @@ export interface ResponseExamplesProps {
 
 export const ResponseExamples = ({ httpOperation, responseMediaType, responseStatusCode }: ResponseExamplesProps) => {
   const [chosenExampleIndex, setChosenExampleIndex] = React.useState(0);
+  const [show, setShow] = React.useState<boolean>(false);
+  const [loading, setLoading] = React.useState<boolean>(false);
 
   const response = httpOperation.responses.find(response => response.code === responseStatusCode);
   const responseContents = response?.contents?.find(content => content.mediaType === responseMediaType);
@@ -24,6 +26,7 @@ export const ResponseExamples = ({ httpOperation, responseMediaType, responseSta
   const responseExample = useGenerateExampleFromMediaTypeContent(responseContents, chosenExampleIndex, {
     skipWriteOnly: true,
   });
+  const exceededSize = responseExample.split(/\r\n|\r|\n/).length > 500;
 
   if (!userDefinedExamples && responseMediaType !== 'application/json') return null;
 
@@ -40,34 +43,37 @@ export const ResponseExamples = ({ httpOperation, responseMediaType, responseSta
     />
   );
 
-  const ResponseExamplesViewer: React.FC<{ example: string }> = ({ example }) => {
-    const [show, setShow] = React.useState<boolean>(false);
-    const exceededSize = example.split(/\r\n|\r|\n/).length > 500;
-
-    if ((exceededSize && show) || !exceededSize)
-      return (
-        <CodeViewer
-          aria-label={example}
-          noCopyButton
-          maxHeight="400px"
-          language="json"
-          value={example}
-          showLineNumbers
-        />
-      );
-
-    return (
-      <Box p="auto" onClick={() => setShow(true)}>
-        This example is too big. Do you want to open it anyway?
-      </Box>
-    );
-  };
-
   return (
     <Panel rounded isCollapsible={false}>
       <Panel.Titlebar>{examplesSelect || <Text color="body">Response Example</Text>}</Panel.Titlebar>
       <Panel.Content p={0}>
-        <ResponseExamplesViewer example={responseExample} />
+        {(exceededSize && show) || !exceededSize ? (
+          <CodeViewer
+            aria-label={responseExample}
+            noCopyButton
+            maxHeight="400px"
+            language="json"
+            value={responseExample}
+            showLineNumbers
+          />
+        ) : (
+          <Flex flexDirection="col" justifyContent="center" alignItems="center" style={{ height: '400px' }}>
+            <Button
+              onPress={() => {
+                setLoading(true);
+                setTimeout(() => setShow(true), 50);
+              }}
+              appearance="minimal"
+              loading={loading}
+              disabled={loading}
+            >
+              {loading ? '...Loading' : 'Load examples'}
+            </Button>
+            <Text fontSize="base" textAlign="center">
+              Large examples are not rendered by default.
+            </Text>
+          </Flex>
+        )}
       </Panel.Content>
     </Panel>
   );
