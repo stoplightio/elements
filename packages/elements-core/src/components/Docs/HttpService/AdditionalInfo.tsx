cd9b52aa192f522a3a6aa0ac1e6afe7a1a4478cd
William Hilton <wmhilton@gmail.com>
2021-08-17 16:37:13 -0400

    chore: remove extra (<>{}</>)

--- packages/elements-core/src/components/Docs/HttpService/AdditionalInfo.tsx
+++ packages/elements-core/src/components/Docs/HttpService/AdditionalInfo.tsx
@@ -22,20 +22,16 @@ export const AdditionalInfo: React.FC<AdditionalInfoProps> = ({ termsOfService, 
   const licenseUrl = license?.url || `https://spdx.org/licenses/${license?.identifier}.html`;
   const licenseLink = license?.name && licenseUrl ? `[${license.name} License](${licenseUrl})` : '';
   const tosLink = termsOfService ? `[Terms of Service](${termsOfService})` : '';
-  return (
-    <>
-      {contactLink || licenseLink || tosLink ? (
-        <Panel rounded isCollapsible={false}>
-          <Panel.Titlebar bg="canvas-300">
-            <span role="heading">Additional Information</span>
-          </Panel.Titlebar>
-          <Panel.Content p={0}>
-            <Panel.Content>
-              <MarkdownViewer style={{ fontSize: 12 }} markdown={`${contactLink}\n \n${licenseLink}\n \n ${tosLink}`} />
-            </Panel.Content>
-          </Panel.Content>
-        </Panel>
-      ) : null}
-    </>
-  );
+  return contactLink || licenseLink || tosLink ? (
+    <Panel rounded isCollapsible={false}>
+      <Panel.Titlebar bg="canvas-300">
+        <span role="heading">Additional Information</span>
+      </Panel.Titlebar>
+      <Panel.Content p={0}>
+        <Panel.Content>
+          <MarkdownViewer style={{ fontSize: 12 }} markdown={`${contactLink}\n \n${licenseLink}\n \n ${tosLink}`} />
+        </Panel.Content>
+      </Panel.Content>
+    </Panel>
+  ) : null;
 };
