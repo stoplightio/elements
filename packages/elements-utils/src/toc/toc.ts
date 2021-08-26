ae9a022fc2e808083963a2c622f3aa2d34eb3fd7
Domagoj Kriskovic <dom@stoplight.io>
2021-08-26 15:32:58 +0200

    fix: group http ops and models together (#1726)
    * fix: group http ops and models together
    
    * chore: version bump
    
    * chore: cleanup

--- packages/elements-utils/src/toc/toc.ts
+++ packages/elements-utils/src/toc/toc.ts
@@ -245,9 +245,15 @@ function appendHttpServiceItemsToToC(toc: ITableOfContents) {
     { httpOperations, models }: { httpOperations: NodeData[]; models: NodeData[] },
     serviceTagNames: string[],
   ) => {
-    const { groups, others } = httpOperations.reduce<{
+    const operationsAndModels = [
+      ...httpOperations.map(i => ({ ...i, nodeType: NodeType.HttpOperation })),
+      ...models.map(i => ({ ...i, nodeType: NodeType.Model })),
+    ];
+
+    const { groups, ungroupedModels, ungroupedOperations } = operationsAndModels.reduce<{
       groups: { [key: string]: Group };
-      others: Item[];
+      ungroupedOperations: Item[];
+      ungroupedModels: NodeData[];
     }>(
       (result, subNode) => {
         const [tagName] = subNode.tags || [];
@@ -269,15 +275,19 @@ function appendHttpServiceItemsToToC(toc: ITableOfContents) {
             };
           }
         } else {
-          result.others.push(item);
+          if (subNode.nodeType === NodeType.HttpOperation) {
+            result.ungroupedOperations.push(item);
+          } else {
+            result.ungroupedModels.push(subNode);
+          }
         }
 
         return result;
       },
-      { groups: {}, others: [] },
+      { groups: {}, ungroupedOperations: [], ungroupedModels: [] },
     );
 
-    others.forEach(item => toc.items.push(item));
+    ungroupedOperations.forEach(item => toc.items.push(item));
 
     const tagNamesLC = serviceTagNames.map(tn => tn.toLowerCase());
 
@@ -298,7 +308,7 @@ function appendHttpServiceItemsToToC(toc: ITableOfContents) {
       })
       .forEach(([, group]) => toc.items.push(group));
 
-    appendModelsToToc(toc, 'group')(models);
+    appendModelsToToc(toc, 'group')(ungroupedModels);
   };
 }
 
