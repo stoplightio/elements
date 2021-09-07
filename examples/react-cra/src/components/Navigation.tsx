e6b65f61f53c340149be13d73b854b0a904363f5
paulatulis <paula@stoplight.io>
2021-09-07 12:08:20 -0500

    feat (dev-portal): add search component

--- examples/react-cra/src/components/Navigation.tsx
+++ examples/react-cra/src/components/Navigation.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { NavLink } from 'react-router-dom';
 
+import { Search } from './Search';
+
 export const Navigation = () => {
+  const queryClient = new QueryClient();
+
   return (
     <nav className="topnav">
       <NavLink to="/stoplight-project">Stoplight Project</NavLink>
       <NavLink to="/zoom-api">Zoom API</NavLink>
+      <QueryClientProvider client={queryClient}>
+        <Search projectIds={['cHJqOjYwNjYx']} workspaceId="d2s6NDE1NTU" />
+      </QueryClientProvider>
     </nav>
   );
 };
---
+++ examples/react-cra/src/components/Search.tsx
@@ -0,0 +1,39 @@
+import { Search as ElementsSearch, useGetNodes } from '@stoplight/elements-dev-portal';
+import * as React from 'react';
+
+export type SearchType = {
+  projectIds: string[];
+  workspaceId: string;
+};
+export const Search = ({ projectIds, workspaceId }: SearchType) => {
+  const [search, setSearch] = React.useState('');
+  const [open, setOpen] = React.useState(false);
+  const { data } = useGetNodes({
+    search,
+    projectIds,
+    workspaceId,
+  });
+
+  const handleClose = () => {
+    setOpen(false);
+    setSearch('');
+  };
+
+  const handleClick = () => {
+    console.log('clicked');
+  };
+
+  return (
+    <>
+      <input placeholder="Search..." style={{ color: 'white' }} onFocus={() => setOpen(true)} />
+      <ElementsSearch
+        search={search}
+        onSearch={setSearch}
+        onClick={handleClick}
+        onClose={handleClose}
+        isOpen={open}
+        searchResults={data}
+      />
+    </>
+  );
+};
