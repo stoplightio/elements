d6c4f133eef5f73e8e7ad6788279d21c032079e0
Mateusz Podlasin <mateusz@stoplight.io>
2021-11-04 11:16:49 +0100

    fix: rerenders and memoization and all that jazz (#1835)
    * fix: rerenders, memoization and all that jazz
    
    * chore: reverse changes in ref proxy tests
    
    * fix: linter errors
    
    * chore: add some unit tests and bring back old logic

--- packages/elements-core/src/context/InlineRefResolver.tsx
+++ packages/elements-core/src/context/InlineRefResolver.tsx
@@ -1,4 +1,4 @@
-import { isPlainObject } from 'lodash';
+import { isPlainObject } from '@stoplight/json';
 import * as React from 'react';
 import { useContext } from 'react';
 
@@ -8,25 +8,31 @@ import { createResolvedObject } from '../utils/ref-resolving/resolvedObject';
 const InlineRefResolverContext = React.createContext<ReferenceResolver | undefined>(undefined);
 InlineRefResolverContext.displayName = 'InlineRefResolverContext';
 
-const DocumentContext = React.createContext<unknown | undefined>(undefined);
+const DocumentContext = React.createContext<object | undefined>(undefined);
 DocumentContext.displayName = 'DocumentContext';
 
-type InlineRefResolverProviderProps =
-  | {
-      document: unknown;
-    }
-  | {
-      resolver: ReferenceResolver;
-    };
+type InlineRefResolverProviderProps = {
+  document?: unknown;
+  resolver?: ReferenceResolver;
+};
 
 /**
  * Populates `InlineRefResolverContext` with either a standard inline ref resolver based on `document`, or a custom resolver function provided by the caller.
  */
-export const InlineRefResolverProvider: React.FC<InlineRefResolverProviderProps> = ({ children, ...props }) => {
-  const document = 'document' in props && isPlainObject(props.document) ? Object(props.document) : undefined;
+export const InlineRefResolverProvider: React.FC<InlineRefResolverProviderProps> = ({
+  children,
+  document: maybeDocument,
+  resolver,
+}) => {
+  const document = isPlainObject(maybeDocument) ? maybeDocument : undefined;
+
+  const computedResolver = React.useMemo(
+    () => resolver || (document !== undefined ? defaultResolver(document) : undefined),
+    [document, resolver],
+  );
 
   return (
-    <InlineRefResolverContext.Provider value={'resolver' in props ? props.resolver : defaultResolver(document)}>
+    <InlineRefResolverContext.Provider value={computedResolver}>
       <DocumentContext.Provider value={document}>{children}</DocumentContext.Provider>
     </InlineRefResolverContext.Provider>
   );
@@ -40,5 +46,8 @@ export const useResolvedObject = (currentObject: object): object => {
   const document = useDocument();
   const resolver = useInlineRefResolver();
 
-  return createResolvedObject(currentObject, { contextObject: document as object, resolver });
+  return React.useMemo(
+    () => createResolvedObject(currentObject, { contextObject: document as object, resolver }),
+    [currentObject, document, resolver],
+  );
 };
