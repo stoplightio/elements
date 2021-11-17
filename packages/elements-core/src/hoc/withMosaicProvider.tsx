728c95c4d52173fd4bc0f96dd5971d488ae3b44c
Marc MacLeod <marc@stoplight.io>
2021-11-16 19:22:30 -0600

    fix: reuse mosaic, react-query context if possible

--- packages/elements-core/src/hoc/withMosaicProvider.tsx
+++ packages/elements-core/src/hoc/withMosaicProvider.tsx
@@ -1,10 +1,20 @@
-import { Provider as MosaicProvider } from '@stoplight/mosaic';
+import { Provider as MosaicProvider, useMosaicContext } from '@stoplight/mosaic';
 import React from 'react';
 
 import { getDisplayName } from './utils';
 
 export function withMosaicProvider<P>(WrappedComponent: React.ComponentType<P>): React.FC<P> {
   const WithMosaicProvider = (props: P) => {
+    try {
+      // if already have mosaic context in tree, use it rather than creating a new provider
+      const mosaicContext = useMosaicContext();
+      if (mosaicContext) {
+        return <WrappedComponent {...props} />;
+      }
+    } catch {
+      // no mosaic context yet in the tree (if `useMosaicContext` throws)
+    }
+
     return (
       <MosaicProvider style={{ height: '100%' }}>
         <WrappedComponent {...props} />
