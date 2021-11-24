72416ed8327a958d5e028f364ef3ad40c63a8e3d
Win Dalmijn <winbulk@gmail.com>
2021-11-24 16:08:31 +0100

    fix(core|dev-portal): linting and TS errors

--- packages/elements-core/src/hoc/withRouter.tsx
+++ packages/elements-core/src/hoc/withRouter.tsx
@@ -4,13 +4,13 @@ import { Route } from 'react-router-dom';
 import { MarkdownComponentsProvider } from '../components/MarkdownViewer/CustomComponents/Provider';
 import { ReactRouterMarkdownLink } from '../components/MarkdownViewer/CustomComponents/ReactRouterLink';
 import { useRouter } from '../hooks/useRouter';
-import { RoutingProps } from '../types';
+import { RouterType, RoutingProps } from '../types';
 import { getDisplayName } from './utils';
 
 export function withRouter<P extends RoutingProps>(WrappedComponent: React.ComponentType<P>): React.FC<P> {
   const WithRouter = (props: P) => {
     const basePath = props.basePath ?? '/';
-    const { Router, routerProps } = useRouter(props.router ?? 'history', basePath);
+    const { Router, routerProps } = useRouter(props.router ?? RouterType.History, basePath);
     return (
       <Router {...routerProps} key={basePath}>
         <Route path="/">
