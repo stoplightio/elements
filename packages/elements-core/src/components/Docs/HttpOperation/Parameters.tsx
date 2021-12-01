b44428b2652d24877909243cd5bc5f06d13b61f3
Marc MacLeod <marc@stoplight.io>
2021-12-01 16:20:05 -0500

    refactor: render params via JSV

--- packages/elements-core/src/components/Docs/HttpOperation/Parameters.tsx
+++ packages/elements-core/src/components/Docs/HttpOperation/Parameters.tsx
@@ -1,8 +1,7 @@
-import { Validations } from '@stoplight/json-schema-viewer';
-import { MarkdownViewer } from '@stoplight/markdown-viewer';
-import { Box, Flex, VStack } from '@stoplight/mosaic';
-import { Dictionary, HttpParamStyles, IHttpParam } from '@stoplight/types';
-import { get, isEmpty, omit, omitBy, sortBy } from 'lodash';
+import { JsonSchemaViewer } from '@stoplight/json-schema-viewer';
+import { HttpParamStyles, IHttpParam } from '@stoplight/types';
+import type { JSONSchema7Object } from 'json-schema';
+import { sortBy } from 'lodash';
 import * as React from 'react';
 
 import { isNodeExample } from '../../../utils/http-spec/examples';
@@ -32,113 +31,62 @@ const defaultStyle = {
 } as const;
 
 export const Parameters: React.FunctionComponent<ParametersProps> = ({ parameters, parameterType }) => {
-  if (!parameters || !parameters.length) return null;
-
-  return (
-    <VStack spacing={2} divider={<Box borderT borderColor="light" w="full"></Box>}>
-      {sortBy(parameters, ['required', 'name']).map(parameter => {
-        return <Parameter key={parameter.name} parameter={parameter} parameterType={parameterType} />;
-      })}
-    </VStack>
+  const schema = React.useMemo(
+    () => httpOperationParamsToSchema({ parameters, parameterType }),
+    [parameters, parameterType],
   );
+
+  if (!schema) return null;
+
+  return <JsonSchemaViewer schema={schema} disableCrumbs />;
 };
 Parameters.displayName = 'HttpOperation.Parameters';
 
-interface IParameterProps {
-  parameter: IHttpParam;
-  parameterType: ParameterType;
-}
+const httpOperationParamsToSchema = ({ parameters, parameterType }: ParametersProps): JSONSchema7Object | null => {
+  if (!parameters || !parameters.length) return null;
 
-export const Parameter: React.FunctionComponent<IParameterProps> = ({ parameter, parameterType }) => {
-  if (!parameter) return null;
+  const schema = {
+    properties: {},
+    required: [],
+  };
 
-  // TODO (CL): This can be removed when http operations are fixed https://github.com/stoplightio/http-spec/issues/26
-  const description = get(parameter, 'description') || get(parameter, 'schema.description');
+  const sortedParams = sortBy(parameters, ['required', 'name']);
 
-  const rootType = get(parameter, 'schema.type', 'unknown');
-  const type =
-    parameter.schema?.items?.['type'] && rootType === 'array' ? `array[${parameter.schema.items['type']}]` : rootType;
+  for (const p of sortedParams) {
+    if (!p.schema) continue;
 
-  const format = parameter.schema?.format;
+    const { name, description, required, deprecated, examples, style } = p;
 
-  // TODO (JJ): schema.deprecated is used in platform - to be removed once it's updated https://github.com/stoplightio/platform-internal/issues/2267
-  const deprecated = get(parameter, 'deprecated') || get(parameter, 'schema.deprecated', false);
+    const paramExamples =
+      examples?.map(example => {
+        if (isNodeExample(example)) {
+          return example.value;
+        }
 
-  const parameterExamples =
-    parameter.examples?.map(example => {
-      if (isNodeExample(example)) {
-        return example.value;
-      }
+        return example.externalValue;
+      }) || [];
+    const schemaExamples = p.schema?.examples;
+    const schemaExamplesArray = Array.isArray(schemaExamples) ? schemaExamples : [];
 
-      return example.externalValue;
-    }) || [];
+    // TODO (CL): This can be removed when http operations are fixed https://github.com/stoplightio/http-spec/issues/26
+    const paramDescription = description || p.schema.description;
 
-  const schemaExamples = parameter.schema?.examples;
-  const schemaExamplesArray = Array.isArray(schemaExamples) ? schemaExamples : [];
+    const paramDeprecated = deprecated || (p.schema as any).deprecated;
+    const paramStyle = style && defaultStyle[parameterType] !== style ? readableStyles[style] || style : undefined;
 
-  const validations = omitBy(
-    {
-      ...omit(parameter, ['name', 'required', 'deprecated', 'description', 'schema', 'style', 'examples']),
-      ...omit(get(parameter, 'schema'), ['description', 'type', 'deprecated']),
-      ...omit(get(parameter, 'schema.items'), ['description', 'type', 'deprecated']),
-      examples: [...parameterExamples, ...schemaExamplesArray],
-    },
-    // Remove empty arrays and objects
-    value => (typeof value === 'object' && isEmpty(value)) || typeof value === 'undefined',
-  ) as Dictionary<unknown, string>;
+    schema.properties![p.name] = {
+      ...p.schema,
+      description: paramDescription,
+      examples: [...paramExamples, ...schemaExamplesArray],
+      deprecated: paramDeprecated,
+      style: paramStyle,
+    };
 
-  return (
-    <Box className="HttpOperation__Parameters">
-      <Flex alignItems="center" my={2}>
-        <Flex alignItems="baseline" fontSize="base" flex={1}>
-          <Box fontFamily="mono" fontWeight="bold">
-            {parameter.name}
-          </Box>
-          <Box ml={2} color="muted">
-            {format ? `${type}<${format}>` : type}
-          </Box>
-        </Flex>
-        <Box fontSize="sm" color="warning">
-          {deprecated && (
-            <Box as="span" ml={2}>
-              deprecated
-            </Box>
-          )}
-          {parameter.required && (
-            <Box as="span" ml={2}>
-              required
-            </Box>
-          )}
-        </Box>
-      </Flex>
+    if (required) {
+      // @ts-expect-error
+      schema.required!.push(name);
+    }
+  }
 
-      {description && (
-        <Box w="full" color="muted" fontSize="sm" my={2}>
-          <MarkdownViewer markdown={description} />
-        </Box>
-      )}
-
-      <Box fontSize="sm">
-        <Validations validations={validations} />
-      </Box>
-
-      {parameter.style && defaultStyle[parameterType] !== parameter.style && (
-        <Flex my={2}>
-          <Box
-            as="span"
-            px={1}
-            color="muted"
-            fontFamily="mono"
-            rounded="lg"
-            fontSize="sm"
-            textTransform="capitalize"
-            style={{ backgroundColor: '#EDF2F7' }}
-          >
-            {readableStyles[parameter.style] || parameter.style}
-          </Box>
-        </Flex>
-      )}
-    </Box>
-  );
+  return schema;
 };
-Parameter.displayName = 'HttpOperation.Parameter';
