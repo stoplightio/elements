c31e983d948228d8b66279b4adf4aff688e3c651
Jakub Rożek <jakub@stoplight.io>
2022-04-12 08:05:47 +0200

    feat: package compliant with Node.js ESM (#2069)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,11 @@
-const {ProvidePlugin} = require("webpack");
+'use strict';
+const { ProvidePlugin } = require('webpack');
 
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   core: {
-    builder: "webpack5",
+    builder: 'webpack5',
   },
   stories: ['../src/**/*.stories.{js,jsx,ts,tsx}'],
   addons: [
@@ -15,30 +16,49 @@ module.exports = {
     {
       name: '@storybook/addon-postcss',
       options: {
-        postcssLoaderOptions: {implementation: require('postcss')}
-      }
-    }
+        postcssLoaderOptions: { implementation: require('postcss') },
+      },
+    },
   ],
+  babel: async () => {
+    return {
+      presets: [
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+        [
+          '@babel/preset-env',
+          {
+            targets: 'last 2 Chrome versions, last 2 Firefox versions, last 1 Safari version',
+            modules: 'commonjs',
+          },
+        ],
+      ],
+    };
+  },
   webpackFinal: config => {
     config.resolve.plugins = config.resolve.plugins || [];
     config.resolve.plugins.push(new TsconfigPathsPlugin());
 
     config.resolve.fallback = {
-      "stream": false,
-      "path": false,
-      "process": false
+      stream: false,
+      path: false,
+      process: false,
     };
+
+    config.resolve.conditionNames = ['require', 'default'];
 
     config.module.rules.push({
       test: /\.m?js/,
       resolve: {
-        fullySpecified: false
-      }
+        fullySpecified: false,
+      },
     });
 
-    config.plugins.push(new ProvidePlugin({
-      process: require.resolve('process/browser'),
-    }));
+    config.plugins.push(
+      new ProvidePlugin({
+        process: require.resolve('process/browser'),
+      }),
+    );
 
     return config;
   },
