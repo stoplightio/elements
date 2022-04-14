ad67f48fe752518c826f69c9972d35eb1904125f
kaylachun <kayla.chun@stoplight.io>
2022-04-14 14:47:27 -0700

    chore: cleanup to pass tests

--- packages/elements-core/src/components/TryIt/Response/ReponseCodeViewer.tsx
+++ packages/elements-core/src/components/TryIt/Response/ReponseCodeViewer.tsx
@@ -1,27 +1,23 @@
 import { CodeViewer, CodeViewerProps } from '@stoplight/mosaic-code-viewer';
 import React from 'react';
 
-import { Loading } from '../../Loading/Loading';
-import { useExampleLineCount } from './hooks/useExampleLineCount';
+import { useLineCount } from './hooks/useLineCount';
 
 const MAX_HIGHLIGHT_LINE_COUNT = 10000;
 
 export const ResponseCodeViewer: <E extends React.ElementType = 'pre'>(
   props: CodeViewerProps<E>,
 ) => React.ReactElement | null = ({ value, ...rest }) => {
-  const exampleLineCount = useExampleLineCount({ example: value });
-  if (!exampleLineCount) {
-    return <Loading />;
-  }
+  const lineCount = useLineCount({ example: value });
 
-  if (exampleLineCount < MAX_HIGHLIGHT_LINE_COUNT) {
+  if (lineCount < MAX_HIGHLIGHT_LINE_COUNT) {
     return <CodeViewer language="json" value={value} />;
   }
 
   return (
     <CodeViewer
       language="json"
-      showAsRaw={MAX_HIGHLIGHT_LINE_COUNT < exampleLineCount}
+      showAsRaw={MAX_HIGHLIGHT_LINE_COUNT < lineCount}
       style={{
         color: 'white',
       }}
--- packages/elements-core/src/components/TryIt/Response/hooks/useLineCount.ts
+++ packages/elements-core/src/components/TryIt/Response/hooks/useLineCount.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export const useExampleLineCount = ({ example }: { example: string }) =>
+export const useLineCount = ({ example }: { example: string }) =>
   React.useMemo(() => {
     const lines = /\r?\n/g;
     return (example.match(lines) || []).length;
