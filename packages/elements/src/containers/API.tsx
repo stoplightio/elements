587d8c30ec21864ca2b794ec1ad30a77f8705e2c
Jakub Rożek <jakub@stoplight.io>
2022-08-21 16:37:42 +0200

    fix(elements): keep unresolvable hashes when hash router is used (#2220)

--- packages/elements/src/containers/API.tsx
+++ packages/elements/src/containers/API.tsx
@@ -181,6 +181,7 @@ export const APIImpl: React.FC<APIProps> = props => {
           exportProps={exportProps}
           tryItCredentialsPolicy={tryItCredentialsPolicy}
           tryItCorsProxy={tryItCorsProxy}
+          router={props.router}
         />
       )}
     </InlineRefResolverProvider>
