20593fe8c2fcfeef76d53dd03151183ecac2ed65
Jakub Rożek <jakub@stoplight.io>
2022-09-14 23:08:02 +0200

    chore: storybook typings

--- packages/elements-core/src/components/Docs/story-helper.ts
+++ packages/elements-core/src/components/Docs/story-helper.ts
@@ -1,13 +1,16 @@
-import { Meta, Story } from '@storybook/react';
+import type { ErrorBoundaryProps } from '@stoplight/react-error-boundary';
+import type { Meta, Story } from '@storybook/react';
 import * as React from 'react';
 
-interface HelperReturn<P> {
+type DocsProps = { data: unknown } & ErrorBoundaryProps;
+
+interface HelperReturn<P extends Partial<DocsProps>> {
   meta: Meta<P>;
   createStory(name: string, input: Partial<P>): Story<P>;
   createHoistedStory(input: Partial<P>): Story<P>;
 }
 
-export const createStoriesForDocsComponent = <P>(
+export const createStoriesForDocsComponent = <P extends Partial<DocsProps> = DocsProps>(
   Component: React.ComponentType<P>,
   title?: string,
 ): HelperReturn<P> => {
@@ -21,7 +24,7 @@ export const createStoriesForDocsComponent = <P>(
     meta: {
       title: `Internal/Docs/${title || Component.displayName}`,
       component: Component,
-      argTypes: {
+      argTypes: <HelperReturn<P>['meta']['argTypes']>{
         data: {
           control: { type: 'object' },
         },
