5b0968370ed79d5ce53960696d410e55da47a472
Marc MacLeod <marc@stoplight.io>
2022-09-15 16:12:39 -0500

    fix: do not use custom link heading if no router + do release (#2244)
    * fix: do not use custom link heading if no router
    
    * chore: release
    
    * chore: trigger netlify

--- packages/elements-core/src/components/LinkHeading.tsx
+++ packages/elements-core/src/components/LinkHeading.tsx
@@ -1,14 +1,21 @@
-import { LinkHeading as MosaicLinkedHeading, LinkHeadingProps } from '@stoplight/mosaic';
+import { LinkHeading as MosaicLinkHeading, LinkHeadingProps } from '@stoplight/mosaic';
 import * as React from 'react';
 import { useLocation } from 'react-router-dom';
 
-import { RouterTypeContext } from '../context/RouterType';
+import { RouterTypeContext, useRouterType } from '../context/RouterType';
 
-export const LinkHeading = React.memo<LinkHeadingProps>(function LinkHeading({ id: _id, ...props }) {
+export const LinkHeading = React.memo<LinkHeadingProps>(function LinkHeading(props) {
+  const isUsingRouter = !!useRouterType();
+  const Comp = isUsingRouter ? CustomLinkHeading : MosaicLinkHeading;
+
+  return <Comp {...props} />;
+});
+
+const CustomLinkHeading = React.memo<LinkHeadingProps>(function LinkHeading({ id: _id, ...props }) {
   const { pathname } = useLocation();
   const routerKind = React.useContext(RouterTypeContext);
   const route = pathname.split('#')[0];
   const id = routerKind === 'hash' ? `${route}#${_id}` : _id;
 
-  return <MosaicLinkedHeading id={id} {...props} />;
+  return <MosaicLinkHeading id={id} {...props} />;
 });
