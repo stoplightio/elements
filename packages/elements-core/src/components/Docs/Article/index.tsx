61e95b81e382570338aa0f8b930d3f4e2078d366
Jakub Rożek <jakub@stoplight.io>
2023-03-09 13:41:48 +0100

    fix(elements-core): toc links yield 404 when using hash router (#2331)

--- packages/elements-core/src/components/Docs/Article/index.tsx
+++ packages/elements-core/src/components/Docs/Article/index.tsx
@@ -2,16 +2,22 @@ import { IMarkdownViewerProps } from '@stoplight/markdown-viewer';
 import { Box } from '@stoplight/mosaic';
 import { withErrorBoundary } from '@stoplight/react-error-boundary';
 import * as React from 'react';
+import { useLocation } from 'react-router-dom';
 
+import { RouterTypeContext } from '../../../context/RouterType';
 import { MarkdownViewer } from '../../MarkdownViewer';
 import { DocsComponentProps } from '..';
 
 type ArticleProps = DocsComponentProps<IMarkdownViewerProps['markdown']>;
 
 const ArticleComponent = React.memo<ArticleProps>(({ data }) => {
+  const { pathname } = useLocation();
+  const routerKind = React.useContext(RouterTypeContext);
+  const basePath = routerKind === 'hash' ? `#${pathname.split('#')[0]}` : '';
+
   return (
     <Box className="sl-elements-article">
-      <MarkdownViewer className="sl-elements-article-content" markdown={data} includeToc />
+      <MarkdownViewer className="sl-elements-article-content" markdown={data} includeToc tocBasePath={basePath} />
     </Box>
   );
 });
