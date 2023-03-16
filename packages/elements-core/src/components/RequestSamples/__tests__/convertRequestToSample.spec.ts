2bd60658992f7040c3b24d22bc14b0668d1144c9
Jakub Rożek <jakub@stoplight.io>
2023-03-16 16:53:08 +0100

    test(elements-core): snapshot example code examples

---
+++ packages/elements-core/src/components/RequestSamples/__tests__/convertRequestToSample.spec.ts
@@ -0,0 +1,69 @@
+import { convertRequestToSample } from '../convertRequestToSample';
+import { requestSampleConfigs } from '../requestSampleConfigs';
+
+const har = {
+  method: 'PUT',
+  url: 'https://todos.stoplight.io/todos/todoId',
+  httpVersion: 'HTTP/1.1',
+  cookies: [],
+  headers: [
+    {
+      name: 'Content-Type',
+      value: 'application/json',
+    },
+    {
+      name: 'account-id',
+      value: 'account-id-default',
+    },
+    {
+      name: 'message-id',
+      value: 'example value',
+    },
+    {
+      name: 'optional_header',
+      value: '',
+    },
+    {
+      name: 'quote',
+      value: '',
+    },
+  ],
+  queryString: [
+    {
+      name: 'value',
+      value: '1',
+    },
+    {
+      name: 'type',
+      value: 'something',
+    },
+    {
+      name: 'API Key',
+      value: '',
+    },
+  ],
+  postData: {
+    mimeType: 'application/json',
+    text: '{\n  "name": "Docs Module",\n  "completed": false\n}',
+    jsonObj: {
+      name: 'Docs Module',
+      completed: false,
+    },
+  },
+  headersSize: -1,
+  bodySize: -1,
+};
+
+const languages = Object.values(requestSampleConfigs).flatMap<string>(({ httpSnippetLanguage, libraries }) => {
+  if (!libraries) {
+    return httpSnippetLanguage;
+  }
+
+  return Object.values(libraries).map(({ httpSnippetLibrary }) => `${httpSnippetLanguage} / ${httpSnippetLibrary}`);
+});
+
+test.each(languages)('given %s, convertRequestToSample converts a request to a sample', input => {
+  const [language, library] = input.split(' / ');
+
+  expect(convertRequestToSample(language, library, har)).toMatchSnapshot();
+});
