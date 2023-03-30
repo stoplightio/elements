c1733960eee3c6a5f048e4fc5345e43d7df4b1d5
chohmann <chelsea.hohmann@gmail.com>
2023-03-30 16:50:15 -0500

    fix: schema property examples are no longer incorrectly arrays

--- packages/elements-core/src/components/TryIt/Parameters/parameter-utils.spec.ts
+++ packages/elements-core/src/components/TryIt/Parameters/parameter-utils.spec.ts
@@ -1,8 +1,9 @@
+import { JSONSchema7Definition } from 'json-schema';
 import { httpOperation } from '../../../__fixtures__/operations/put-todos';
-import { initialParameterValues } from './parameter-utils';
+import { initialParameterValues, mapSchemaPropertiesToParameters } from './parameter-utils';
 
 describe('Parameter Utils', () => {
-  describe(initialParameterValues.name, () => {
+  describe('initialParameterValues.name', () => {
     it('should fill initial parameters', () => {
       const allParameters = [
         ...(httpOperation.request?.path ?? []),
@@ -18,6 +19,65 @@ describe('Parameter Utils', () => {
         'account-id': 'account-id-default',
         'message-id': 'example value',
       });
+    });
+  });
+
+  describe('mapSchemaPropertiesToParameters.name', () => {
+    it('should convert schema properties to parameters', () => {
+      const properties: { [key: string]: JSONSchema7Definition } = {
+        withExamples: { type: 'string', examples: ['blah'] },
+        withEmptyExamples: { type: 'string', examples: [] },
+        withNoExamples: { type: 'string' },
+        requiredWithExamples: { type: 'string', examples: ['blah'] },
+        requiredWithEmptyExamples: { type: 'string', examples: [] },
+        requiredWithNoExamples: { type: 'string' },
+        requiredBoolean: true,
+        boolean: false,
+      };
+      const required = [
+        'requiredWithExamples',
+        'requiredWithEmptyExamples',
+        'requiredWithNoExamples',
+        'requiredBoolean',
+      ];
+      expect(mapSchemaPropertiesToParameters(properties, required)).toEqual([
+        {
+          name: 'withExamples',
+          schema: { type: 'string', examples: ['blah'] },
+          examples: [{ key: 'example', value: 'blah' }],
+        },
+        {
+          name: 'withEmptyExamples',
+          schema: { type: 'string', examples: [] },
+        },
+        {
+          name: 'withNoExamples',
+          schema: { type: 'string' },
+        },
+        {
+          name: 'requiredWithExamples',
+          schema: { type: 'string', examples: ['blah'] },
+          examples: [{ key: 'example', value: 'blah' }],
+          required: true,
+        },
+        {
+          name: 'requiredWithEmptyExamples',
+          schema: { type: 'string', examples: [] },
+          required: true,
+        },
+        {
+          name: 'requiredWithNoExamples',
+          schema: { type: 'string' },
+          required: true,
+        },
+        {
+          name: 'requiredBoolean',
+          required: true,
+        },
+        {
+          name: 'boolean',
+        },
+      ]);
     });
   });
 });
