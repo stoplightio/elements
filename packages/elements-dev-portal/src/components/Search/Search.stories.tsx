eca78baede62bbd360acb948c071363b90fe7e3b
Kayla Chun <58235624+kaylachun@users.noreply.github.com>
2023-08-31 12:18:01 -0700

    feat(elements-dev-portal): embedded search component (#2397)
    * chore(search): extract search results
    
    * feat(search): embedded search
    
    * feat(search): extract input
    
    * feat(search): searchResults component
    
    * chore(search): wrap new search component
    
    * chore(search): cleanup
    
    * chore(search): toggle divider
    
    * chore(search): export searchresult component
    
    * v1.13.0
    
    * chore(elements-dev-portal): bump version

--- packages/elements-dev-portal/src/components/Search/Search.stories.tsx
+++ packages/elements-dev-portal/src/components/Search/Search.stories.tsx
@@ -1,11 +1,12 @@
-import { Input, useModalState } from '@stoplight/mosaic';
+import { faSearch, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { Box, Icon, Input, useModalState } from '@stoplight/mosaic';
 import { Story } from '@storybook/react';
 import * as React from 'react';
 
 import { useGetNodes } from '../../hooks/useGetNodes';
 import { useGetWorkspace } from '../../hooks/useGetWorkspace';
 import { NodeSearchResult } from '../../types';
-import { Search } from './';
+import { Search, SearchResults } from './';
 
 type SearchWrapperProps = { projectIds: string[]; workspaceId: string };
 // Wrapper to show how to use the node content hook
@@ -53,6 +54,46 @@ const SearchWrapper = ({ projectIds, workspaceId }: SearchWrapperProps) => {
   );
 };
 
+const EmbeddedSearchWrapper = ({ projectIds, workspaceId }: SearchWrapperProps) => {
+  const [search, setSearch] = React.useState('');
+  const { data, isFetching } = useGetNodes({
+    search,
+    projectIds,
+    workspaceId,
+  });
+
+  const { data: workspace } = useGetWorkspace({
+    projectIds,
+  });
+
+  const handleClick = (searchResult: NodeSearchResult) => {
+    console.log('Search clicked', searchResult);
+    window.open(
+      `https://${workspace?.workspace.slug}.stoplight.io/docs/${searchResult.project_slug}${searchResult.uri}`,
+      '_blank',
+    );
+  };
+
+  return (
+    <>
+      <Input
+        appearance="minimal"
+        borderB
+        icon={<Box as={Icon} ml={1} icon={isFetching ? faSpinner : faSearch} spin={isFetching} />}
+        autoFocus
+        placeholder="Search..."
+        value={search}
+        onChange={e => {
+          setSearch(e.currentTarget.value);
+        }}
+        type="search"
+      />
+      <Box p={5}>
+        <SearchResults searchResults={data} onClick={handleClick} isEmbedded={true} />
+      </Box>
+    </>
+  );
+};
 export default {
   title: 'Public/Search',
   component: SearchWrapper,
@@ -69,5 +110,8 @@ export default {
 };
 
 export const Playground: Story<SearchWrapperProps> = args => <SearchWrapper {...args} />;
+export const EmbeddedSearch: Story<SearchWrapperProps> = args => <EmbeddedSearchWrapper {...args} />;
 
 Playground.storyName = 'Studio Demo';
+
+EmbeddedSearch.storyName = 'Embedded Search';
