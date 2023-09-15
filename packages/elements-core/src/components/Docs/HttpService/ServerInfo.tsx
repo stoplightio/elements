575ddc19655cee66687ecf1869c81eec193f69c4
kaylachun <kayla.chun@stoplight.io>
2023-09-15 15:41:12 -0700

    chore: remove wordbreak

--- packages/elements-core/src/components/Docs/HttpService/ServerInfo.tsx
+++ packages/elements-core/src/components/Docs/HttpService/ServerInfo.tsx
@@ -85,7 +85,7 @@ const ServerUrl: React.FC<IServer & { hasAnyServerVariables: boolean; defaultIsO
         <Tooltip
           placement="right"
           renderTrigger={() => (
-            <Text aria-label={description} whitespace="normal" wordBreak="words">
+            <Text aria-label={description} whitespace="normal">
               {urlFragments.map(({ kind, value }, i) => (
                 <Text key={i} fontWeight={kind === 'variable' ? 'semibold' : 'normal'}>
                   {value}
