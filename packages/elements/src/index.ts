4e73b721ffb853601cdfe47f66765aefdda13c3d
Jakub Jankowski <jakubj@stoplight.io>
2023-11-10 03:37:57 +0100

    feat: export oas utils (#2440)
    * feat: export oas utils
    
    * chore: release

--- packages/elements/src/index.ts
+++ packages/elements/src/index.ts
@@ -1,2 +1,4 @@
 export type { APIProps } from './containers/API';
 export { API } from './containers/API';
+export { useExportDocumentProps } from './hooks/useExportDocumentProps';
+export { transformOasToServiceNode } from './utils/oas';
