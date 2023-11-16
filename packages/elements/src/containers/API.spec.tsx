ee9f9843429d9b2f3716d2fca8748a8d1bff28a3
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2023-11-16 13:21:10 +0000

    fix: update imports

--- packages/elements/src/containers/API.spec.tsx
+++ packages/elements/src/containers/API.spec.tsx
@@ -6,7 +6,8 @@ import {
   withQueryClientProvider,
   withStyles,
 } from '@jpmorganchase/elemental-core';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { createMemoryHistory } from 'history';
 import { flow } from 'lodash';
 import * as React from 'react';
