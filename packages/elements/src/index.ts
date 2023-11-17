533e0046b84d005b71b0048d9880d118902dd3bb
Jakub Jankowski <jakubj@stoplight.io>
2023-11-18 03:57:59 +0800

    chore: export stacked layout (#2452)
    * chore: export stacked layout
    
    * chore: showPoweredByLink property

--- packages/elements/src/index.ts
+++ packages/elements/src/index.ts
@@ -1,3 +1,4 @@
+export { APIWithStackedLayout } from './components/API/APIWithStackedLayout';
 export type { APIProps } from './containers/API';
 export { API } from './containers/API';
 export { useExportDocumentProps } from './hooks/useExportDocumentProps';
