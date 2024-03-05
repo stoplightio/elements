bda3ca22ae147a4bad528595c9d261fa3158d3e3
kaylachun <kayla.chun@stoplight.io>
2024-03-05 14:31:36 -0800

    chore: increase cypress timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -15,5 +15,6 @@ export default defineConfig({
       return require('./cypress/plugins/index.js')(on, config);
     },
     baseUrl: 'http://localhost:4200/',
+    defaultCommandTimeout: 5000,
   },
 });
