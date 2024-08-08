844ff95808d9134b270bf5f4d3aa68e0525ce2f7
Darek Pławecki <dariusz.plawecki@smartbear.com>
2024-08-08 13:46:08 +0200

    chore(examples): Remove Gatsby example (#2636)
    * chore(examples): Remove gatsby example
    
    * chore(examples): Force the use of compatible version of @babel/core

--- use-local-elements.sh
+++ use-local-elements.sh
@@ -3,11 +3,10 @@ example=$1
 
 cd ./examples-dev/$example
 
-npx json -I -f package.json -e "this.resolutions={
-\"@jpmorganchase/elemental\": \"file:../../packages/elements/dist\",
-\"@jpmorganchase/elemental-dev-portal\": \"file:../../packages/elements-dev-portal/dist\",
-\"@jpmorganchase/elemental-core\": \"file:../../packages/elements-core/dist\"
-}"
+npx json -I -f package.json -e "this.resolutions=this.resolutions || {}"
+npx json -I -f package.json -e "this.resolutions[\"@jpmorganchase/elemental\"]=\"file:../../packages/elements/dist\""
+npx json -I -f package.json -e "this.resolutions[\"@jpmorganchase/elemental-dev-portal\"]=\"file:../../packages/elements-dev-portal/dist\""
+npx json -I -f package.json -e "this.resolutions[\"@jpmorganchase/elemental-core\"]=\"file:../../packages/elements-core/dist\""
 
 npx json -I -f package.json -e "this.dependencies[\"@jpmorganchase/elemental\"]=\"file:../../packages/elements/dist\""
 npx json -I -f package.json -e "this.dependencies[\"@jpmorganchase/elemental-dev-portal\"]=\"file:../../packages/elements-dev-portal/dist\""
