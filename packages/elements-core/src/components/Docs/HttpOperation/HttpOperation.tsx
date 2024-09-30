dc778f3975a8d37c14308f2f04eae9d74b6c22ef
SB-venkatyadavilli <165135356+SB-venkatyadavilli@users.noreply.github.com>
2024-09-30 16:58:06 +0530

    fix:copy paste URL & bumping the Elements versions (#2714)
    fix/979 merging into main branch

--- packages/elements-core/src/components/Docs/HttpOperation/HttpOperation.tsx
+++ packages/elements-core/src/components/Docs/HttpOperation/HttpOperation.tsx
@@ -170,7 +170,7 @@ function MethodPathInner({ method, path, chosenServerUrl }: MethodPathProps & { 
 
   const pathElem = (
     <Flex overflowX="hidden" fontSize="lg" userSelect="all">
-      <Box dir="rtl" color="muted" textOverflow="truncate" overflowX="hidden">
+      <Box as="span" dir="rtl" color="muted" textOverflow="truncate" overflowX="hidden">
         <Box as="span" dir="ltr" style={{ unicodeBidi: 'bidi-override' }}>
           {chosenServerUrl}
           {path}
