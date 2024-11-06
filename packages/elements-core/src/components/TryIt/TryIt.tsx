1bf1d3f3e1b0f6751e579d6ffaeb44cb531d48ef
Kyle Johnston <kyle9657@gmail.com>
2024-11-06 12:16:15 +0000

    feat: re organise some imports

--- packages/elements-core/src/components/TryIt/TryIt.tsx
+++ packages/elements-core/src/components/TryIt/TryIt.tsx
@@ -1,9 +1,10 @@
+import { useAtom } from 'jotai';
+import * as React from 'react';
 import ExamplesContext from '@jpmorganchase/elemental-core/context/ExamplesContext';
 import { Box, Button, HStack, Icon, Menu, MenuItems, Panel, useThemeIsDark } from '@stoplight/mosaic';
 import type { IHttpOperation, IMediaTypeContent, IServer } from '@stoplight/types';
 import { Request as HarRequest } from 'har-format';
-import { useAtom } from 'jotai';
-import * as React from 'react';
+
 
 import { HttpMethodColors } from '../../constants';
 import { getServersToDisplay, getServerVariables } from '../../utils/http-spec/IServer';
