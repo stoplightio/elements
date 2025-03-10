7f5a648529c2bccaab1cd5f4cbc804e4c227f879
Pradeep Bande <pradeep.bande@smartbear.com>
2025-03-10 17:55:57 +0530

    fix: fixed issue of Improper encoding of curly braces in Request Sample

--- packages/elements-core/src/components/RequestSamples/convertRequestToSample.ts
+++ packages/elements-core/src/components/RequestSamples/convertRequestToSample.ts
@@ -16,10 +16,36 @@ export const convertRequestToSample = async (
     } else {
       converted = converted || null;
     }
+    
+    // This code checks if the 'converted' string is a valid string, decodes any URL-encoded curly braces in it, 
+    // and then iterates through each query parameter in the request's query string. For any query parameter 
+    // containing curly braces, it selectively encodes the characters while preserving the curly braces. 
+    // The decoded query string is then replaced with the fully encoded query in the 'converted' string to ensure 
+    // proper encoding of the query parameters while keeping curly braces intact.
+
     if (typeof converted === 'string') {
       converted = converted.replace(/%7B/g, '{').replace(/%7D/g, '}');
-    }
 
+      if (request?.queryString?.length) {
+          for (const query of request.queryString) {
+              if (query?.value.includes('{') || query?.value.includes('}')) {
+                let encodeValue = ''
+                for(const val of query?.value){
+                  if(val !== '{' && val !== '}'){
+                    encodeValue += encodeURIComponent(val)
+                  } else{
+                    encodeValue += val
+                  }
+                }
+                const decodedQuery = `${query?.name}=${encodeValue}`;
+                const encodedQuery = `${query?.name}=${encodeURIComponent(query?.value)}`
+
+                converted = converted.replace(decodedQuery, encodedQuery);
+              }
+          }
+      }
+    }
+  
     return converted;
   } catch (err) {
     console.error(err);
