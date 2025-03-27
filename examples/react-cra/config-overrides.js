7bdc5f0575aeca5fb0af695b84c1843d10c6ecd0
Dariusz Plawecki <dariusz.plawecki@smartbear.com>
2025-03-27 13:21:16 +0100

    chore: clean config and commit yarn.lock

--- examples/react-cra/config-overrides.js
+++
@@ -1,6 +0,0 @@
-// Overrides create-react-app webpack configs without ejecting
-// https://github.com/timarney/react-app-rewired
-
-const { addBabelPlugins, override } = require('customize-cra');
-
-module.exports = override(...addBabelPlugins('@babel/plugin-proposal-optional-chaining'));
