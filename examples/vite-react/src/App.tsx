417800fd7c3d7e0c5da1e2191fc29d81f2d2bd51
Sanjay <54913971+San1ay@users.noreply.github.com>
2025-06-11 00:24:41 +0530

    added react vite example

---
+++ examples/vite-react/src/App.tsx
@@ -0,0 +1,16 @@
+// @ts-ignore
+import {API} from "@stoplight/elements"
+import '@stoplight/elements/styles.min.css';
+
+function App() {
+
+  return (
+    <>
+     <API
+  apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml"
+/>
+    </>
+  )
+}
+
+export default App
---
+++ examples/vite-react/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),nodePolyfills()],
+})
