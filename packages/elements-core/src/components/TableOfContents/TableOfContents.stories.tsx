acef357dd78310fe2ac9fca86dd2f09c894cfd6a
SB-venkatyadavilli <venkat.yadavilli@smartbear.com>
2025-11-12 16:05:32 +0530

    fix:added groupId & groupIndex in TableOfContents.stories.tsx

--- packages/elements-core/src/components/TableOfContents/TableOfContents.stories.tsx
+++ packages/elements-core/src/components/TableOfContents/TableOfContents.stories.tsx
@@ -21,17 +21,13 @@ export const Playground: Story<TableOfContentsProps> = args => (
 Playground.storyName = 'Todo API';
 Playground.args = {
   tree: [
-    {
-      id: '/',
-      slug: '/',
-      title: 'Overview',
-      type: 'overview',
-      meta: '',
-    },
+    { groupId: 0, groupIndex: 0, id: '/', slug: '/', title: 'Overview', type: 'overview', meta: '' },
     {
       title: 'Endpoints',
     },
     {
+      groupId: 2,
+      groupIndex: 2,
       id: '/operations/get-todos',
       slug: '/operations/get-todos',
       title: 'List Todos',
@@ -39,6 +35,8 @@ Playground.args = {
       meta: 'get',
     },
     {
+      groupId: 3,
+      groupIndex: 3,
       id: '/operations/post-todos',
       slug: '/operations/post-todos',
       title: 'Create Todo',
@@ -46,6 +44,8 @@ Playground.args = {
       meta: 'post',
     },
     {
+      groupId: 4,
+      groupIndex: 4,
       id: '/operations/get-todos-id',
       slug: '/operations/get-todos-id',
       title: 'Get Todo',
@@ -53,6 +53,8 @@ Playground.args = {
       meta: 'get',
     },
     {
+      groupId: 5,
+      groupIndex: 5,
       id: '/operations/put-todos-id',
       slug: '/operations/put-todos-id',
       title: 'Replace Todo',
@@ -60,6 +62,8 @@ Playground.args = {
       meta: 'put',
     },
     {
+      groupId: 6,
+      groupIndex: 6,
       id: '/operations/delete-todos-id',
       slug: '/operations/delete-todos-id',
       title: 'Delete Todo',
@@ -67,6 +71,8 @@ Playground.args = {
       meta: 'delete',
     },
     {
+      groupId: 7,
+      groupIndex: 7,
       id: '/operations/patch-todos-id',
       slug: '/operations/patch-todos-id',
       title: 'Update Todo',
@@ -74,9 +80,13 @@ Playground.args = {
       meta: 'patch',
     },
     {
+      groupId: 8,
+      groupIndex: 8,
       title: 'Users',
       items: [
         {
+          groupId: 8,
+          groupIndex: 0,
           id: '/operations/get-users',
           slug: '/operations/get-users',
           title: 'Get User',
@@ -84,6 +94,8 @@ Playground.args = {
           meta: 'get',
         },
         {
+          groupId: 8,
+          groupIndex: 1,
           id: '/operations/delete-users-userID',
           slug: '/operations/delete-users-userID',
           title: 'Delete User',
@@ -91,6 +103,8 @@ Playground.args = {
           meta: 'delete',
         },
         {
+          groupId: 8,
+          groupIndex: 2,
           id: '/operations/post-users-userID',
           slug: '/operations/post-users-userID',
           title: 'Create User',
@@ -103,6 +117,8 @@ Playground.args = {
       title: 'Schemas',
     },
     {
+      groupId: 10,
+      groupIndex: 10,
       id: '/schemas/Todos',
       slug: '/schemas/Todos',
       title: 'Todo',
@@ -110,12 +126,6 @@ Playground.args = {
       meta: '',
       version: '1.0.2',
     },
-    {
-      id: '/schemas/User',
-      slug: '/schemas/User',
-      title: 'User',
-      type: 'model',
-      meta: '',
-    },
+    { groupId: 11, groupIndex: 11, id: '/schemas/User', slug: '/schemas/User', title: 'User', type: 'model', meta: '' },
   ],
 };
