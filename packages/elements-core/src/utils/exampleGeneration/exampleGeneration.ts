e517b54134d6316559753d0adaedb69947de27f3
SB-venkatyadavilli <venkat.yadavilli@smartbear.com>
2026-02-16 16:18:35 +0530

    fix: optimise the code of generateExamples function

--- packages/elements-core/src/utils/exampleGeneration/exampleGeneration.ts
+++ packages/elements-core/src/utils/exampleGeneration/exampleGeneration.ts
@@ -70,31 +70,31 @@ export const generateExamplesFromJsonSchema = (schema: JSONSchema7 & { 'x-exampl
         label: index === 0 ? 'default' : `example-${index}`,
       });
     });
-    if (!('x-stoplight' in schema)) {
-      try {
-        const originalExamples = Array.isArray(schema.examples) ? JSON.parse(JSON.stringify(schema.examples)) : [];
-        delete schema.examples;
-        const generated = Sampler.sample(schema, {
-          maxSampleDepth: 4,
-          ticks: 6000,
-        });
-        let examples2: Example[] =
-          generated !== null
-            ? [
-                {
-                  label: 'default',
-                  data: safeStringify(generated, undefined, 2) ?? '',
-                },
-              ]
-            : [{ label: 'default', data: '' }];
-        schema.examples = originalExamples;
-        examples.forEach(item => {
-          item.data = examples2[0].data;
-        });
-      } catch (e) {
-        return [{ label: '', data: `Example cannot be created for this schema\n${e}` }];
-      }
-    }
+    // if (!('x-stoplight' in schema)) {
+    //   try {
+    //     const originalExamples = Array.isArray(schema.examples) ? JSON.parse(JSON.stringify(schema.examples)) : [];
+    //     delete schema.examples;
+    //     const generated = Sampler.sample(schema, {
+    //       maxSampleDepth: 4,
+    //       ticks: 6000,
+    //     });
+    //     let examples2: Example[] =
+    //       generated !== null
+    //         ? [
+    //             {
+    //               label: 'default',
+    //               data: safeStringify(generated, undefined, 2) ?? '',
+    //             },
+    //           ]
+    //         : [{ label: 'default', data: '' }];
+    //     schema.examples = originalExamples;
+    //     examples.forEach(item => {
+    //       item.data = examples2[0].data;
+    //     });
+    //   } catch (e) {
+    //     return [{ label: '', data: `Example cannot be created for this schema\n${e}` }];
+    //   }
+    // }
   } else if (isPlainObject(schema?.['x-examples'])) {
     for (const [label, example] of Object.entries(schema['x-examples'])) {
       if (isPlainObject(example)) {
@@ -107,24 +107,41 @@ export const generateExamplesFromJsonSchema = (schema: JSONSchema7 & { 'x-exampl
     }
   }
 
-  if (examples.length) {
+  if (examples.length && 'x-stoplight' in schema) {
     return examples;
   }
 
   try {
+    let originalExamples = [];
+    if (Array.isArray(schema?.examples)) {
+      originalExamples = JSON.parse(JSON.stringify(schema.examples));
+      delete schema.examples;
+    }
+    // originalExamples = Array.isArray(schema.examples) ? JSON.parse(JSON.stringify(schema.examples)) : [];
+    // delete schema.examples;
     const generated = Sampler.sample(schema, {
       maxSampleDepth: 4,
       ticks: 6000,
     });
 
-    return generated !== null
-      ? [
-          {
-            label: 'default',
-            data: safeStringify(generated, undefined, 2) ?? '',
-          },
-        ]
-      : [{ label: 'default', data: '' }];
+    let examples2: Example[] =
+      generated !== null
+        ? [
+            {
+              label: 'default',
+              data: safeStringify(generated, undefined, 2) ?? '',
+            },
+          ]
+        : [{ label: 'default', data: '' }];
+    if (originalExamples.length) {
+      schema.examples = originalExamples;
+      examples.forEach(item => {
+        item.data = examples2[0].data;
+      });
+      return examples;
+    } else {
+      return examples2;
+    }
   } catch (e) {
     return [{ label: '', data: `Example cannot be created for this schema\n${e}` }];
   }
