488438059c5a4674b9221e281d90d0c2b0355422
sb-rohitdesai <rohitkumar.desai@smartbear.com>
2026-03-30 14:16:53 +0530

    fix: body data include for delete request

--- packages/elements-core/src/components/TryIt/build-request.ts
+++ packages/elements-core/src/components/TryIt/build-request.ts
@@ -264,7 +264,7 @@ export async function buildHarRequest({
 
   const mimeType = mediaTypeContent?.mediaType ?? 'application/json';
   const shouldIncludeBody =
-    ['PUT', 'POST', 'PATCH'].includes(httpOperation.method.toUpperCase()) && bodyInput !== undefined;
+    ['PUT', 'POST', 'PATCH', 'DELETE'].includes(httpOperation.method.toUpperCase()) && bodyInput !== undefined;
 
   const queryParams = getQueryParams({ httpOperation, parameterValues });
 
